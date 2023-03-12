61418a85b7d2ec625379315731a94162a657bc8f
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-12 13:05:41 -0400

    moved constants code here

--- lib/src/app-config.dart
+++ lib/src/app-config.dart
@@ -1,19 +1,48 @@
 library ez_ui;
 
-import 'constants.dart';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+const String backImageKey = 'backImage';
+const String backColorKey = 'appBackgroundColor';
+const String themeColorKey = 'themeColor';
+const String themeTextColorKey = 'themeTextColor';
+const String buttonColorKey = 'buttonColor';
+const String buttonTextColorKey = 'buttonTextColor';
+const String buttonSpacingKey = 'buttonSpacing';
+const String dialogSpacingKey = 'dialogSpacing';
+const String marginKey = 'margin';
+const String paddingKey = 'padding';
+const String fontFamilyKey = 'fontFamily';
+const String fontSizeKey = 'fontSize';
+
 class AppConfig {
   static late FocusManager focus;
-
   static late SharedPreferences preferences;
-  static Map<String, dynamic> prefs = new Map.from(defaultConfig);
+  static late Map<String, dynamic> prefs;
+
+  static Map<String, dynamic> defaults = {
+    backImageKey: null,
+    backColorKey: 0xE6A520DA, // Empathetech purple
+    themeColorKey: 0xFF141414, // Almost black
+    themeTextColorKey: 0xFFFFFFFF, // White
+    buttonColorKey: 0xE6DAA520, // Empathetech gold
+    buttonTextColorKey: 0xFF000000, // Black
+    buttonSpacingKey: 35.0,
+    dialogSpacingKey: 20.0,
+    marginKey: 15.0,
+    paddingKey: 12.5,
+    fontFamilyKey: 'Roboto',
+    fontSizeKey: 24.0,
+  };
 
   // Populate prefs, overwriting defaults whenever a user value is found
-  static void init() {
-    defaultConfig.forEach((key, value) {
+  static void init([Map<String, dynamic>? customDefaults]) {
+    if (customDefaults != null) defaults.addAll(customDefaults);
+
+    prefs = new Map.from(defaults);
+
+    defaults.forEach((key, value) {
       dynamic userPref;
 
       if (value is int) {
@@ -30,9 +59,5 @@ class AppConfig {
 
       if (userPref != null) prefs[key] = userPref;
     });
-  }
-
-  static void expandPrefs(Map<String, dynamic> uniqueConfig) {
-    prefs.addAll(uniqueConfig);
   }
 }
