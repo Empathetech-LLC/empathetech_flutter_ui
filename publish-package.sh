238ec95715a1c11453915a13822a0d98dec7f638
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-15 08:17:17 -0400

    removed checks, not needed

--- publish-package.sh
+++ publish-package.sh
@@ -10,21 +10,6 @@ if [[ $BRANCH != "main" ]]; then
   exit 1
 fi
 
-# Make sure there's an existing git tag && release for the current version
-CURR_VERSION=$(cat APP_VERSION)
-
-# Tag check
-if ! git rev-parse --quiet --verify "refs/tags/$CURR_VERSION" > /dev/null 2>&1; then
-  echo "ERROR: there's no git tag matching version $CURR_VERSION"
-  exit 1
-fi
-
-# Release check
-if ! git rev-parse --quiet --verify "refs/tags/$CURR_VERSION" > /dev/null 2>&1; then
-  echo "ERROR: there's no git release matching version $CURR_VERSION"
-  exit 1
-fi
-
 # Publish
 
 dart pub publish
