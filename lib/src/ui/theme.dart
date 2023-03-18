2469927dfb418c18d8900ae736e273688405f126
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-18 09:22:42 -0400

    adding theme configs

---
+++ lib/src/ui/theme.dart
@@ -0,0 +1,15 @@
+library empathetech_flutter_ui;
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+MaterialAppData androidAppTheme() {
+  return MaterialAppData();
+}
+
+CupertinoAppData iosAppTheme() {
+  return CupertinoAppData();
+}
