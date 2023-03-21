22a60bf5ad84b31162dead388b91a12c2484a660
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-21 06:47:55 -0400

    updated imports

--- lib/src/notifications.dart
+++ lib/src/notifications.dart
@@ -1,4 +1,7 @@
-import 'dart:async';
+library empathetech_flutter_ui;
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
