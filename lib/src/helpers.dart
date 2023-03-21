b2b0e76416b4bff371b7aca66f7b7511afbffa28
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-21 10:42:54 -0400

    added do nothing

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -39,3 +39,7 @@ Color getContrastColor(Color background) {
 
   return (((r * 0.299) + (g * 0.587) + (b * 0.114)) >= 150) ? Colors.black : Colors.white;
 }
+
+/// Do you have a void [Function] as a parameter that you want to be optional?
+/// Then do nothing!
+void doNothing() {}
