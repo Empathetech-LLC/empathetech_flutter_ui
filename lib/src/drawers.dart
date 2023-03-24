65938dc1af103876b9db77316bf05cdf424490aa
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-24 16:22:58 -0400

    made platform dynamic drawer

--- lib/src/drawers.dart
+++ lib/src/drawers.dart
@@ -8,18 +8,34 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 /// Builds a [PlatformTarget] dynamic end [Drawer]
 /// Cupertino retuins a [CupertinoActionSheet] for [showCupertinoModalPopup]
-Widget ezDrawer({
+Widget? ezDrawer({
   required BuildContext context,
   required PlatformTarget platform,
-  required Widget header,
-  required List<Widget> body,
+  required Widget? header,
+  required List<Widget>? body,
 }) {
+  if (header == null || body == null) {
+    return null;
+  }
+
   Color themeColor = Color(AppConfig.prefs[themeColorKey]);
 
   if (platform == PlatformTarget.iOS || platform == PlatformTarget.macOS) {
-    return CupertinoActionSheet();
+    return GestureDetector(
+      onTap: () => showCupertinoModalPopup(
+        context: context,
+        builder: (BuildContext context) => CupertinoActionSheet(
+          title: header,
+          actions: body,
+        ),
+      ),
+      child: Icon(
+        CupertinoIcons.line_horizontal_3,
+        color: Color(AppConfig.prefs[themeTextColorKey]),
+      ),
+    );
   } else {
-    body.insert(0, header);
+    body.insert(0, DrawerHeader(child: header));
     return Drawer(
       backgroundColor: themeColor,
       child: ezScrollView(children: body),
