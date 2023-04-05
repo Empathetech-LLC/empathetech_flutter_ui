50d7c52633423f32f2a31a113b99bae700d490d9
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-05 13:21:54 -0400

    refresh should work now

--- lib/src/image-setting.dart
+++ lib/src/image-setting.dart
@@ -2,13 +2,13 @@ library empathetech_flutter_ui;
 
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 /// Creates a tool for updating the image at [prefsKey]'s path
-/// Take in the [isAsset] bool so an image preview can be built
 class ImageSetting extends StatefulWidget {
   const ImageSetting({
     Key? key,
@@ -16,100 +16,117 @@ class ImageSetting extends StatefulWidget {
     required this.fullscreen,
     required this.title,
     required this.credits,
+    required this.allowClear,
   }) : super(key: key);
 
   final String prefsKey;
   final bool fullscreen;
   final String title;
   final String credits;
+  final bool allowClear;
 
   @override
   _ImageSettingState createState() => _ImageSettingState();
 }
 
 class _ImageSettingState extends State<ImageSetting> {
-  // Initialize state
-
+  late String title = widget.title;
   late String currPath = widget.prefsKey;
+
   late TextStyle buttonTextStyle = getTextStyle(buttonStyleKey);
+
   late double buttonSpacer = AppConfig.prefs[buttonSpacingKey];
   late double dialogSpacer = AppConfig.prefs[dialogSpacingKey];
 
+  /// Cleanup any custom files
+  void cleanup() async {
+    if (!isAsset(widget.prefsKey)) {
+      try {
+        File toDelete = File(widget.prefsKey);
+        await toDelete.delete();
+      } catch (e) {
+        doNothing();
+      }
+    }
+  }
+
   /// Opens an [ezDialog] for choosing the [ImageSource] for updating [prefsKey]
   /// Selection is sent to [changeImage]
-  void chooseImage() {
-    ezDialog(
-      context,
-      title: 'Update background',
-      content: [
-        // From file
+  Future<dynamic> chooseImage() async {
+    List<Widget> options = [
+      // From file
+      EZButton.icon(
+        action: () async {
+          String? changed = await changeImage(
+            context,
+            prefsPath: widget.prefsKey,
+            source: ImageSource.gallery,
+          );
+
+          popScreen(context, pass: changed);
+        },
+        message: 'File',
+        icon: ezIcon(PlatformIcons(context).folder),
+      ),
+      Container(height: buttonSpacer),
+
+      // From camera
+      EZButton.icon(
+        action: () async {
+          String? changed = await changeImage(
+            context,
+            prefsPath: widget.prefsKey,
+            source: ImageSource.camera,
+          );
+
+          popScreen(context, pass: changed);
+        },
+        message: 'Camera',
+        icon: ezIcon(PlatformIcons(context).photoCamera),
+      ),
+      Container(height: buttonSpacer),
+
+      // Reset
+      EZButton.icon(
+        action: () async {
+          cleanup();
+
+          AppConfig.preferences.remove(widget.prefsKey);
+          popScreen(context, pass: AppConfig.defaults[widget.prefsKey]);
+        },
+        message: 'Reset',
+        icon: ezIcon(PlatformIcons(context).refresh),
+      ),
+    ];
+
+    if (widget.allowClear)
+      options.addAll([
+        Container(height: buttonSpacer),
         EZButton.icon(
           action: () async {
-            await changeImage(
-              context,
-              prefsPath: widget.prefsKey,
-              source: ImageSource.gallery,
-            );
+            cleanup();
 
-            Navigator.of(context).pop();
-          },
-          icon: ezIcon(PlatformIcons(context).folder),
-          message: 'File',
-        ),
-        Container(height: buttonSpacer),
-
-        // From camera
-        EZButton.icon(
-          action: () async {
-            await changeImage(
-              context,
-              prefsPath: widget.prefsKey,
-              source: ImageSource.camera,
-            );
-
-            Navigator.of(context).pop();
-          },
-          icon: ezIcon(PlatformIcons(context).photoCamera),
-          message: 'Camera',
-        ),
-        Container(height: buttonSpacer),
-
-        // Reset
-        EZButton.icon(
-          action: () {
-            AppConfig.preferences.remove(widget.prefsKey);
-            setState(() {
-              AppConfig.prefs[widget.prefsKey] = AppConfig.defaults[widget.prefsKey];
-              currPath = widget.prefsKey;
-            });
-            Navigator.of(context).pop();
-          },
-          icon: ezIcon(PlatformIcons(context).refresh),
-          message: 'Reset',
-        ),
-        Container(height: buttonSpacer),
-
-        // Clear
-        EZButton.icon(
-          action: () {
             AppConfig.preferences.setString(widget.prefsKey, noImageKey);
-            setState(() {
-              currPath = noImageKey;
-            });
-            Navigator.of(context).pop();
+            popScreen(context, pass: noImageKey);
           },
-          icon: ezIcon(PlatformIcons(context).clear),
           message: 'Clear',
+          icon: ezIcon(PlatformIcons(context).clear),
         ),
-        Container(height: buttonSpacer),
-      ],
-    );
+      ]);
+
+    return ezDialog(context, title: 'Update $title', content: options);
   }
 
   @override
   Widget build(BuildContext context) {
     return EZButton(
-      action: chooseImage,
+      action: () async {
+        dynamic newPath = await chooseImage();
+        if (newPath is String)
+          setState(() {
+            currPath = newPath;
+          });
+      },
       longAction: () => ezDialog(
         context,
         title: 'Credit to:',
@@ -120,7 +137,7 @@ class _ImageSettingState extends State<ImageSetting> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           // Title on the left
-          Text(widget.title, style: getTextStyle(imageSettingStyleKey)),
+          Text(title, style: getTextStyle(imageSettingStyleKey)),
 
           // Preview on the right
           // 16:9 for backgrounds, 1:1 for the rest
@@ -129,10 +146,7 @@ class _ImageSettingState extends State<ImageSetting> {
             width: widget.fullscreen ? 90 : 75,
             child: currPath == noImageKey
                 ? ezIcon(PlatformIcons(context).clear)
-                : buildImage(
-                    pathKey: currPath,
-                    fit: BoxFit.fill,
-                  ),
+                : buildImage(pathKey: currPath, fit: BoxFit.fill),
           ),
         ],
       ),
