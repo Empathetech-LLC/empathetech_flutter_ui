a4b8a073283919b7db85fce19dc825de81fb87cf
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-12 13:24:22 -0400

    fixed replay bug with listener

--- lib/src/classes/EzVideoPlayer.dart
+++ lib/src/classes/EzVideoPlayer.dart
@@ -10,16 +10,20 @@ class EzVideoPlayer extends StatefulWidget {
   final VideoPlayerController controller;
   final Color iconColor;
   final double? alwaysOn;
+  final bool autoLoop;
 
   /// [Stack]s play, mute, and replay buttons on top of an [AspectRatio], the recommended parent for [VideoPlayer]s
   /// Optionally provide an opacity value with [alwaysOn] if you want the buttons to persist
   /// Otherwise, the buttons will be transparent until the user hovers over the [VideoPlayer]
+  /// Optionally provide [autoLoop] if you want the video to loop upon completion
+  /// Otherwise, the video will automatically pause on the last frame
   /// Videos begin muted, but the volume will be raised to full upon replay
   const EzVideoPlayer({
     Key? key,
     required this.controller,
     required this.iconColor,
     this.alwaysOn,
+    this.autoLoop = false,
   }) : super(key: key);
 
   @override
@@ -62,6 +66,27 @@ class _EzVideoPlayerState extends State<EzVideoPlayer> {
     widget.controller.setVolume(1.0);
     widget.controller.play();
     setState(() {});
+  }
+
+  /// Automatically pauses the video on the final frame
+  /// Prevents a visually bug that occurs when a video is considered completed
+  void _videoListener() {
+    if (widget.controller.value.isPlaying &&
+        widget.controller.value.position >= widget.controller.value.duration) {
+      _pauseVideo();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.autoLoop) {
+      widget.controller.setLooping(true);
+    } else {
+      widget.controller.setLooping(false);
+      widget.controller.addListener(_videoListener);
+    }
   }
 
   @override
@@ -142,5 +167,11 @@ class _EzVideoPlayerState extends State<EzVideoPlayer> {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    if (!widget.autoLoop) widget.controller.removeListener(_videoListener);
+    super.dispose();
   }
 }
