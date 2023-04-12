8b4a23cbada1705fd332794ca1a10a431991a5dc
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-12 08:23:14 -0400

    added video controls

--- lib/src/web-widgets.dart
+++ lib/src/web-widgets.dart
@@ -1,6 +1,9 @@
 library empathetech_flutter_ui;
 
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 /// Quick [MouseRegion] && [GestureDetector] combo
 Widget ezClickable({
@@ -15,5 +18,40 @@ Widget ezClickable({
       onLongPress: onLongPress,
       child: child,
     ),
+  );
+}
+
+Stack userVideoControls({
+  required BuildContext context,
+  required AspectRatio video,
+  required void Function() play,
+  required void Function() mute,
+}) {
+  double margin = EzConfig.prefs[dialogSpacingKey];
+
+  return Stack(
+    children: [
+      video,
+
+      // Play/pause
+      Positioned(
+        bottom: margin,
+        left: margin,
+        child: ezClickable(
+          child: ezIcon(PlatformIcons(context).playArrow),
+          onTap: play,
+        ),
+      ),
+
+      // Volume
+      Positioned(
+        bottom: margin,
+        left: margin * 2.0,
+        child: ezClickable(
+          child: ezIcon(PlatformIcons(context).volumeMute),
+          onTap: mute,
+        ),
+      ),
+    ],
   );
 }
