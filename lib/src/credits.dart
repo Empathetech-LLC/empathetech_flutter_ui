2ab0e076566afd730e945cdd26fe80577ff5921c
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-12 16:08:39 -0400

    initial commit

---
+++ lib/src/credits.dart
@@ -0,0 +1,14 @@
+library empathetech_flutter_ui;
+
+/// If you want to show some love
+const String EmpathetechPayPal =
+    'https://www.paypal.com/donate/?hosted_button_id=NGEL6AB5A6KNL';
+
+/// If you want to show some love
+const String EmpathetechVenmo = 'https://venmo.com/empathetech-llc';
+
+/// If you want to show some love
+const String EmpathetechCashApp = 'https://cash.app/\$empathetech';
+
+/// Where the magic happens
+const String EmpathetechGitHub = 'https://github.com/Empathetech-LLC';
