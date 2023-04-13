53f535a2311881ccae95b3e5c497e972336e70df
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-13 17:13:38 -0400

    added optional params

--- lib/src/classes/EzAppBar.dart
+++ lib/src/classes/EzAppBar.dart
@@ -8,31 +8,45 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 class EzAppBar extends PlatformAppBar {
   final Key? key;
   final Key? widgetKey;
-  final bool? automaticallyImplyLeading;
-  final Widget? leading;
   final Widget title;
+
+  /// Default -> [EzConfig] themeColorKey
+  final Color? backgroundColor;
+
+  final Widget? leading;
   final dynamic trailing;
+  final bool? automaticallyImplyLeading;
+  final MaterialAppBarData Function(BuildContext, PlatformTarget)? material;
+  final CupertinoNavigationBarData Function(BuildContext, PlatformTarget)? cupertino;
 
   /// Styles a [PlatformAppBar] with [EzConfig.prefs]
   EzAppBar({
     this.key,
     this.widgetKey,
-    this.automaticallyImplyLeading,
-    this.leading,
     required this.title,
+    this.backgroundColor,
+    this.leading,
     this.trailing,
+    this.automaticallyImplyLeading,
+    this.material,
+    this.cupertino,
   }) : super(
-          // Widgets
-          automaticallyImplyLeading: automaticallyImplyLeading,
-          leading: leading,
-          title: title,
-          trailingActions: (trailing is List<Widget>) ? trailing : null,
+          key: key,
+          widgetKey: widgetKey,
 
-          // Color
-          backgroundColor: Color(EzConfig.prefs[themeColorKey]),
+          title: title,
+          backgroundColor: backgroundColor ?? Color(EzConfig.prefs[themeColorKey]),
+          leading: leading,
+
+          // Only set trailing actions if trailing is a List
+          // Otherwise, trailing is a [Drawer], and will be handled in [EzScaffold]
+          trailingActions: (trailing is List<Widget>) ? trailing : null,
+          automaticallyImplyLeading: automaticallyImplyLeading,
 
           // Platform configs
-          // Material is handled in EzScaffold.dart
+          // By default, only override the iOS trailing widget in the case that this.trailing
+          // is a [Drawer]
+          material: material,
           cupertino: (context, platform) => CupertinoNavigationBarData(
             trailing: (trailing is Widget) ? trailing : null,
           ),
