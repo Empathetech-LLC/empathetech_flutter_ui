07f3cdfb3a8e7baa70dbbd3cf8fd8c52e2edf1a9
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-14 09:19:29 -0400

    removed unused const

--- lib/src/classes/EzApp.dart
+++ lib/src/classes/EzApp.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-const String homeRoute = '/';
-
 class EzApp extends PlatformProvider {
   final Key? key;
   final Key? widgetKey;
