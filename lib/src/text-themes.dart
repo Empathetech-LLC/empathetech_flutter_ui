1f96b6878e6f5f110198d45eba0998a267e01345
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-19 13:58:43 -0400

    re-organized

--- lib/src/text-themes.dart
+++ lib/src/text-themes.dart
@@ -5,6 +5,123 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+/// Overrides defaults with [EzConfig]
+TextTheme materialTextTheme() {
+  double fontScalar = EzConfig.prefs[fontScalarKey];
+
+  return TextTheme(
+    // Displays
+    displayLarge: EzTextStyle(
+      fontSize: 58 * fontScalar,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: EzTextStyle(
+      fontSize: 46 * fontScalar,
+      fontWeight: FontWeight.bold,
+    ),
+    displaySmall: EzTextStyle(
+      fontSize: 36 * fontScalar,
+      fontWeight: FontWeight.bold,
+    ),
+
+    // Headlines
+    headlineLarge: EzTextStyle(
+      fontSize: 32,
+      decoration: TextDecoration.underline,
+    ),
+    headlineMedium: EzTextStyle(
+      fontSize: 28,
+      decoration: TextDecoration.underline,
+    ),
+    headlineSmall: EzTextStyle(
+      fontSize: 24,
+      decoration: TextDecoration.underline,
+    ),
+
+    // Titles
+    titleLarge: EzTextStyle(fontSize: 22),
+    titleMedium: EzTextStyle(fontSize: 16),
+    titleSmall: EzTextStyle(fontSize: 14),
+
+    // Labels
+    labelLarge: EzTextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+    ),
+    labelMedium: EzTextStyle(
+      fontSize: 12,
+      decoration: TextDecoration.underline,
+    ),
+    labelSmall: EzTextStyle(
+      fontSize: 10,
+      decoration: TextDecoration.underline,
+    ),
+
+    // Body
+    bodyLarge: EzTextStyle(fontSize: 16),
+    bodyMedium: EzTextStyle(fontSize: 14),
+    bodySmall: EzTextStyle(fontSize: 12),
+  );
+}
+
+enum MaterialStyles {
+  displayLarge,
+  displayMedium,
+  displaySmall,
+  headlineLarge,
+  headlineMedium,
+  headlineSmall,
+  titleLarge,
+  titleMedium,
+  titleSmall,
+  labelLarge,
+  labelMedium,
+  labelSmall,
+  bodyLarge,
+  bodyMedium,
+  bodySmall,
+}
+
+/// Overrides defaults with [EzConfig]
+CupertinoTextThemeData cupertinoTextTheme() {
+  return CupertinoTextThemeData(
+    primaryColor: Color(EzConfig.prefs[themeTextColorKey]),
+
+    // bodyLarge
+    textStyle: EzTextStyle(fontSize: 16),
+
+    // labelLarge
+    actionTextStyle: EzTextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+    ),
+
+    // labelLarge
+    tabLabelTextStyle: EzTextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+    ),
+
+    // titleMedium
+    navTitleTextStyle: EzTextStyle(fontSize: 16),
+
+    // titleLarge
+    navLargeTitleTextStyle: EzTextStyle(fontSize: 22),
+
+    // labelLarge
+    navActionTextStyle: EzTextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+    ),
+
+    // bodyLarge
+    pickerTextStyle: EzTextStyle(fontSize: 16),
+
+    // bodyLarge
+    dateTimePickerTextStyle: EzTextStyle(fontSize: 16),
+  );
+}
+
 /// More readable than Theme.of(context).textTheme.
 /// Also redundant for confidence
 TextStyle ezTextStyle(
@@ -90,121 +207,4 @@ TextStyle ezTextStyle(
     case MaterialStyles.bodySmall:
       return Theme.of(context).textTheme.bodySmall ?? EzTextStyle(fontSize: 12);
   }
-}
-
-enum MaterialStyles {
-  displayLarge,
-  displayMedium,
-  displaySmall,
-  headlineLarge,
-  headlineMedium,
-  headlineSmall,
-  titleLarge,
-  titleMedium,
-  titleSmall,
-  labelLarge,
-  labelMedium,
-  labelSmall,
-  bodyLarge,
-  bodyMedium,
-  bodySmall,
-}
-
-/// Overrides defaults with [EzConfig]
-TextTheme materialTextTheme() {
-  double fontScalar = EzConfig.prefs[fontScalarKey];
-
-  return TextTheme(
-    // Displays
-    displayLarge: EzTextStyle(
-      fontSize: 58 * fontScalar,
-      fontWeight: FontWeight.bold,
-    ),
-    displayMedium: EzTextStyle(
-      fontSize: 46 * fontScalar,
-      fontWeight: FontWeight.bold,
-    ),
-    displaySmall: EzTextStyle(
-      fontSize: 36 * fontScalar,
-      fontWeight: FontWeight.bold,
-    ),
-
-    // Headlines
-    headlineLarge: EzTextStyle(
-      fontSize: 32,
-      decoration: TextDecoration.underline,
-    ),
-    headlineMedium: EzTextStyle(
-      fontSize: 28,
-      decoration: TextDecoration.underline,
-    ),
-    headlineSmall: EzTextStyle(
-      fontSize: 24,
-      decoration: TextDecoration.underline,
-    ),
-
-    // Titles
-    titleLarge: EzTextStyle(fontSize: 22),
-    titleMedium: EzTextStyle(fontSize: 16),
-    titleSmall: EzTextStyle(fontSize: 14),
-
-    // Labels
-    labelLarge: EzTextStyle(
-      fontSize: 14,
-      decoration: TextDecoration.underline,
-    ),
-    labelMedium: EzTextStyle(
-      fontSize: 12,
-      decoration: TextDecoration.underline,
-    ),
-    labelSmall: EzTextStyle(
-      fontSize: 10,
-      decoration: TextDecoration.underline,
-    ),
-
-    // Body
-    bodyLarge: EzTextStyle(fontSize: 16),
-    bodyMedium: EzTextStyle(fontSize: 14),
-    bodySmall: EzTextStyle(fontSize: 12),
-  );
-}
-
-/// Overrides defaults with [EzConfig]
-CupertinoTextThemeData cupertinoTextTheme() {
-  return CupertinoTextThemeData(
-    primaryColor: Color(EzConfig.prefs[themeTextColorKey]),
-
-    // bodyLarge
-    textStyle: EzTextStyle(fontSize: 16),
-
-    // labelLarge
-    actionTextStyle: EzTextStyle(
-      fontSize: 14,
-      decoration: TextDecoration.underline,
-    ),
-
-    // labelLarge
-    tabLabelTextStyle: EzTextStyle(
-      fontSize: 14,
-      decoration: TextDecoration.underline,
-    ),
-
-    // titleMedium
-    navTitleTextStyle: EzTextStyle(fontSize: 16),
-
-    // titleLarge
-    navLargeTitleTextStyle: EzTextStyle(fontSize: 22),
-
-    // labelLarge
-    navActionTextStyle: EzTextStyle(
-      fontSize: 14,
-      decoration: TextDecoration.underline,
-    ),
-
-    // bodyLarge
-    pickerTextStyle: EzTextStyle(fontSize: 16),
-
-    // bodyLarge
-    dateTimePickerTextStyle: EzTextStyle(fontSize: 16),
-  );
 }
