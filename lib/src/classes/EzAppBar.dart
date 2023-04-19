8cc2dfe6510db8b64cefe2ee5bb6de4c4f02fe02
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-19 17:38:07 -0400

    switching to app bar

--- lib/src/classes/EzAppBar.dart
+++ lib/src/classes/EzAppBar.dart
@@ -3,53 +3,64 @@ library empathetech_flutter_ui;
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter/services.dart';
 
-class EzAppBar extends PlatformAppBar {
-  final Key? key;
-  final Key? widgetKey;
-  final Widget title;
-
-  /// Default:
-  /// [EzConfig.prefs] -> themeColorKey
-  final Color? backgroundColor;
-
-  final Widget? leading;
-  final dynamic trailing;
-  final bool? automaticallyImplyLeading;
-  final MaterialAppBarData Function(BuildContext, PlatformTarget)? material;
-  final CupertinoNavigationBarData Function(BuildContext, PlatformTarget)? cupertino;
-
-  /// Styles a [PlatformAppBar] with [EzConfig.prefs]
+class EzAppBar extends AppBar {
   EzAppBar({
-    this.key,
-    this.widgetKey,
-    required this.title,
-    this.backgroundColor,
-    this.leading,
-    this.trailing,
-    this.automaticallyImplyLeading,
-    this.material,
-    this.cupertino,
+    Key? key,
+    Widget? leading,
+    bool automaticallyImplyLeading = true,
+    Widget? title,
+    List<Widget>? actions,
+    Widget? flexibleSpace,
+    PreferredSizeWidget? bottom,
+    double? elevation,
+    double? scrolledUnderElevation,
+    bool Function(ScrollNotification) notificationPredicate =
+        defaultScrollNotificationPredicate,
+    Color? shadowColor,
+    Color? surfaceTintColor,
+    ShapeBorder? shape,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    IconThemeData? iconTheme,
+    IconThemeData? actionsIconTheme,
+    bool primary = true,
+    bool? centerTitle,
+    bool excludeHeaderSemantics = false,
+    double? titleSpacing,
+    double toolbarOpacity = 1.0,
+    double bottomOpacity = 1.0,
+    double? toolbarHeight,
+    double? leadingWidth,
+    TextStyle? toolbarTextStyle,
+    TextStyle? titleTextStyle,
+    SystemUiOverlayStyle? systemOverlayStyle,
   }) : super(
           key: key,
-          widgetKey: widgetKey,
-
-          title: title,
-          backgroundColor: backgroundColor,
           leading: leading,
-
-          // Only set trailing actions if trailing is a List
-          // Otherwise, trailing is a [Drawer], and will be handled in [EzScaffold]
-          trailingActions: (trailing is List<Widget>) ? trailing : null,
           automaticallyImplyLeading: automaticallyImplyLeading,
-
-          // Platform configs
-          // By default, only override the iOS trailing widget in the case that this.trailing
-          // is a [Drawer]
-          material: material,
-          cupertino: (context, platform) => CupertinoNavigationBarData(
-            trailing: (trailing is Widget) ? trailing : null,
-          ),
+          title: title,
+          actions: actions,
+          flexibleSpace: flexibleSpace,
+          bottom: bottom,
+          elevation: elevation,
+          shadowColor: shadowColor,
+          shape: shape,
+          backgroundColor: backgroundColor,
+          foregroundColor: foregroundColor,
+          iconTheme: iconTheme,
+          actionsIconTheme: actionsIconTheme,
+          primary: primary,
+          centerTitle: centerTitle,
+          excludeHeaderSemantics: excludeHeaderSemantics,
+          titleSpacing: titleSpacing,
+          toolbarOpacity: toolbarOpacity,
+          bottomOpacity: bottomOpacity,
+          toolbarHeight: toolbarHeight,
+          leadingWidth: leadingWidth,
+          toolbarTextStyle: toolbarTextStyle,
+          titleTextStyle: titleTextStyle,
+          systemOverlayStyle: systemOverlayStyle,
         );
 }
