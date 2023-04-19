99ff1c3557fae2702751a7f9d33412cbb170c78e
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-19 09:49:57 -0400

    added wrapper

---
+++ lib/src/classes/EzTextStyle.dart
@@ -0,0 +1,65 @@
+library empathetech_flutter_ui;
+
+import 'dart:ui';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+
+class EzTextStyle extends TextStyle {
+  /// Setup a [TextStyle] with some custom defaults from [EzConfig]
+  EzTextStyle({
+    bool inherit = true,
+    Color? color,
+    Color? backgroundColor,
+    double? fontSize,
+    FontWeight? fontWeight,
+    FontStyle? fontStyle,
+    double? letterSpacing,
+    double? wordSpacing,
+    TextBaseline? textBaseline,
+    double? height,
+    TextLeadingDistribution? leadingDistribution,
+    Locale? locale,
+    Paint? foreground,
+    Paint? background,
+    List<Shadow>? shadows,
+    List<FontFeature>? fontFeatures,
+    List<FontVariation>? fontVariations,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+    String? debugLabel,
+    String? fontFamily,
+    List<String>? fontFamilyFallback,
+    String? package,
+    TextOverflow? overflow,
+  }) : super(
+          inherit: inherit,
+          color: color,
+          backgroundColor: backgroundColor,
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          fontStyle: fontStyle,
+          letterSpacing: letterSpacing,
+          wordSpacing: wordSpacing,
+          textBaseline: textBaseline,
+          height: height,
+          leadingDistribution: leadingDistribution,
+          locale: locale,
+          foreground: foreground,
+          background: background,
+          shadows: shadows,
+          fontFeatures: fontFeatures,
+          fontVariations: fontVariations,
+          decoration: decoration,
+          decorationColor: decorationColor,
+          decorationStyle: decorationStyle,
+          decorationThickness: decorationThickness,
+          debugLabel: debugLabel,
+          fontFamily: fontFamily ?? gStyle(EzConfig.prefs[fontFamilyKey]).fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+          package: package,
+          overflow: overflow,
+        );
+}
