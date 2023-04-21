d86b2f76bd0790683e1069bb79995af9e564a8ef
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-21 13:51:58 -0400

    expanded imports

--- lib/src/classes/EzMaterialAppData.dart
+++ lib/src/classes/EzMaterialAppData.dart
@@ -6,44 +6,86 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzMaterialAppData extends MaterialAppData {
+  final Key? widgetKey;
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final Widget? home;
+  final Map<String, Widget Function(BuildContext)>? routes;
+  final String? initialRoute;
+  final Route<dynamic>? Function(RouteSettings)? onGenerateRoute;
+  final Route<dynamic>? Function(RouteSettings)? onUnknownRoute;
+  final List<NavigatorObserver>? navigatorObservers;
+  final String? title;
+  final String Function(BuildContext)? onGenerateTitle;
+  final Color? color;
+  final Locale? locale;
+  final Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates;
+  final Locale? Function(List<Locale>?, Iterable<Locale>)?
+      localeListResolutionCallback;
+  final Locale? Function(Locale?, Iterable<Locale>)? localeResolutionCallback;
+  final Iterable<Locale>? supportedLocales;
+  final bool? showPerformanceOverlay;
+  final bool? checkerboardRasterCacheImages;
+  final bool? checkerboardOffscreenLayers;
+  final bool? showSemanticsDebugger;
+  final bool? debugShowCheckedModeBanner;
+  final Map<LogicalKeySet, Intent>? shortcuts;
+  final Map<Type, Action<Intent>>? actions;
+  final List<Route<dynamic>> Function(String)? onGenerateInitialRoutes;
+  final String? restorationScopeId;
+  final ScrollBehavior? scrollBehavior;
+  final bool? useInheritedMediaQuery;
+
+  /// Default: [ezThemeData] -> light: true
+  final ThemeData? theme;
+  final bool? debugShowMaterialGrid;
+
+  /// Default: [ezThemeData] -> light: false
+  final ThemeData? darkTheme;
+
+  /// Default: [EzConfig.themeMode]
+  final ThemeMode? themeMode;
+
+  final GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey;
+  final Curve themeAnimationCurve;
+  final Duration themeAnimationDuration;
+
   /// [MaterialAppData] wrapper that uses handles switching between
   /// [ezThemeData] light && dark
   EzMaterialAppData({
-    Key? widgetKey,
-    GlobalKey<NavigatorState>? navigatorKey,
-    Widget? home,
-    Map<String, Widget Function(BuildContext)>? routes,
-    String? initialRoute,
-    Route<dynamic>? Function(RouteSettings)? onGenerateRoute,
-    Route<dynamic>? Function(RouteSettings)? onUnknownRoute,
-    List<NavigatorObserver>? navigatorObservers,
-    String? title,
-    String Function(BuildContext)? onGenerateTitle,
-    Color? color,
-    Locale? locale,
-    Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates,
-    Locale? Function(List<Locale>?, Iterable<Locale>)?
-        localeListResolutionCallback,
-    Locale? Function(Locale?, Iterable<Locale>)? localeResolutionCallback,
-    Iterable<Locale>? supportedLocales,
-    bool? showPerformanceOverlay,
-    bool? checkerboardRasterCacheImages,
-    bool? checkerboardOffscreenLayers,
-    bool? showSemanticsDebugger,
-    bool? debugShowCheckedModeBanner,
-    Map<LogicalKeySet, Intent>? shortcuts,
-    Map<Type, Action<Intent>>? actions,
-    List<Route<dynamic>> Function(String)? onGenerateInitialRoutes,
-    String? restorationScopeId,
-    ScrollBehavior? scrollBehavior,
-    bool? useInheritedMediaQuery,
-    ThemeData? theme,
-    bool? debugShowMaterialGrid,
-    ThemeData? darkTheme,
-    ThemeMode? themeMode,
-    GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey,
-    Curve themeAnimationCurve = Curves.linear,
-    Duration themeAnimationDuration = kThemeAnimationDuration,
+    this.widgetKey,
+    this.navigatorKey,
+    this.home,
+    this.routes,
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onUnknownRoute,
+    this.navigatorObservers,
+    this.title,
+    this.onGenerateTitle,
+    this.color,
+    this.locale,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales,
+    this.showPerformanceOverlay,
+    this.checkerboardRasterCacheImages,
+    this.checkerboardOffscreenLayers,
+    this.showSemanticsDebugger,
+    this.debugShowCheckedModeBanner,
+    this.shortcuts,
+    this.actions,
+    this.onGenerateInitialRoutes,
+    this.restorationScopeId,
+    this.scrollBehavior,
+    this.useInheritedMediaQuery,
+    this.theme,
+    this.debugShowMaterialGrid,
+    this.darkTheme,
+    this.themeMode,
+    this.scaffoldMessengerKey,
+    this.themeAnimationCurve = Curves.linear,
+    this.themeAnimationDuration = kThemeAnimationDuration,
   }) : super(
           widgetKey: widgetKey,
           navigatorKey: navigatorKey,
@@ -75,7 +117,7 @@ class EzMaterialAppData extends MaterialAppData {
           theme: theme ?? ezThemeData(light: true),
           debugShowMaterialGrid: debugShowMaterialGrid,
           darkTheme: darkTheme ?? ezThemeData(light: false),
-          themeMode: themeMode ?? ThemeMode.system,
+          themeMode: themeMode ?? EzConfig.themeMode,
           scaffoldMessengerKey: scaffoldMessengerKey,
           themeAnimationCurve: themeAnimationCurve,
           themeAnimationDuration: themeAnimationDuration,
