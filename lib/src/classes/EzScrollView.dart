a9c213ee658314a5864551133be12e6ab71b25c6
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-21 10:17:52 -0400

    cleanup

--- lib/src/classes/EzScrollView.dart
+++ lib/src/classes/EzScrollView.dart
@@ -3,6 +3,7 @@ library empathetech_flutter_ui;
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 class EzScrollView extends SingleChildScrollView {
   final Key? key;
@@ -15,10 +16,13 @@ class EzScrollView extends SingleChildScrollView {
   final ScrollPhysics? physics;
   final ScrollController? controller;
   final Widget? child;
-  final List<Widget> children;
 
   /// Default: [Clip.hardEdge]
   final Clip clipBehavior;
+
+  /// Default: [DragStartBehavior.start]
+  final DragStartBehavior dragStartBehavior;
+
   final String? restorationId;
 
   /// Default: [ScrollViewKeyboardDismissBehavior.manual]
@@ -39,11 +43,14 @@ class EzScrollView extends SingleChildScrollView {
   /// Default: [VerticalDirection.down]
   final VerticalDirection verticalDirection;
 
+  final List<Widget> children;
+
   /// Styles a [SingleChildScrollView] from [EzConfig.prefs]
   /// Dynamically switches between row/col based on [direction]
   /// Uses a [children] list rather than [child] Widget
   /// Optionally overwrite with -> child: child && children: []
   EzScrollView({
+    // SingleChildScrollView
     this.key,
     this.scrollDirection = Axis.vertical,
     this.reverse = false,
@@ -52,16 +59,19 @@ class EzScrollView extends SingleChildScrollView {
     this.physics,
     this.controller,
     this.child,
-    required this.children,
     this.clipBehavior = Clip.hardEdge,
+    this.dragStartBehavior = DragStartBehavior.start,
     this.restorationId,
     this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+
+    // Row/Column
     this.mainAxisSize = MainAxisSize.min,
     this.mainAxisAlignment = MainAxisAlignment.spaceEvenly,
     this.crossAxisAlignment = CrossAxisAlignment.center,
     this.textDirection,
     this.textBaseline,
     this.verticalDirection = VerticalDirection.down,
+    required this.children,
   });
 
   Widget _buildCore() {
@@ -99,6 +109,7 @@ class EzScrollView extends SingleChildScrollView {
       physics: physics ?? BouncingScrollPhysics(),
       controller: controller,
       child: child ?? core,
+      dragStartBehavior: dragStartBehavior,
       clipBehavior: clipBehavior,
       restorationId: restorationId,
       keyboardDismissBehavior: keyboardDismissBehavior,
