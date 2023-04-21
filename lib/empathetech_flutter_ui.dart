c7c041d4b4951b6bb4dda66d812b4e6aebaae6d9
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-21 10:06:27 -0400

    updated exports

--- lib/empathetech_flutter_ui.dart
+++ lib/empathetech_flutter_ui.dart
@@ -3,11 +3,12 @@ library empathetech_flutter_ui;
 export 'src/classes/classes.dart';
 
 export 'src/alerts.dart';
+export 'src/colors.dart';
 export 'src/credits.dart';
 export 'src/dialogs.dart';
 export 'src/google-fonts.dart';
 export 'src/helpers.dart';
-export 'src/scaffold-views.dart';
+export 'src/navigators.dart';
 export 'src/storage.dart';
 export 'src/text-themes.dart';
 export 'src/theme-data.dart';
