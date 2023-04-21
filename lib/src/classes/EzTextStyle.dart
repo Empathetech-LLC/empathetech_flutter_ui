4426a0922c59387d84e48d3f1ebc47dbf1c9307d
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-21 14:15:23 -0400

    expanded params

--- lib/src/classes/EzTextStyle.dart
+++ lib/src/classes/EzTextStyle.dart
@@ -6,34 +6,71 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 
 class EzTextStyle extends TextStyle {
-  /// Setup a [TextStyle] with some custom defaults from [EzConfig]
+  final bool inherit;
+  final Color? color;
+  final Color? backgroundColor;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final FontStyle? fontStyle;
+  final double? letterSpacing;
+
+  /// Default: 1.25
+  final double wordSpacing;
+
+  final TextBaseline? textBaseline;
+
+  /// Default: 1.5
+  final double height;
+
+  final TextLeadingDistribution? leadingDistribution;
+  final Locale? locale;
+  final Paint? foreground;
+  final Paint? background;
+  final List<Shadow>? shadows;
+  final List<FontFeature>? fontFeatures;
+  final List<FontVariation>? fontVariations;
+  final TextDecoration? decoration;
+  final Color? decorationColor;
+  final TextDecorationStyle? decorationStyle;
+  final double? decorationThickness;
+  final String? debugLabel;
+
+  /// Default: [gStyle] -> [EzConfig.prefs] -> [fontFamilyKey].fontFamily
+  final String? fontFamily;
+
+  final List<String>? fontFamilyFallback;
+  final String? package;
+  final TextOverflow? overflow;
+
+  /// Setup a [TextStyle] using the [fontFamilyKey] from [EzConfig.prefs]
+  /// And some custom spacing defaults for readability
   EzTextStyle({
-    bool inherit = true,
-    Color? color,
-    Color? backgroundColor,
-    double? fontSize,
-    FontWeight? fontWeight,
-    FontStyle? fontStyle,
-    double? letterSpacing,
-    double? wordSpacing,
-    TextBaseline? textBaseline,
-    double? height,
-    TextLeadingDistribution? leadingDistribution,
-    Locale? locale,
-    Paint? foreground,
-    Paint? background,
-    List<Shadow>? shadows,
-    List<FontFeature>? fontFeatures,
-    List<FontVariation>? fontVariations,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-    String? debugLabel,
-    String? fontFamily,
-    List<String>? fontFamilyFallback,
-    String? package,
-    TextOverflow? overflow,
+    this.inherit = true,
+    this.color,
+    this.backgroundColor,
+    this.fontSize,
+    this.fontWeight,
+    this.fontStyle,
+    this.letterSpacing,
+    this.wordSpacing = 1.25,
+    this.textBaseline,
+    this.height = 1.5,
+    this.leadingDistribution,
+    this.locale,
+    this.foreground,
+    this.background,
+    this.shadows,
+    this.fontFeatures,
+    this.fontVariations,
+    this.decoration,
+    this.decorationColor,
+    this.decorationStyle,
+    this.decorationThickness,
+    this.debugLabel,
+    this.fontFamily,
+    this.fontFamilyFallback,
+    this.package,
+    this.overflow,
   }) : super(
           inherit: inherit,
           color: color,
@@ -42,9 +79,9 @@ class EzTextStyle extends TextStyle {
           fontWeight: fontWeight,
           fontStyle: fontStyle,
           letterSpacing: letterSpacing,
-          wordSpacing: wordSpacing ?? 1.25,
+          wordSpacing: wordSpacing,
           textBaseline: textBaseline,
-          height: height ?? 1.5,
+          height: height,
           leadingDistribution: leadingDistribution,
           locale: locale,
           foreground: foreground,
