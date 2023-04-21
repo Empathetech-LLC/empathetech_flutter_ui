5dbd43c177fd2949828984f5381fab85c1479274
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-21 13:12:56 -0400

    expanded imports

--- lib/src/classes/EzCupertinoAppData.dart
+++ lib/src/classes/EzCupertinoAppData.dart
@@ -6,36 +6,64 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzCupertinoAppData extends CupertinoAppData {
+  final Key? widgetKey;
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final Widget? home;
+  final Map<String, Widget Function(BuildContext)>? routes;
+  final String? initialRoute;
+  final Route<dynamic>? Function(RouteSettings)? onGenerateRoute;
+  final Route<dynamic>? Function(RouteSettings)? onUnknownRoute;
+  final List<NavigatorObserver>? navigatorObservers;
+  final String? title;
+  final String Function(BuildContext)? onGenerateTitle;
+  final Color? color;
+  final Locale? locale;
+  final Map<LogicalKeySet, Intent>? shortcuts;
+  final Map<Type, Action<Intent>>? actions;
+  final List<Route<dynamic>> Function(String)? onGenerateInitialRoutes;
+  final Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates;
+  final Locale? Function(List<Locale>?, Iterable<Locale>)?
+      localeListResolutionCallback;
+  final Locale? Function(Locale?, Iterable<Locale>)? localeResolutionCallback;
+  final Iterable<Locale>? supportedLocales;
+  final bool? showPerformanceOverlay;
+  final bool? checkerboardRasterCacheImages;
+  final bool? checkerboardOffscreenLayers;
+  final bool? showSemanticsDebugger;
+  final bool? debugShowCheckedModeBanner;
+  final ScrollBehavior? scrollBehavior;
+  final bool? useInheritedMediaQuery;
+  final CupertinoThemeData? theme;
+
   /// [CupertinoAppData] wrapper that uses [ezCupertinoThemeData] by default
   EzCupertinoAppData({
-    Key? widgetKey,
-    GlobalKey<NavigatorState>? navigatorKey,
-    Widget? home,
-    Map<String, Widget Function(BuildContext)>? routes,
-    String? initialRoute,
-    Route<dynamic>? Function(RouteSettings)? onGenerateRoute,
-    Route<dynamic>? Function(RouteSettings)? onUnknownRoute,
-    List<NavigatorObserver>? navigatorObservers,
-    String? title,
-    String Function(BuildContext)? onGenerateTitle,
-    Color? color,
-    Locale? locale,
-    Map<LogicalKeySet, Intent>? shortcuts,
-    Map<Type, Action<Intent>>? actions,
-    List<Route<dynamic>> Function(String)? onGenerateInitialRoutes,
-    Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates,
-    Locale? Function(List<Locale>?, Iterable<Locale>)?
-        localeListResolutionCallback,
-    Locale? Function(Locale?, Iterable<Locale>)? localeResolutionCallback,
-    Iterable<Locale>? supportedLocales,
-    bool? showPerformanceOverlay,
-    bool? checkerboardRasterCacheImages,
-    bool? checkerboardOffscreenLayers,
-    bool? showSemanticsDebugger,
-    bool? debugShowCheckedModeBanner,
-    ScrollBehavior? scrollBehavior,
-    bool? useInheritedMediaQuery,
-    CupertinoThemeData? theme,
+    this.widgetKey,
+    this.navigatorKey,
+    this.home,
+    this.routes,
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onUnknownRoute,
+    this.navigatorObservers,
+    this.title,
+    this.onGenerateTitle,
+    this.color,
+    this.locale,
+    this.shortcuts,
+    this.actions,
+    this.onGenerateInitialRoutes,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales,
+    this.showPerformanceOverlay,
+    this.checkerboardRasterCacheImages,
+    this.checkerboardOffscreenLayers,
+    this.showSemanticsDebugger,
+    this.debugShowCheckedModeBanner,
+    this.scrollBehavior,
+    this.useInheritedMediaQuery,
+    this.theme,
   }) : super(
           widgetKey: widgetKey,
           navigatorKey: navigatorKey,
