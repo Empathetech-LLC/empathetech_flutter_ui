b89fe6f5296913a02c1f60d03098eb98c43e1a2f
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-22 07:38:09 -0400

    initial commit

---
+++ lib/src/classes/EzLeftySwitch.dart
@@ -0,0 +1,20 @@
+library empathetech_flutter_ui;
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+
+class EzLeftySwitch extends StatefulWidget {
+  /// Standardized tool for updating [EzConfig.dominantSide]
+  EzLeftySwitch({Key? key}) : super(key: key);
+
+  @override
+  _LeftySwitchState createState() => _LeftySwitchState();
+}
+
+class _LeftySwitchState extends State<EzLeftySwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/classes/EzThemeModeSwitch.dart
@@ -0,0 +1,20 @@
+library empathetech_flutter_ui;
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+
+class EzThemeModeSwitch extends StatefulWidget {
+  /// Standardized tool for optionally overwriting [ThemeMode.system] via [EzConfig]
+  EzThemeModeSwitch({Key? key}) : super(key: key);
+
+  @override
+  _ThemeModeSwitchState createState() => _ThemeModeSwitchState();
+}
+
+class _ThemeModeSwitchState extends State<EzThemeModeSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
