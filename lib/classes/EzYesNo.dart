6dff66c26c665f4ef27511e79e974d8cf1634935
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-26 11:45:25 -0400

    it's classified

--- lib/classes/EzYesNo.dart
+++ lib/classes/EzYesNo.dart
@@ -5,53 +5,64 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-Widget EzYesNo({
-  required BuildContext context,
-  required void Function() onConfirm,
-  required void Function() onDeny,
-  required Axis axis,
-  double? spacer,
-  String confirmMsg = 'Yes',
-  String denyMsg = 'No',
-  Icon? customConfirm,
-  Icon? customDeny,
-}) {
-  // Gather theme data
+class EzYesNo extends StatelessWidget {
+  final void Function() onConfirm;
+  final void Function() onDeny;
+  final Axis axis;
+  final double? spacer;
+  final String confirmMsg;
+  final String denyMsg;
+  final Icon? customConfirm;
+  final Icon? customDeny;
 
-  Icon confirmIcon = customConfirm ?? Icon(PlatformIcons(context).checkMark);
-  Icon denyIcon = customDeny ?? Icon(PlatformIcons(context).clear);
-  double spacing =
-      (spacer is double) ? spacer : EzConfig.prefs[buttonSpacingKey];
+  EzYesNo({
+    required this.onConfirm,
+    required this.onDeny,
+    required this.axis,
+    this.spacer,
+    this.confirmMsg = 'Yes',
+    this.denyMsg = 'No',
+    this.customConfirm,
+    this.customDeny,
+  });
 
-  return axis == Axis.vertical
-      ? Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton.icon(
-              onPressed: onConfirm,
-              icon: confirmIcon,
-              label: Text(confirmMsg),
-            ),
-            Container(height: spacing),
-            ElevatedButton.icon(
-              onPressed: onDeny,
-              icon: denyIcon,
-              label: Text(denyMsg),
-            ),
-          ],
-        )
-      : Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton.icon(
+  @override
+  Widget build(BuildContext context) {
+    Icon confirmIcon = customConfirm ?? Icon(PlatformIcons(context).checkMark);
+    Icon denyIcon = customDeny ?? Icon(PlatformIcons(context).clear);
+    double spacing =
+        (spacer is double) ? spacer : EzConfig.prefs[buttonSpacingKey];
+
+    return axis == Axis.vertical
+        ? Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton.icon(
                 onPressed: onConfirm,
                 icon: confirmIcon,
-                label: Text(confirmMsg)),
-            Container(width: spacing),
-            ElevatedButton.icon(
-                onPressed: onDeny, icon: denyIcon, label: Text(denyMsg)),
-          ],
-        );
+                label: Text(confirmMsg),
+              ),
+              Container(height: spacing),
+              ElevatedButton.icon(
+                onPressed: onDeny,
+                icon: denyIcon,
+                label: Text(denyMsg),
+              ),
+            ],
+          )
+        : Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: onConfirm,
+                  icon: confirmIcon,
+                  label: Text(confirmMsg)),
+              Container(width: spacing),
+              ElevatedButton.icon(
+                  onPressed: onDeny, icon: denyIcon, label: Text(denyMsg)),
+            ],
+          );
+  }
 }
