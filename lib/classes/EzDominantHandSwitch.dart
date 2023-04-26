ed7e173b0baa9d5828c48a8ca3ec33d1ce1bdc8e
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-04-26 12:26:51 -0400

    updated hand switch setting

---
+++ lib/classes/EzDominantHandSwitch.dart
@@ -0,0 +1,59 @@
+library empathetech_flutter_ui;
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class EzLeftySwitch extends StatefulWidget {
+  /// Standardized tool for updating [EzConfig.dominantSide]
+  EzLeftySwitch({Key? key}) : super(key: key);
+
+  @override
+  _LeftySwitchState createState() => _LeftySwitchState();
+}
+
+class _LeftySwitchState extends State<EzLeftySwitch> {
+  late TextStyle? style = headlineMedium(context);
+
+  List<DropdownMenuItem<Hand>> _handOptions() {
+    return [
+      DropdownMenuItem<Hand>(
+        child: Text('Right'),
+        value: Hand.right,
+      ),
+      DropdownMenuItem<Hand>(
+        child: Text('Left'),
+        value: Hand.left,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return EzScrollView(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      scrollDirection: Axis.horizontal,
+      children: [
+        // Label
+        ezText('Dominant hand', style: style),
+        Container(width: EzConfig.prefs[buttonSpacingKey]),
+
+        // Button
+        DropdownButton<Hand>(
+          value: EzConfig.dominantSide,
+          items: _handOptions(),
+          onChanged: (Hand? newDominantSide) {
+            if (newDominantSide != null) {
+              setState(() {
+                EzConfig.dominantSide =
+                    (newDominantSide == Hand.right) ? Hand.right : Hand.left;
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
