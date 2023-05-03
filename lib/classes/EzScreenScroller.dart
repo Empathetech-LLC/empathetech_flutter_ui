555e182b9e4886ecba1dc9454a7f1cdc7dddf40b
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-05-03 11:12:10 -0400

    removed column req

--- lib/classes/EzScreenScroller.dart
+++ lib/classes/EzScreenScroller.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
 class EzPageScroller extends SingleChildScrollView {
-  // SingleChildScrollView
-  final Key? viewKey;
   final EdgeInsetsGeometry? padding;
   final bool? primary;
   final ScrollPhysics? physics;
@@ -14,21 +12,9 @@ class EzPageScroller extends SingleChildScrollView {
   final String? restorationId;
   final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
 
-  // Column
-  final Key? columnKey;
-  final MainAxisAlignment mainAxisAlignment;
-  final MainAxisSize mainAxisSize;
-  final CrossAxisAlignment crossAxisAlignment;
-  final TextDirection? textDirection;
-  final VerticalDirection verticalDirection;
-  final TextBaseline? textBaseline;
-  final List<Widget> children;
-
-  /// Wraps a vertical [SingleChildScrollView] Widget with a [Column] child that holds [children]
-  /// The vertical view is wrapped in a horizontal [SingleChildScrollView] to allow for bi-directional scrolling
+  /// [SingleChildScrollView] inside another [SingleChildScrollView]
+  /// [Axis.horizontal] then [Axis.vertical]
   const EzPageScroller({
-    // SingleChildScrollView
-    this.viewKey,
     this.padding,
     this.primary,
     this.physics,
@@ -36,22 +22,11 @@ class EzPageScroller extends SingleChildScrollView {
     this.clipBehavior = Clip.hardEdge,
     this.restorationId,
     this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
-
-    // Column
-    this.columnKey,
-    this.mainAxisAlignment = MainAxisAlignment.center,
-    this.mainAxisSize = MainAxisSize.max,
-    this.crossAxisAlignment = CrossAxisAlignment.center,
-    this.textDirection,
-    this.verticalDirection = VerticalDirection.down,
-    this.textBaseline,
-    required this.children,
   });
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      key: viewKey,
       padding: padding,
       primary: primary,
       physics: physics,
@@ -61,7 +36,6 @@ class EzPageScroller extends SingleChildScrollView {
       keyboardDismissBehavior: keyboardDismissBehavior,
       scrollDirection: Axis.horizontal,
       child: SingleChildScrollView(
-        key: key,
         padding: padding,
         primary: primary,
         physics: physics,
@@ -70,16 +44,7 @@ class EzPageScroller extends SingleChildScrollView {
         restorationId: restorationId,
         keyboardDismissBehavior: keyboardDismissBehavior,
         scrollDirection: Axis.vertical,
-        child: Column(
-          key: columnKey,
-          mainAxisAlignment: mainAxisAlignment,
-          mainAxisSize: mainAxisSize,
-          crossAxisAlignment: crossAxisAlignment,
-          textDirection: textDirection,
-          verticalDirection: verticalDirection,
-          textBaseline: textBaseline,
-          children: children,
-        ),
+        child: child,
       ),
     );
   }
