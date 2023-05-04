56c3725d3192cc7c3c66e3ef20e606c86a1b0b3a
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-05-04 07:48:40 -0400

    using 2 ez scroll views

--- lib/classes/EzScreenScroller.dart
+++ lib/classes/EzScreenScroller.dart
@@ -5,7 +5,7 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
-class EzScreenScroller extends SingleChildScrollView {
+class EzScreenScroller extends StatelessWidget {
   final Key? key;
   final ScrollPhysics physics;
   final Clip clipBehavior;
@@ -15,7 +15,7 @@ class EzScreenScroller extends SingleChildScrollView {
   final CrossAxisAlignment crossAxisAlignment;
   final List<Widget> children;
 
-  /// Wraps a vertical [EzScrollView] in a horizontal [SingleChildScrollView]
+  /// Wraps a vertical [EzScrollView] in a horizontal [EzScrollView]
   const EzScreenScroller({
     this.key,
     this.physics = const BouncingScrollPhysics(),
@@ -25,19 +25,27 @@ class EzScreenScroller extends SingleChildScrollView {
     this.mainAxisAlignment = MainAxisAlignment.start,
     this.crossAxisAlignment = CrossAxisAlignment.center,
     required this.children,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return EzScrollView(
+      key: key,
       scrollDirection: Axis.horizontal,
+      primary: false,
+      reverseHands: false,
       reverse: false,
       physics: physics,
       clipBehavior: clipBehavior,
       dragStartBehavior: dragStartBehavior,
       keyboardDismissBehavior: keyboardDismissBehavior,
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      verticalDirection: VerticalDirection.down,
       child: EzScrollView(
         scrollDirection: Axis.vertical,
+        primary: true,
         reverseHands: false,
         reverse: false,
         physics: physics,
