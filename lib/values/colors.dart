db1ad45fd22aeb1e07beb50aca40ca41416a2e6d
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-05-04 10:02:07 -0400

    added more colors

--- lib/values/colors.dart
+++ lib/values/colors.dart
@@ -2,11 +2,17 @@ library empathetech_flutter_ui;
 
 import 'package:flutter/material.dart';
 
-const int EmpathEucalyptusCode = 0xFF20DAA5;
-const Color EmpathEucalyptus = Color(EmpathEucalyptusCode);
+const int EmpathEucalyptusHex = 0xFF20DAA5;
+const Color EmpathEucalyptus = Color(EmpathEucalyptusHex);
 
-const int EmpathPurpleCode = 0xFFA520DA;
-const Color EmpathPurple = Color(EmpathPurpleCode);
+const int EmpathPurpleHex = 0xFFA520DA;
+const Color EmpathPurple = Color(EmpathPurpleHex);
 
-const int EmpathGoldenrodCode = 0xFFDAA520;
-const Color EmpathGoldenrod = Color(EmpathGoldenrodCode);
+const int EmpathGoldenrodHex = 0xFFDAA520;
+const Color EmpathGoldenrod = Color(EmpathGoldenrodHex);
+
+const int whiteHex = 0xFFFFFF;
+const int offWhiteHex = 0xFFF5F5F5;
+
+const int blackHex = 0xFF000000;
+const int offBlackHex = 0xFF0A0A0A;
