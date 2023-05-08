edd2b538db7d6d56ccb703b5ee0d5fa1245ec323
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-05-08 11:00:22 -0400

    using adaptive theme

--- lib/classes/EzApp.dart
+++ lib/classes/EzApp.dart
@@ -3,24 +3,38 @@ library empathetech_flutter_ui;
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-class EzApp extends PlatformApp {
+class EzApp extends StatelessWidget {
+  final Key? key;
   final String title;
   final Widget homeScreenWidget;
+  final AdaptiveThemeMode? initialTheme;
 
   /// [PlatformApp] wrapper with [EzConfig] theming
   EzApp({
+    this.key,
     required this.title,
     required this.homeScreenWidget,
-  }) : super(
-          title: title,
-          debugShowCheckedModeBanner: false,
-          routes: {'/': (context) => homeScreenWidget},
-          material: (context, platform) => MaterialAppData(
-            theme: ezThemeData(light: true),
-            darkTheme: ezThemeData(light: false),
-            themeMode: EzConfig.instance.themeMode,
-          ),
-        );
+    this.initialTheme,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ezThemeData(light: true),
+      dark: ezThemeData(light: false),
+      initial: initialTheme ?? AdaptiveThemeMode.system,
+      builder: (theme, darkTheme) => PlatformApp(
+        title: title,
+        debugShowCheckedModeBanner: false,
+        routes: {'/': (context) => homeScreenWidget},
+        material: (context, platform) => MaterialAppData(
+          theme: theme,
+          darkTheme: darkTheme,
+        ),
+      ),
+    );
+  }
 }
