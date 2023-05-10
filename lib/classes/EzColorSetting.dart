29fc120dd8d4bf82380098ba3fcd2270f8eb796b
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-05-10 08:31:32 -0400

    updated icon size

--- lib/classes/EzColorSetting.dart
+++ lib/classes/EzColorSetting.dart
@@ -186,6 +186,7 @@ class _ColorSettingState extends State<EzColorSetting> {
           child: Icon(
             PlatformIcons(context).edit,
             color: EzContrastColor(currColor),
+            size: diameter / 2,
           ),
           style: Theme.of(context).elevatedButtonTheme.style?.copyWith(
                 backgroundColor: MaterialStatePropertyAll(currColor),
