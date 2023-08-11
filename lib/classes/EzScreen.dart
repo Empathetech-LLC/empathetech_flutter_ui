a789cbc68471c7cfe8d53225062ab9f453e50af5
empathictech <105748722+empathictech@users.noreply.github.com>
2023-08-11 15:44:36 -0400

    added click and drag to go back

--- lib/classes/EzScreen.dart
+++ lib/classes/EzScreen.dart
@@ -22,6 +22,8 @@ class EzScreen extends StatelessWidget {
   final Clip clipBehavior;
 
   /// [Container] wrapper that defaults to max size with a margin from [EzConfig]
+  /// The [Container] is also wrapped in a [GestureDetector] for faking trackpad support
+  /// Flutter has no easy way of implementing trackpad swipes, so a click and drag to the right will pop the stack
   const EzScreen({
     // Container
     this.key,
@@ -45,20 +47,28 @@ class EzScreen extends StatelessWidget {
     final EdgeInsetsGeometry screenMargin =
         margin ?? EdgeInsets.all(EzConfig.instance.prefs[marginKey]);
 
-    return Container(
-      key: key,
-      alignment: alignment,
-      padding: padding,
-      decoration: decoration,
-      foregroundDecoration: foregroundDecoration,
-      width: width,
-      height: height,
-      constraints: constraints,
-      margin: screenMargin,
-      transform: transform,
-      transformAlignment: transformAlignment,
-      child: child,
-      clipBehavior: clipBehavior,
+    return GestureDetector(
+      onPanUpdate: (details) {
+        // "Swipe" right
+        if (details.delta.dx > 0) {
+          popScreen(context: context);
+        }
+      },
+      child: Container(
+        key: key,
+        alignment: alignment,
+        padding: padding,
+        decoration: decoration,
+        foregroundDecoration: foregroundDecoration,
+        width: width,
+        height: height,
+        constraints: constraints,
+        margin: screenMargin,
+        transform: transform,
+        transformAlignment: transformAlignment,
+        child: child,
+        clipBehavior: clipBehavior,
+      ),
     );
   }
 }
