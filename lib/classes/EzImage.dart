23f1f46711ee468ba6dde7a601a9353ada3d1cb5
empathictech <105748722+empathictech@users.noreply.github.com>
2023-08-11 16:23:07 -0400

    added image wrapper

--- lib/classes/EzImage.dart
+++ lib/classes/EzImage.dart
@@ -2,14 +2,13 @@ library empathetech_flutter_ui;
 
 import 'package:flutter/material.dart';
 
-class EzImage extends StatelessWidget {
-  final String label;
+class EzImage extends Image {
+  final Key? key;
   final ImageProvider<Object> image;
   final Widget Function(BuildContext, Widget, int?, bool)? frameBuilder;
   final Widget Function(BuildContext, Widget, ImageChunkEvent?)? loadingBuilder;
   final Widget Function(BuildContext, Object, StackTrace?)? errorBuilder;
-  final String? semanticLabel;
-  final bool excludeFromSemantics;
+  final String semanticLabel;
   final double? width;
   final double? height;
   final Color? color;
@@ -24,56 +23,47 @@ class EzImage extends StatelessWidget {
   final bool isAntiAlias;
   final FilterQuality filterQuality;
 
-  /// Image wrapped in a [Semantics] layer for encforcing accessibility
+  /// [Image] that requires a [semanticLabel] for encforcing accessibility
   EzImage({
-    required this.label,
+    this.key,
     required this.image,
     this.frameBuilder,
     this.loadingBuilder,
     this.errorBuilder,
-    this.semanticLabel,
-    required this.excludeFromSemantics,
+    required this.semanticLabel,
     this.width,
     this.height,
     this.color,
     this.opacity,
     this.colorBlendMode,
     this.fit,
-    required this.alignment,
-    required this.repeat,
+    this.alignment = Alignment.center,
+    this.repeat = ImageRepeat.noRepeat,
     this.centerSlice,
-    required this.matchTextDirection,
-    required this.gaplessPlayback,
-    required this.isAntiAlias,
-    required this.filterQuality,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Semantics(
-      label: label,
-      child: Image(
-        image: image,
-        key: key,
-        frameBuilder: frameBuilder,
-        loadingBuilder: loadingBuilder,
-        errorBuilder: errorBuilder,
-        semanticLabel: semanticLabel,
-        excludeFromSemantics: excludeFromSemantics,
-        width: width,
-        height: height,
-        color: color,
-        opacity: opacity,
-        colorBlendMode: colorBlendMode,
-        fit: fit,
-        alignment: alignment,
-        repeat: repeat,
-        centerSlice: centerSlice,
-        matchTextDirection: matchTextDirection,
-        gaplessPlayback: gaplessPlayback,
-        isAntiAlias: isAntiAlias,
-        filterQuality: filterQuality,
-      ),
-    );
-  }
+    this.matchTextDirection = false,
+    this.gaplessPlayback = false,
+    this.isAntiAlias = false,
+    this.filterQuality = FilterQuality.low,
+  }) : super(
+          key: key,
+          image: image,
+          frameBuilder: frameBuilder,
+          loadingBuilder: loadingBuilder,
+          errorBuilder: errorBuilder,
+          semanticLabel: semanticLabel,
+          excludeFromSemantics: false,
+          width: width,
+          height: height,
+          color: color,
+          opacity: opacity,
+          colorBlendMode: colorBlendMode,
+          fit: fit,
+          alignment: alignment,
+          repeat: repeat,
+          centerSlice: centerSlice,
+          matchTextDirection: matchTextDirection,
+          gaplessPlayback: gaplessPlayback,
+          isAntiAlias: isAntiAlias,
+          filterQuality: filterQuality,
+        );
 }
