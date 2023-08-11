aed1f1a889599258eff5abee4a0909e81480a7da
empathictech <105748722+empathictech@users.noreply.github.com>
2023-08-11 13:01:23 -0400

    comments

--- lib/classes/EzWebLink.dart
+++ lib/classes/EzWebLink.dart
@@ -1,18 +1,22 @@
 library empathetech_flutter_ui;
 
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class EzWebLink extends TextSpan {
-  /// Use with [EzWebLink.onTap] to setup a quick link text span
+  /// Creates a [TextSpan] with an external link via [TapGestureRecognizer] && [launchUrl]
+  /// See [EzLink] for making internal links
+  /// --- WARNING! ---
+  /// Unfortunately, the context (right-click) menu will not work as expected here
+  /// Current theory: the [TextSpan] clobbers it, but...
+  /// Empathetech chose to prioritize selectable text >> context menus
   EzWebLink({
     required String text,
     required Uri url,
     TextStyle? style,
-    MouseCursor? mouseCursor,
-    void Function(PointerEnterEvent)? onEnter,
-    void Function(PointerExitEvent)? onExit,
   }) : super(
           text: text,
           recognizer: new TapGestureRecognizer()..onTap = () => launchUrl(url),
