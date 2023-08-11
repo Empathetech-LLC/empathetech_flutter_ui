ffd9bb71725b420e2d97405e5c61eccfdb501497
empathictech <105748722+empathictech@users.noreply.github.com>
2023-08-11 13:09:16 -0400

    removed ez screen

--- lib/empathetech_flutter_ui.dart
+++ lib/empathetech_flutter_ui.dart
@@ -15,7 +15,6 @@ export 'classes/EzLink.dart';
 export 'classes/EzNotifications.dart';
 export 'classes/EzRow.dart';
 export 'classes/EzRowCol.dart';
-export 'classes/EzScreen.dart';
 export 'classes/EzScreenScroller.dart';
 export 'classes/EzScrollView.dart';
 export 'classes/EzSelectableText.dart';
