3247c2f002cb2e689fd9cb3c71f5153191229642
empathictech <105748722+empathictech@users.noreply.github.com>
2023-08-25 22:30:55 -0400

    removed un-needed library tags

--- lib/classes/EzDialog.dart
+++ lib/classes/EzDialog.dart
@@ -1,7 +1,6 @@
 /// empathetech_flutter_ui
 /// Copyright (c) 2023 Empathetech LLC. All rights reserved.
 /// See LICENSE for distribution and usage details.
-library empathetech_flutter_ui;
 
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
