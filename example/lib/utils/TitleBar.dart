4b180607801321165e0db2af1b4d2c0d4a08cb23
empathictech <105748722+empathictech@users.noreply.github.com>
2023-09-11 17:39:45 -0400

    initial commit

---
+++ example/lib/utils/TitleBar.dart
@@ -0,0 +1,61 @@
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class TitleBar extends StatelessWidget {
+  final GlobalKey? key;
+
+  /// [TextStyle] to use on the links' text
+  final TextStyle? style;
+
+  /// Pass in the value from MediaQuery.of(context).textScaleFactor
+  final double scalar;
+
+  /// How much distance should be between the links
+  final double spacer;
+
+  /// Internal page links to put in the [AppBar]
+  const TitleBar({
+    this.key,
+    required this.style,
+    required this.scalar,
+    required this.spacer,
+  }) : super(key: key);
+
+  double get width {
+    double total = spacer;
+
+    total += measureText(
+      text: 'Have fun!',
+      scalar: scalar,
+      style: style,
+    ).width;
+
+    return total + spacer;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final EzSelectableText title = EzSelectableText.rich(
+      EzLink(
+        text: 'Have fun!',
+        style: style,
+        action: () => context.goNamed('settings'),
+        semanticsLabel: 'Return to the home screen',
+      ),
+    );
+
+    // Return build //
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        EzSpacer.row(spacer),
+        title,
+        EzSpacer.row(spacer),
+      ],
+    );
+  }
+}
