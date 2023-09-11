8bf74b8b56d5db7fa85189b72a219fa2ce59b8d1
empathictech <105748722+empathictech@users.noreply.github.com>
2023-09-11 17:07:44 -0400

    copied settings from dotnet

---
+++ example/lib/screens/color-settings.dart
@@ -0,0 +1,145 @@
+import '../utils/utils.dart';
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class ColorSettingsScreen extends StatefulWidget {
+  const ColorSettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ColorSettingsScreenState createState() => _ColorSettingsScreenState();
+}
+
+class _ColorSettingsScreenState extends State<ColorSettingsScreen> {
+  // Set page/tab title //
+
+  @override
+  void initState() {
+    super.initState();
+    setPageTitle(context: context, title: 'Color settings');
+  }
+
+  // Gather theme data //
+
+  late bool isLight = !PlatformTheme.of(context)!.isDark;
+
+  final double paragraphSpacer = EzConfig.instance.prefs[paragraphSpacingKey];
+  final double buttonSpacer = EzConfig.instance.prefs[buttonSpacingKey];
+  final double diameter = EzConfig.instance.prefs[circleDiameterKey];
+
+  // Build page //
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      body: EzScreen(
+        child: EzScrollView(
+          children: [
+            // Title
+            EzSelectableText('Make it yours!', style: headlineSmall(context)),
+            EzSpacer(paragraphSpacer),
+
+            // Settings
+            EzScrollView(
+              scrollDirection: Axis.horizontal,
+              mainAxisSize: MainAxisSize.min,
+              primary: false,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Theme colors //
+
+                  // Base
+                  EzColorSetting(
+                    toControl: isLight ? lightThemeColorKey : darkThemeColorKey,
+                    message: 'Theme',
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Text
+                  EzColorSetting(
+                    toControl: isLight
+                        ? lightThemeTextColorKey
+                        : darkThemeTextColorKey,
+                    message: 'Theme text',
+                    textBackgroundKey:
+                        isLight ? lightThemeColorKey : darkThemeColorKey,
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Page colors //
+
+                  // Base
+                  EzColorSetting(
+                    toControl: isLight
+                        ? lightBackgroundColorKey
+                        : darkBackgroundColorKey,
+                    message: 'Page',
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Text
+                  EzColorSetting(
+                    toControl: isLight
+                        ? lightBackgroundTextColorKey
+                        : darkBackgroundTextColorKey,
+                    message: 'Page text',
+                    textBackgroundKey: isLight
+                        ? lightBackgroundColorKey
+                        : darkBackgroundColorKey,
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Button colors //
+
+                  // Base
+                  EzColorSetting(
+                    toControl:
+                        isLight ? lightButtonColorKey : darkButtonColorKey,
+                    message: 'Buttons',
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Text
+                  EzColorSetting(
+                    toControl: isLight
+                        ? lightButtonTextColorKey
+                        : darkButtonTextColorKey,
+                    message: 'Button text',
+                    textBackgroundKey:
+                        isLight ? lightButtonColorKey : darkButtonColorKey,
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Accent colors //
+
+                  // Base
+                  EzColorSetting(
+                    toControl:
+                        isLight ? lightAccentColorKey : darkAccentColorKey,
+                    message: 'Accent',
+                  ),
+                  EzSpacer(buttonSpacer),
+
+                  // Text
+                  EzColorSetting(
+                    toControl: isLight
+                        ? lightAccentTextColorKey
+                        : darkAccentTextColorKey,
+                    message: 'Accent text',
+                    textBackgroundKey:
+                        isLight ? lightAccentColorKey : darkAccentColorKey,
+                  ),
+                ],
+              ),
+            ),
+            EzSpacer(buttonSpacer),
+          ],
+        ),
+      ),
+      fab: const BackFAB(),
+    );
+  }
+}
---
+++ example/lib/screens/style-settings.dart
@@ -0,0 +1,105 @@
+import '../utils/utils.dart';
+
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+
+class StyleSettingsScreen extends StatefulWidget {
+  const StyleSettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  _StyleSettingsScreenState createState() => _StyleSettingsScreenState();
+}
+
+class _StyleSettingsScreenState extends State<StyleSettingsScreen> {
+  // Set page/tab title //
+
+  @override
+  void initState() {
+    super.initState();
+    setPageTitle(context: context, title: 'Style settings');
+  }
+
+  // Gather theme data //
+
+  final double paragraphSpacer = EzConfig.instance.prefs[paragraphSpacingKey];
+  final double buttonSpacer = EzConfig.instance.prefs[buttonSpacingKey];
+  final double diameter = EzConfig.instance.prefs[circleDiameterKey];
+
+  // Build page //
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      body: EzScreen(
+        child: EzScrollView(
+          children: [
+            // Title
+            EzSelectableText('Make it yours!', style: headlineSmall(context)),
+            EzSpacer(paragraphSpacer),
+
+            // Font
+            const EzFontSetting(),
+            EzSpacer(buttonSpacer),
+
+            // Margin
+            const EzSliderSetting(
+              prefsKey: marginKey,
+              type: SettingType.margin,
+              title: 'Margin',
+              min: 5.0,
+              max: 50.0,
+              steps: 18,
+            ),
+            EzSpacer(buttonSpacer),
+
+            // Padding
+            const EzSliderSetting(
+              prefsKey: paddingKey,
+              type: SettingType.padding,
+              title: 'Padding',
+              min: 0.0,
+              max: 50.0,
+              steps: 20,
+            ),
+            EzSpacer(buttonSpacer),
+
+            // Circle button size
+            const EzSliderSetting(
+              prefsKey: circleDiameterKey,
+              type: SettingType.circleSize,
+              title: 'Circle button size',
+              min: 30,
+              max: 100,
+              steps: 14,
+            ),
+            EzSpacer(buttonSpacer),
+
+            // Button spacing
+            const EzSliderSetting(
+              prefsKey: buttonSpacingKey,
+              type: SettingType.buttonSpacing,
+              title: 'Button spacing',
+              min: 10.0,
+              max: 100.0,
+              steps: 18,
+            ),
+            EzSpacer(buttonSpacer),
+
+            // Paragraph spacing
+            const EzSliderSetting(
+              prefsKey: paragraphSpacingKey,
+              type: SettingType.paragraphSpacing,
+              title: 'Paragraph spacing',
+              min: 10.0,
+              max: 100.0,
+              steps: 18,
+            ),
+            EzSpacer(buttonSpacer),
+          ],
+        ),
+      ),
+      fab: const BackFAB(),
+    );
+  }
+}
