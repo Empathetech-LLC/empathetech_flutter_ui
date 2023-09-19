ee69874c5b3e845aeafeb9d1d665c4d139daa6c3
empathictech <105748722+empathictech@users.noreply.github.com>
2023-09-19 11:03:13 -0400

    removed unused

--- lib/src/classes/EzAppProvider.dart
+++ lib/src/classes/EzAppProvider.dart
@@ -6,7 +6,6 @@
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzAppProvider extends StatelessWidget {
@@ -45,29 +44,6 @@ class EzAppProvider extends StatelessWidget {
             ? ThemeMode.light
             : ThemeMode.dark;
 
-    final CupertinoThemeData cupertinoLight =
-        MaterialBasedCupertinoThemeData(materialTheme: materialLight);
-
-    // Cupertino Dark requires some customization
-    // Guide taken from...
-    // https://github.com/stryder-dev/flutter_platform_widgets/wiki/Theming
-    const darkDefaultCupertinoTheme = CupertinoThemeData(brightness: Brightness.dark);
-
-    final CupertinoThemeData cupertinoDark = MaterialBasedCupertinoThemeData(
-      materialTheme: materialDark.copyWith(
-        cupertinoOverrideTheme: CupertinoThemeData(
-          brightness: Brightness.dark,
-          barBackgroundColor: darkDefaultCupertinoTheme.barBackgroundColor,
-          textTheme: CupertinoTextThemeData(
-            navActionTextStyle: darkDefaultCupertinoTheme.textTheme.navActionTextStyle
-                .copyWith(color: const Color(0xF0F9F9F9)),
-            navLargeTitleTextStyle: darkDefaultCupertinoTheme.textTheme.navLargeTitleTextStyle
-                .copyWith(color: const Color(0xF0F9F9F9)),
-          ),
-        ),
-      ),
-    );
-
     return PlatformProvider(
       key: key,
       settings: PlatformSettingsData(
@@ -78,8 +54,8 @@ class EzAppProvider extends StatelessWidget {
         themeMode: initialTheme,
         materialLightTheme: materialLight,
         materialDarkTheme: materialDark,
-        cupertinoLightTheme: cupertinoLight,
-        cupertinoDarkTheme: cupertinoDark,
+        cupertinoLightTheme: MaterialBasedCupertinoThemeData(materialTheme: materialLight),
+        cupertinoDarkTheme: MaterialBasedCupertinoThemeData(materialTheme: materialDark),
         matchCupertinoSystemChromeBrightness: true,
         builder: (context) => app,
       ),
