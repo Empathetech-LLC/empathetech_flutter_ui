fa8ef44e7f7979ce441acda205f32af836edf84a
empathictech <105748722+empathictech@users.noreply.github.com>
2023-09-25 10:32:23 -0400

    removed semantics label (hint is better)

--- lib/src/classes/EzWebLink.dart
+++ lib/src/classes/EzWebLink.dart
@@ -3,6 +3,8 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -15,17 +17,14 @@ class EzWebLink extends TextSpan {
   /// Unfortunately, the context (right-click) menu will not work as expected here
   /// Current theory: the [TextSpan] clobbers it, but...
   /// Empathetech chose to prioritize selectable text >> context menus
-  ///
-  /// Requires [semanticsLabel] to enforce accessibility
+
   EzWebLink({
     required String text,
     required Uri url,
     TextStyle? style,
-    required String semanticsLabel,
   }) : super(
           text: text,
           recognizer: new TapGestureRecognizer()..onTap = () => launchUrl(url),
           style: style,
-          semanticsLabel: semanticsLabel,
         );
 }
