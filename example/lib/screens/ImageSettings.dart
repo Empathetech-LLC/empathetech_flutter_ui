c723f52873d414c02d7ed46ab78ea2fe93772549
empathictech <105748722+empathictech@users.noreply.github.com>
2023-10-02 15:30:23 -0400

    added light/dark

--- example/lib/screens/ImageSettings.dart
+++ example/lib/screens/ImageSettings.dart
@@ -3,6 +3,7 @@ import '../utils/utils.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class ImageSettingsScreen extends StatefulWidget {
   const ImageSettingsScreen({Key? key}) : super(key: key);
@@ -22,28 +23,85 @@ class _ImageSettingsScreenState extends State<ImageSettingsScreen> {
 
   // Gather theme data //
 
+  late bool isLight = !PlatformTheme.of(context)!.isDark;
+
   final double textSpacer = EzConfig.instance.prefs[textSpacingKey];
   final double buttonSpacer = EzConfig.instance.prefs[buttonSpacingKey];
-  final double diameter = EzConfig.instance.prefs[circleDiameterKey];
+
+  late final TextStyle? resetLinkStyle =
+      bodyLarge(context)?.copyWith(decoration: TextDecoration.underline);
 
   // Build page //
 
   @override
   Widget build(BuildContext context) {
+    final String themeProfile = isLight ? 'Light' : 'Dark';
+    final String resetMessage = "Reset all $themeProfile images?";
+
     return ExampleScaffold(
       body: EzScreen(
         child: EzScrollView(
           children: [
-            // Page
-            const EzImageSetting(
-              prefsKey: pageImageKey,
-              title: 'Page',
-              allowClear: true,
-              fullscreen: true,
-              credits: 'Wherever you got it!',
-              semantics: 'Page background image',
+            // Current theme mode reminder
+            EzSelectableText(
+              'Editing: $themeProfile theme',
+              style: titleSmall(context),
             ),
-            EzSpacer(buttonSpacer),
+            EzSpacer(textSpacer),
+
+            // Settings //
+
+            // Nested in a horizontal scroll view in case the screen doesn't have enough horizontal space
+            EzScrollView(
+              scrollDirection: Axis.horizontal,
+              mainAxisSize: MainAxisSize.min,
+              primary: false,
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: isLight
+                      ? // Editing light theme //
+                      [
+                          // Page
+                          const EzImageSetting(
+                            prefsKey: lightPageImageKey,
+                            title: 'Page',
+                            allowClear: true,
+                            fullscreen: true,
+                            credits: 'Wherever you got it!',
+                            semantics: 'Page background image',
+                          ),
+                          EzSpacer(buttonSpacer),
+                        ]
+                      : // Editing dark theme //
+                      [
+                          // Page
+                          const EzImageSetting(
+                            prefsKey: darkPageImageKey,
+                            title: 'Page',
+                            allowClear: true,
+                            fullscreen: true,
+                            credits: 'Wherever you got it!',
+                            semantics: 'Page background image',
+                          ),
+                          EzSpacer(buttonSpacer),
+                        ]),
+            ),
+
+            // Local reset "all"
+            EzResetButton(
+              context: context,
+              hint: resetMessage,
+              style: resetLinkStyle,
+              dialogTitle: resetMessage,
+              onConfirm: () {
+                if (isLight) {
+                  EzConfig.instance.preferences.remove(lightPageImageKey);
+                } else {
+                  EzConfig.instance.preferences.remove(darkPageImageKey);
+                }
+              },
+            ),
+            EzSpacer(textSpacer),
           ],
         ),
       ),
