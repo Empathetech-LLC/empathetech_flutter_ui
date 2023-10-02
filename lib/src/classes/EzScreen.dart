5d9059d9f5728f8c43ce82af7f20cedc640d2f9f
empathictech <105748722+empathictech@users.noreply.github.com>
2023-10-02 19:55:28 -0400

    added decoration images

--- lib/src/classes/EzScreen.dart
+++ lib/src/classes/EzScreen.dart
@@ -5,6 +5,7 @@
 
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 class EzScreen extends StatelessWidget {
@@ -12,7 +13,14 @@ class EzScreen extends StatelessWidget {
   final AlignmentGeometry? alignment;
   final EdgeInsetsGeometry? padding;
   final Color? color;
+
+  /// See the custom parameter [decorationImageKey] before providing this
   final Decoration? decoration;
+
+  /// Optional [EzConfig.instance] key that will be used to create a [DecorationImage] for a [BoxDecoration] that will cover the [EzScreen]
+  /// Only provide this OR [decoration]
+  final String? decorationImageKey;
+
   final Decoration? foregroundDecoration;
   final double? width;
   final double? height;
@@ -31,6 +39,7 @@ class EzScreen extends StatelessWidget {
     this.padding,
     this.color,
     this.decoration,
+    this.decorationImageKey,
     this.foregroundDecoration,
     this.width = double.infinity,
     this.height = double.infinity,
@@ -40,7 +49,24 @@ class EzScreen extends StatelessWidget {
     this.transformAlignment,
     required this.child,
     this.clipBehavior = Clip.none,
-  });
+  }) : assert(
+          !(decoration != null && decorationImageKey != null),
+          'Either decoration or decorationImageKey can be provided, but not both.',
+        );
+
+  DecorationImage? _buildDecoration(String? path) {
+    return (path == null || path == noImageKey)
+        ? null
+        : isAsset(path)
+            ? DecorationImage(
+                image: AssetImage(path),
+                fit: BoxFit.fill,
+              )
+            : DecorationImage(
+                image: FileImage(File(path)),
+                fit: BoxFit.fill,
+              );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +77,9 @@ class EzScreen extends StatelessWidget {
       key: key,
       alignment: alignment,
       padding: padding,
-      decoration: decoration,
+      decoration: decorationImageKey == null
+          ? BoxDecoration(image: _buildDecoration(EzConfig.instance.prefs[decorationImageKey]))
+          : decoration,
       foregroundDecoration: foregroundDecoration,
       width: width,
       height: height,
