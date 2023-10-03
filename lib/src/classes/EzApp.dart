9e77f32ea28aabb4ce1cdd43be46987894a0f943
empathictech <105748722+empathictech@users.noreply.github.com>
2023-10-03 13:28:45 -0400

    saving progress

--- lib/src/classes/EzApp.dart
+++ lib/src/classes/EzApp.dart
@@ -7,121 +7,175 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzApp extends PlatformApp {
   final Key? key;
   final Key? widgetKey;
-
-  /// App title
-  final String title;
-
-  /// Optionally paint a debug banner on all screens
-  final bool? debugShowCheckedModeBanner;
-
-  /// Starting screen for mobile and desktop apps
-  /// Provide [routerConfig] OR [homeScreenWidget]
-  final Widget? homeScreenWidget;
-
-  /// [GoRouter] config for apps with a web deployment
-  /// Provide [routerConfig] OR [homeScreenWidget]
-  final GoRouter? routerConfig;
-
-  /// Used for internationalizing your app
-  final Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates;
-
-  /// Used for internationalizing your app
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final Widget? home;
+  final Map<String, WidgetBuilder>? routes;
+  final String? initialRoute;
+  final RouteFactory? onGenerateRoute;
+  final RouteFactory? onUnknownRoute;
+  final List<NavigatorObserver>? navigatorObservers;
+  final TransitionBuilder? builder;
+  final String? title;
+  final GenerateAppTitle? onGenerateTitle;
+  final Color? color;
   final Locale? locale;
-
-  /// Used for internationalizing your app
+  final Iterable<LocalizationsDelegate>? localizationsDelegates;
   final LocaleListResolutionCallback? localeListResolutionCallback;
-
-  /// Used for internationalizing your app
   final LocaleResolutionCallback? localeResolutionCallback;
-
-  /// Used for internationalizing your app
   final Iterable<Locale>? supportedLocales;
-
   final bool? showPerformanceOverlay;
   final bool? checkerboardRasterCacheImages;
   final bool? checkerboardOffscreenLayers;
+  final bool? showSemanticsDebugger;
+  final bool? debugShowCheckedModeBanner;
   final Map<LogicalKeySet, Intent>? shortcuts;
   final Map<Type, Action<Intent>>? actions;
+  final InitialRouteListFactory? onGenerateInitialRoutes;
   final String? restorationScopeId;
+  final ScrollBehavior? scrollBehavior;
+  final bool? useInheritedMediaQuery;
+  final PlatformBuilder<MaterialAppData>? material;
+  final PlatformBuilder<CupertinoAppData>? cupertino;
 
   /// [PlatformApp] wrapper with some recommended defaults
-  /// Automatically returns a [PlatformApp.router] when [routerConfig] is provided
-  /// [routerConfig] enable deep linking (ideal for web apss)
-  /// Otherwise, provide a [homeScreenWidget]
   /// Pair with [EzAppProvider] to enable [EzConfig] theming!
-  EzApp({
+  const EzApp({
     this.key,
     this.widgetKey,
-    required this.title,
-    this.debugShowCheckedModeBanner = false,
-    this.homeScreenWidget,
-    this.routerConfig,
-    this.localizationsDelegates,
+    this.navigatorKey,
+    this.home,
+    this.routes,
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onUnknownRoute,
+    this.navigatorObservers,
+    this.builder,
+    this.title,
+    this.onGenerateTitle,
+    this.color,
     this.locale,
+    this.localizationsDelegates,
     this.localeListResolutionCallback,
     this.localeResolutionCallback,
     this.supportedLocales,
     this.showPerformanceOverlay,
     this.checkerboardRasterCacheImages,
     this.checkerboardOffscreenLayers,
+    this.showSemanticsDebugger,
+    this.debugShowCheckedModeBanner = false,
+    this.shortcuts,
+    this.actions,
+    this.onGenerateInitialRoutes,
+    this.restorationScopeId,
+    this.scrollBehavior,
+    this.useInheritedMediaQuery,
+    this.material,
+    this.cupertino,
+  }) : super(
+          key: key,
+          widgetKey: widgetKey,
+          navigatorKey: navigatorKey,
+          home: home,
+          routes: routes,
+          initialRoute: initialRoute,
+          onGenerateRoute: onGenerateRoute,
+          onUnknownRoute: onUnknownRoute,
+          navigatorObservers: navigatorObservers,
+          builder: builder,
+          title: title,
+          onGenerateTitle: onGenerateTitle,
+          color: color,
+          locale: locale,
+          localizationsDelegates: localizationsDelegates ?? defaultLocalizations,
+          localeListResolutionCallback: localeListResolutionCallback,
+          localeResolutionCallback: localeResolutionCallback,
+          supportedLocales: supportedLocales,
+          showPerformanceOverlay: showPerformanceOverlay,
+          checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+          checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+          showSemanticsDebugger: showSemanticsDebugger,
+          debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+          shortcuts: shortcuts,
+          actions: actions,
+          onGenerateInitialRoutes: onGenerateInitialRoutes,
+          restorationScopeId: restorationScopeId,
+          scrollBehavior: scrollBehavior,
+          useInheritedMediaQuery: useInheritedMediaQuery,
+          material: material,
+          cupertino: cupertino,
+        );
+
+  /// [PlatformApp.router] wrapper with some recommended defaults
+  /// Pair with [EzAppProvider] to enable [EzConfig] theming!
+  const EzApp.router({
+    this.key,
+    RouteInformationProvider? routeInformationProvider,
+    RouteInformationParser<Object>? routeInformationParser,
+    RouterDelegate<Object>? routerDelegate,
+    RouterConfig<Object>? routerConfig,
+    BackButtonDispatcher? backButtonDispatcher,
+    this.widgetKey,
+    this.builder,
+    this.title,
+    this.onGenerateTitle,
+    this.color,
+    this.locale,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales,
+    this.showPerformanceOverlay,
+    this.checkerboardRasterCacheImages,
+    this.checkerboardOffscreenLayers,
+    this.showSemanticsDebugger,
+    this.debugShowCheckedModeBanner = false,
     this.shortcuts,
     this.actions,
     this.restorationScopeId,
-  }) : assert(routerConfig != null || homeScreenWidget != null);
-
-  @override
-  Widget build(BuildContext context) {
-    // Use default localizations if none were provided
-    final localizations = localizationsDelegates == null
-        ? <LocalizationsDelegate<dynamic>>[
-            DefaultMaterialLocalizations.delegate,
-            DefaultWidgetsLocalizations.delegate,
-            DefaultCupertinoLocalizations.delegate,
-          ]
-        : localizationsDelegates;
-
-    return (routerConfig == null)
-        ? PlatformApp(
-            key: key,
-            widgetKey: widgetKey,
-            debugShowCheckedModeBanner: debugShowCheckedModeBanner,
-            title: title,
-            home: homeScreenWidget,
-            localizationsDelegates: localizations,
-            locale: locale,
-            localeListResolutionCallback: localeListResolutionCallback,
-            localeResolutionCallback: localeResolutionCallback,
-            supportedLocales: supportedLocales,
-            showPerformanceOverlay: showPerformanceOverlay,
-            checkerboardRasterCacheImages: checkerboardRasterCacheImages,
-            checkerboardOffscreenLayers: checkerboardOffscreenLayers,
-            shortcuts: shortcuts,
-            actions: actions,
-            restorationScopeId: restorationScopeId,
-          )
-        : PlatformApp.router(
-            key: key,
-            widgetKey: widgetKey,
-            debugShowCheckedModeBanner: debugShowCheckedModeBanner,
-            title: title,
-            routerConfig: routerConfig,
-            localizationsDelegates: localizations,
-            locale: locale,
-            localeListResolutionCallback: localeListResolutionCallback,
-            localeResolutionCallback: localeResolutionCallback,
-            supportedLocales: supportedLocales,
-            showPerformanceOverlay: showPerformanceOverlay,
-            checkerboardRasterCacheImages: checkerboardRasterCacheImages,
-            checkerboardOffscreenLayers: checkerboardOffscreenLayers,
-            shortcuts: shortcuts,
-            actions: actions,
-            restorationScopeId: restorationScopeId,
-          );
-  }
+    this.scrollBehavior,
+    this.useInheritedMediaQuery,
+    PlatformBuilder<MaterialAppRouterData>? material,
+    PlatformBuilder<CupertinoAppRouterData>? cupertino,
+  }) : super.router(
+          key: key,
+          routeInformationProvider: routeInformationProvider,
+          routeInformationParser: routeInformationParser,
+          routerDelegate: routerDelegate,
+          routerConfig: routerConfig,
+          backButtonDispatcher: backButtonDispatcher,
+          widgetKey: widgetKey,
+          builder: builder,
+          title: title,
+          onGenerateTitle: onGenerateTitle,
+          color: color,
+          locale: locale,
+          localizationsDelegates: localizationsDelegates ?? defaultLocalizations,
+          localeListResolutionCallback: localeListResolutionCallback,
+          localeResolutionCallback: localeResolutionCallback,
+          supportedLocales: supportedLocales,
+          showPerformanceOverlay: showPerformanceOverlay,
+          checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+          checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+          showSemanticsDebugger: showSemanticsDebugger,
+          debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+          shortcuts: shortcuts,
+          actions: actions,
+          restorationScopeId: restorationScopeId,
+          scrollBehavior: scrollBehavior,
+          useInheritedMediaQuery: useInheritedMediaQuery,
+          material: material,
+          cupertino: cupertino,
+        );
 }
+
+const Iterable<LocalizationsDelegate<dynamic>> defaultLocalizations =
+    <LocalizationsDelegate<dynamic>>[
+  DefaultMaterialLocalizations.delegate,
+  DefaultWidgetsLocalizations.delegate,
+  DefaultCupertinoLocalizations.delegate,
+];
