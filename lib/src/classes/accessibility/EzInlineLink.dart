9336fab9493fe41b84cf7cb3dbba66d6f4ce37f9
empathictech <105748722+empathictech@users.noreply.github.com>
2023-10-20 17:41:52 -0400

    using widget span to mirror ez plain

--- lib/src/classes/accessibility/EzInlineLink.dart
+++ lib/src/classes/accessibility/EzInlineLink.dart
@@ -6,47 +6,41 @@
 import '../../../empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class EzInlineLink extends TextSpan {
+class EzInlineLink extends WidgetSpan {
   final String text;
   final TextStyle? style;
+  final TextAlign? textAlign;
   final void Function()? onTap;
   final Uri? url;
   final String? semanticsLabel;
-  final bool? spellOut;
-  final Locale? locale;
-  final MouseCursor mouseCursor;
-  final void Function(PointerEnterEvent)? onEnter;
-  final void Function(PointerExitEvent)? onExit;
+  final PlaceholderAlignment alignment;
+  final TextBaseline? basline;
 
-  /// [TextSpan] wrapper that opens either an internal link via [onTap] or an external link to [url]
+  /// [WidgetSpan] wrapper that either opens an internal link via [onTap]
+  /// Or an external link to [url]
   /// Requires [semanticsLabel] for screen readers
-  /// Pairs well with [EzRichText]
   EzInlineLink(
     this.text, {
     this.style,
+    this.textAlign,
+    required this.semanticsLabel,
     this.onTap,
     this.url,
-    required this.semanticsLabel,
-    this.spellOut,
-    this.locale,
-    this.mouseCursor = SystemMouseCursors.click,
-    this.onEnter,
-    this.onExit,
+    this.alignment = PlaceholderAlignment.middle,
+    this.basline,
   })  : assert((onTap == null) != (url == null),
             'Either onTap or url should be provided, but not both.'),
         super(
-          text: text,
-          recognizer: TapGestureRecognizer()
-            ..onTap = onTap ?? () => launchUrl(url!),
-          semanticsLabel: semanticsLabel,
-          spellOut: spellOut,
-          locale: locale,
-          style: style,
-          mouseCursor: mouseCursor,
-          onEnter: onEnter,
-          onExit: onExit,
+          child: EzText(
+            text,
+            style: style,
+            textAlign: textAlign,
+            onTap: onTap ?? () => launchUrl(url!),
+            semanticsLabel: semanticsLabel,
+          ),
+          alignment: alignment,
+          baseline: basline,
         );
 }
