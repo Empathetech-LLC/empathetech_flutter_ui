0699578ad834cb15f4418a33ec7d7b069ae2cce9
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-18 22:43:32 -0500

    added material 3 keys

--- lib/src/consts/keys.dart
+++ lib/src/consts/keys.dart
@@ -3,45 +3,87 @@
  * See LICENSE for distribution and usage details.
  */
 
-// App-wide SharedPreferences keys //
+// Navigation //
 
 const String homeRoute = '/';
+
+// Theme //
 
 const String isLightKey = 'isLight';
 const String isRightKey = 'isRight';
 
-const String noImageKey = 'noImage';
+// Styling //
+
+const String fontFamilyKey = 'fontFamily';
 
 const String marginKey = 'margin';
 const String paddingKey = 'padding';
-
-const String fontFamilyKey = 'fontFamily';
 
 const String buttonSpacingKey = 'buttonSpacing';
 const String textSpacingKey = 'textSpacing';
 
 const String circleDiameterKey = 'circleDiameter';
 
-// Theme specific keys //
+// Colors //
 
-// Light theme
-const String lightPageImageKey = 'lightPageImage';
-const String lightPageColorKey = 'lightPageColor';
-const String lightPageTextColorKey = 'lightPageTextColor';
-const String lightThemeColorKey = 'lightThemeColor';
-const String lightThemeTextColorKey = 'lightThemeTextColor';
-const String lightButtonColorKey = 'lightButtonColor';
-const String lightButtonTextColorKey = 'lightButtonTextColor';
-const String lightAccentColorKey = 'lightAccentColor';
-const String lightAccentTextColorKey = 'lightAccentTextColor';
+// Light
 
-// Dark theme
-const String darkPageImageKey = 'darkPageImage';
-const String darkPageColorKey = 'darkPageColor';
-const String darkPageTextColorKey = 'darkPageTextColor';
-const String darkThemeColorKey = 'darkThemeColor';
-const String darkThemeTextColorKey = 'darkThemeTextColor';
-const String darkButtonColorKey = 'darkButtonColor';
-const String darkButtonTextColorKey = 'darkButtonTextColor';
-const String darkAccentColorKey = 'darkAccentColor';
-const String darkAccentTextColorKey = 'darkAccentTextColor';
+const String lightPrimaryColor = 'lightPrimaryColor';
+const String lightOnPrimaryColor = 'lightOnPrimaryColor';
+const String lightPrimaryContainerColor = 'lightPrimaryContainerColor';
+const String lightOnPrimaryContainerColor = 'lightOnPrimaryContainerColor';
+
+const String lightSecondaryColor = 'lightSecondaryColor';
+const String lightOnSecondaryColor = 'lightOnSecondaryColor';
+const String lightSecondaryContainerColor = 'lightSecondaryContainerColor';
+const String lightOnSecondaryContainerColor = 'lightOnSecondaryContainerColor';
+
+const String lightTertiaryColor = 'lightTertiaryColor';
+const String lightOnTertiaryColor = 'lightOnTertiaryColor';
+const String lightTertiaryContainerColor = 'lightTertiaryContainerColor';
+const String lightOnTertiaryContainerColor = 'lightOnTertiaryContainerColor';
+
+const String lightErrorColor = 'lightErrorColor';
+const String lightOnErrorColor = 'lightOnErrorColor';
+const String lightErrorContainerColor = 'lightErrorContainerColor';
+const String lightOnErrorContainerColor = 'lightOnErrorContainerColor';
+
+const String lightBackgroundColor = 'lightBackgroundColor';
+const String lightOnBackgroundColor = 'lightOnBackgroundColor';
+const String lightSurfaceColor = 'lightSurfaceColor';
+const String lightOnSurfaceColor = 'lightOnSurfaceColor';
+
+const String lightOutlineColor = 'lightOutlineColor';
+
+// Dark
+
+const String darkPrimaryColor = 'darkPrimaryColor';
+const String darkOnPrimaryColor = 'darkOnPrimaryColor';
+const String darkPrimaryContainerColor = 'darkPrimaryContainerColor';
+const String darkOnPrimaryContainerColor = 'darkOnPrimaryContainerColor';
+
+const String darkSecondaryColor = 'darkSecondaryColor';
+const String darkOnSecondaryColor = 'darkOnSecondaryColor';
+const String darkSecondaryContainerColor = 'darkSecondaryContainerColor';
+const String darkOnSecondaryContainerColor = 'darkOnSecondaryContainerColor';
+
+const String darkTertiaryColor = 'darkTertiaryColor';
+const String darkOnTertiaryColor = 'darkOnTertiaryColor';
+const String darkTertiaryContainerColor = 'darkTertiaryContainerColor';
+const String darkOnTertiaryContainerColor = 'darkOnTertiaryContainerColor';
+
+const String darkErrorColor = 'darkErrorColor';
+const String darkOnErrorColor = 'darkOnErrorColor';
+const String darkErrorContainerColor = 'darkErrorContainerColor';
+const String darkOnErrorContainerColor = 'darkOnErrorContainerColor';
+
+const String darkBackgroundColor = 'darkBackgroundColor';
+const String darkOnBackgroundColor = 'darkOnBackgroundColor';
+const String darkSurfaceColor = 'darkSurfaceColor';
+const String darkOnSurfaceColor = 'darkOnSurfaceColor';
+
+const String darkOutlineColor = 'darkOutlineColor';
+
+// Images //
+
+const String noImageKey = 'noImage';
