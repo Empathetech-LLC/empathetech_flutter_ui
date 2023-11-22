5ac2ee70f1301fe69d2f3c2f070eb05fb2ee5a7f
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-22 15:09:49 -0500

    expanded definitions

--- lib/src/consts/keys.dart
+++ lib/src/consts/keys.dart
@@ -24,70 +24,146 @@ const String circleDiameterKey = 'circleDiameter';
 const String buttonSpacingKey = 'buttonSpacing';
 const String textSpacingKey = 'textSpacing';
 
+const List<String> stylingKeys = [
+  fontFamilyKey,
+  marginKey,
+  paddingKey,
+  circleDiameterKey,
+  buttonSpacingKey,
+  textSpacingKey,
+];
+
 // Colors //
 
 // Light
 
+const String lightBackgroundColorKey = 'lightBackgroundColor';
+const String lightOnBackgroundColorKey = 'lightOnBackgroundColor';
+
+const String lightSurfaceColorKey = 'lightSurfaceColor';
+const String lightOnSurfaceColorKey = 'lightOnSurfaceColor';
+
 const String lightPrimaryColorKey = 'lightPrimaryColor';
 const String lightOnPrimaryColorKey = 'lightOnPrimaryColor';
+
 const String lightPrimaryContainerColorKey = 'lightPrimaryContainerColor';
 const String lightOnPrimaryContainerColorKey = 'lightOnPrimaryContainerColor';
 
 const String lightSecondaryColorKey = 'lightSecondaryColor';
 const String lightOnSecondaryColorKey = 'lightOnSecondaryColor';
+
 const String lightSecondaryContainerColorKey = 'lightSecondaryContainerColor';
 const String lightOnSecondaryContainerColorKey =
     'lightOnSecondaryContainerColor';
 
 const String lightTertiaryColorKey = 'lightTertiaryColor';
 const String lightOnTertiaryColorKey = 'lightOnTertiaryColor';
+
 const String lightTertiaryContainerColorKey = 'lightTertiaryContainerColor';
 const String lightOnTertiaryContainerColorKey = 'lightOnTertiaryContainerColor';
 
 const String lightErrorColorKey = 'lightErrorColor';
 const String lightOnErrorColorKey = 'lightOnErrorColor';
+
 const String lightErrorContainerColorKey = 'lightErrorContainerColor';
 const String lightOnErrorContainerColorKey = 'lightOnErrorContainerColor';
 
-const String lightBackgroundColorKey = 'lightBackgroundColor';
-const String lightOnBackgroundColorKey = 'lightOnBackgroundColor';
-const String lightSurfaceColorKey = 'lightSurfaceColor';
-const String lightOnSurfaceColorKey = 'lightOnSurfaceColor';
-
 const String lightOutlineColorKey = 'lightOutlineColor';
+
+const List<String> lightColorKeys = [
+  lightBackgroundColorKey,
+  lightOnBackgroundColorKey,
+  lightSurfaceColorKey,
+  lightOnSurfaceColorKey,
+  lightPrimaryColorKey,
+  lightOnPrimaryColorKey,
+  lightPrimaryContainerColorKey,
+  lightOnPrimaryContainerColorKey,
+  lightSecondaryColorKey,
+  lightOnSecondaryColorKey,
+  lightSecondaryContainerColorKey,
+  lightOnSecondaryContainerColorKey,
+  lightTertiaryColorKey,
+  lightOnTertiaryColorKey,
+  lightTertiaryContainerColorKey,
+  lightOnTertiaryContainerColorKey,
+  lightErrorColorKey,
+  lightOnErrorColorKey,
+  lightErrorContainerColorKey,
+  lightOnErrorContainerColorKey,
+  lightOutlineColorKey,
+];
 
 // Dark
 
+const String darkBackgroundColorKey = 'darkBackgroundColor';
+const String darkOnBackgroundColorKey = 'darkOnBackgroundColor';
+
+const String darkSurfaceColorKey = 'darkSurfaceColor';
+const String darkOnSurfaceColorKey = 'darkOnSurfaceColor';
+
 const String darkPrimaryColorKey = 'darkPrimaryColor';
 const String darkOnPrimaryColorKey = 'darkOnPrimaryColor';
+
 const String darkPrimaryContainerColorKey = 'darkPrimaryContainerColor';
 const String darkOnPrimaryContainerColorKey = 'darkOnPrimaryContainerColor';
 
 const String darkSecondaryColorKey = 'darkSecondaryColor';
 const String darkOnSecondaryColorKey = 'darkOnSecondaryColor';
+
 const String darkSecondaryContainerColorKey = 'darkSecondaryContainerColor';
 const String darkOnSecondaryContainerColorKey = 'darkOnSecondaryContainerColor';
 
 const String darkTertiaryColorKey = 'darkTertiaryColor';
 const String darkOnTertiaryColorKey = 'darkOnTertiaryColor';
+
 const String darkTertiaryContainerColorKey = 'darkTertiaryContainerColor';
 const String darkOnTertiaryContainerColorKey = 'darkOnTertiaryContainerColor';
 
 const String darkErrorColorKey = 'darkErrorColor';
 const String darkOnErrorColorKey = 'darkOnErrorColor';
+
 const String darkErrorContainerColorKey = 'darkErrorContainerColor';
 const String darkOnErrorContainerColorKey = 'darkOnErrorContainerColor';
 
-const String darkBackgroundColorKey = 'darkBackgroundColor';
-const String darkOnBackgroundColorKey = 'darkOnBackgroundColor';
-const String darkSurfaceColorKey = 'darkSurfaceColor';
-const String darkOnSurfaceColorKey = 'darkOnSurfaceColor';
-
 const String darkOutlineColorKey = 'darkOutlineColor';
+
+const List<String> darkColorKeys = [
+  darkBackgroundColorKey,
+  darkOnBackgroundColorKey,
+  darkSurfaceColorKey,
+  darkOnSurfaceColorKey,
+  darkPrimaryColorKey,
+  darkOnPrimaryColorKey,
+  darkPrimaryContainerColorKey,
+  darkOnPrimaryContainerColorKey,
+  darkSecondaryColorKey,
+  darkOnSecondaryColorKey,
+  darkSecondaryContainerColorKey,
+  darkOnSecondaryContainerColorKey,
+  darkTertiaryColorKey,
+  darkOnTertiaryColorKey,
+  darkTertiaryContainerColorKey,
+  darkOnTertiaryContainerColorKey,
+  darkErrorColorKey,
+  darkOnErrorColorKey,
+  darkErrorContainerColorKey,
+  darkOnErrorContainerColorKey,
+  darkOutlineColorKey,
+];
 
 // Images //
 
 const String noImageKey = 'noImage';
 
+// Light
+
 const String lightPageImageKey = 'lightPageImage';
+
+const List<String> lightImageKeys = [lightPageImageKey];
+
+// Dark
+
 const String darkPageImageKey = 'darkPageImage';
+
+const List<String> darkImageKeys = [darkPageImageKey];
