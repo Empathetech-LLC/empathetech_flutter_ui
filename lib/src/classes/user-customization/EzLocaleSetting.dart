0d24e5281d46c859bf207d18a420de9dd7485d0f
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-29 12:06:39 -0500

    saving progress

--- lib/src/classes/user-customization/EzLocaleSetting.dart
+++ lib/src/classes/user-customization/EzLocaleSetting.dart
@@ -25,73 +25,79 @@ class _LocaleSettingState extends State<EzLocaleSetting> {
 
   @override
   Widget build(BuildContext context) {
-    // Define the list items //
+    // Gather the list items //
 
     final TextStyle? style = Theme.of(context).dropdownMenuTheme.textStyle;
 
-    final List<DropdownMenuItem<Locale>> items = [
-      DropdownMenuItem<Hand>(
-        child: Text(handName(context, Hand.right)),
-        value: Hand.right,
-      ),
-      DropdownMenuItem<Hand>(
-        child: Text(handName(context, Hand.left)),
-        value: Hand.left,
-      ),
-    ];
+    final List<Locale> _locales = (widget.locales == null)
+        ? EFUILang.supportedLocales
+        : EFUILang.supportedLocales + widget.locales!;
+
+    final List<DropdownMenuItem<Locale>> items = _locales.map((locale) {
+      return DropdownMenuItem<Locale>(
+        child: Text(locale.toString()),
+        value: locale,
+      );
+    }).toList();
 
     // Return the build //
 
-    // Define the build contents locally so it can be reversed in real-time alongside user selections
-    List<Widget> _children = [
-      // Label
-      Text(
-        EFUILang.of(context)!.hsLocale,
-        style: style,
-        textAlign: TextAlign.center,
-      ),
-      EzSpacer.row(EzConfig.instance.prefs[buttonSpacingKey]),
-
-      // Button
-      Semantics(
-        button: true,
-        hint:
-            "${EFUILang.of(context)!.hsHandSemantics} ${handName(context, currSide)}",
-        child: ExcludeSemantics(
-          child: DropdownButton<Hand>(
-            value: currSide,
-            items: items,
-            dropdownColor: Theme.of(context).scaffoldBackgroundColor,
-            onChanged: (Hand? newLocale) {
-              switch (newLocale) {
-                case Hand.right:
-                  EzConfig.instance.preferences.remove(isRightKey);
-                  setState(() {
-                    currSide = Hand.right;
-                  });
-                  break;
-
-                case Hand.left:
-                  EzConfig.instance.preferences.setBool(isRightKey, false);
-                  setState(() {
-                    currSide = Hand.left;
-                  });
-                  break;
-
-                default:
-                  break;
-              }
-            },
-          ),
-        ),
-      ),
-    ];
-
-    return Row(
+    return EzRow(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
-      children:
-          (currSide == Hand.right) ? _children : _children.reversed.toList(),
+      children: [
+        // Label
+        Text(
+          EFUILang.of(context)!.hsLanguage,
+          style: style,
+          textAlign: TextAlign.center,
+        ),
+        EzSpacer.row(EzConfig.instance.prefs[buttonSpacingKey]),
+
+        // Button
+        Semantics(
+          button: true,
+          hint:
+              "${EFUILang.of(context)!.hsThemeSemantics} ${_getName(currMode)}",
+          child: ExcludeSemantics(
+            child: DropdownButton<ThemeMode>(
+              value: currMode,
+              items: items,
+              dropdownColor: Theme.of(context).scaffoldBackgroundColor,
+              onChanged: (ThemeMode? newThemeMode) {
+                switch (newThemeMode) {
+                  case ThemeMode.system:
+                    EzConfig.instance.preferences.remove(isLightKey);
+                    setState(() {
+                      currMode = ThemeMode.system;
+                      PlatformTheme.of(context)!.themeMode = ThemeMode.system;
+                    });
+                    break;
+
+                  case ThemeMode.light:
+                    EzConfig.instance.preferences.setBool(isLightKey, true);
+                    setState(() {
+                      currMode = ThemeMode.light;
+                      PlatformTheme.of(context)!.themeMode = ThemeMode.light;
+                    });
+                    break;
+
+                  case ThemeMode.dark:
+                    EzConfig.instance.preferences.setBool(isLightKey, false);
+                    setState(() {
+                      currMode = ThemeMode.dark;
+                      PlatformTheme.of(context)!.themeMode = ThemeMode.dark;
+                    });
+                    break;
+
+                  default:
+                    break;
+                }
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
