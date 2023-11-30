809b7fe76c84112d2119d742ae90baeb7dee84ff
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-30 11:51:51 -0500

    split and renamed

---
+++ lib/src/consts/empathetechValues.dart
@@ -0,0 +1,76 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2023 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:flutter/material.dart';
+
+// General //
+
+const String homeRoute = '/';
+
+/// Allows for app images whose default is an asset to be "null"
+const String noImageValue = 'noImage';
+
+// Brand Colors //
+
+const int EmpathEucalyptusHex = 0xFF20DAA5;
+const Color EmpathEucalyptus = Color(EmpathEucalyptusHex);
+
+const int EmpathPurpleHex = 0xFFA520DA;
+const Color EmpathPurple = Color(EmpathPurpleHex);
+
+const int EmpathGoldenrodHex = 0xFFDAA520;
+const Color EmpathGoldenrod = Color(EmpathGoldenrodHex);
+
+const int whiteHex = 0xFFFFFFFF;
+const int offWhiteHex = 0xFFF5F5F5;
+const Color offWhite = Color(offWhiteHex);
+
+const int blackHex = 0xFF000000;
+const int offBlackHex = 0xFF191919;
+const Color offBlack = Color(offBlackHex);
+
+// Public links //
+
+/// One of us, one of us, one of us!
+const String EmpathetechCommunity = 'community@empathetech.net';
+
+/// If you need a hand
+const String EmpathetechSupport = 'support@empathetech.net';
+
+/// If you need two hands
+const String EmpathetechAdmin = 'admin@empathetech.net';
+
+/// See what we're up to!
+const String EmpathetechLinkedIn =
+    "https://linkedin.com/company/empathetech-llc";
+
+/// See what we're up to!
+const String EmpathetechMastodon = "https://mastodon.social/@empathetech";
+
+/// Where the magic happens
+const String EmpathetechGitHub = 'https://github.com/Empathetech-LLC';
+
+/// One of us, one of us, one of us!
+const String EmpathetechFoldingTeam =
+    'https://stats.foldingathome.org/team/1063265';
+
+/// If you want to show some love
+const String EmpathetechPayPal =
+    'https://www.paypal.com/donate/?hosted_button_id=NGEL6AB5A6KNL';
+
+/// If you want to show some love
+const String EmpathetechVenmo = 'https://venmo.com/empathetech';
+
+/// If you want to show some love
+const String EmpathetechCashApp = 'https://cash.app/\$empathetech';
+
+/// If you want to show some love
+const String EmpathetechPatreon = "https://patreon.com/empathetech";
+
+/// If you want to show some love
+const String EmpathetechCoffee = 'https://www.buymeacoffee.com/empathetech';
+
+/// If you want to show some love
+const String EmpathetechKofi = "https://ko-fi.com/empathetech";
