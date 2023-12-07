6dcdb1ae53b7c870bd27d39658088445b70db871
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-07 17:25:59 -0500

    updated to use mouse region hover

--- lib/src/classes/accessibility/EzLink.dart
+++ lib/src/classes/accessibility/EzLink.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class EzLink extends StatelessWidget {
+class EzLink extends StatefulWidget {
   final Key? key;
 
   /// Link message
@@ -22,12 +22,15 @@ class EzLink extends StatelessWidget {
   /// Message for screen readers
   final String semanticsLabel;
 
+  final Color? color;
+
   final MaterialStatesController? statesController;
 
   /// [TextButton] wrapper that acts like [Text] and either opens an internal link via [onTap]
   /// Or an external link to [url]
   /// Requires [semanticsLabel] for screen readers
-  /// Automatically colors [text] with [ColorScheme.primary] and adds an [underline] on hover
+  /// Automatically colors [text] with [ColorScheme.primary] and adds an [TextDecoration.underline] on hover
+  /// The [color] can optionally be overwritten
   EzLink(
     this.text, {
     this.key,
@@ -37,25 +40,52 @@ class EzLink extends StatelessWidget {
     this.url,
     this.onLongPress,
     required this.semanticsLabel,
+    this.color,
     this.statesController,
   })  : assert((onTap == null) != (url == null),
             'Either onTap or url should be provided, but not both.'),
         super(key: key);
 
   @override
+  _EzLinkState createState() => _EzLinkState();
+}
+
+class _EzLinkState extends State<EzLink> {
+  late TextStyle? _style = widget.style?.copyWith(
+    color: widget.color ?? Theme.of(context).colorScheme.primary,
+    decoration: TextDecoration.none,
+  );
+
+  void _addUnderline(bool isHovering) {
+    if (isHovering) {
+      setState(() {
+        _style = _style?.copyWith(decoration: TextDecoration.underline);
+      });
+    } else {
+      setState(() {
+        _style = _style?.copyWith(decoration: TextDecoration.none);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Semantics(
       link: true,
-      hint: semanticsLabel,
+      hint: widget.semanticsLabel,
       child: ExcludeSemantics(
-        child: TextButton(
-          onPressed: onTap ?? () => launchUrl(url!),
-          onLongPress: onLongPress,
-          onHover: _addUnderline,
-          autofocus: false,
-          clipBehavior: Clip.none,
-          statesController: statesController,
-          child: Text(text, style: _style, textAlign: textAlign),
+        child: MouseRegion(
+          onEnter: (_) => _addUnderline(true),
+          onExit: (_) => _addUnderline(false),
+          child: TextButton(
+            onPressed: widget.onTap ?? () => launchUrl(widget.url!),
+            onLongPress: widget.onLongPress,
+            child: Text(
+              widget.text,
+              style: _style,
+              textAlign: widget.textAlign,
+            ),
+          ),
         ),
       ),
     );
