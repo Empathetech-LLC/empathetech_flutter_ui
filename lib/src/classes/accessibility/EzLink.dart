0051c37a4d2f96ce485800181249f7d037d04b8e
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-08 08:11:46 -0500

    restored banner

--- lib/src/classes/accessibility/EzLink.dart
+++ lib/src/classes/accessibility/EzLink.dart
@@ -1,3 +1,8 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2023 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
