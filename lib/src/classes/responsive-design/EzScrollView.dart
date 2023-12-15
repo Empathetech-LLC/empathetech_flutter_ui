12e6ad12ad0c3d5de1c75f7f99db562e4b59ea11
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-15 11:52:37 -0500

    switched to list view

--- lib/src/classes/responsive-design/EzScrollView.dart
+++ lib/src/classes/responsive-design/EzScrollView.dart
@@ -3,114 +3,84 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../../../empathetech_flutter_ui.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 
-class EzScrollView extends SingleChildScrollView {
+class EzScrollView extends StatelessWidget {
   final Key? key;
   final Axis scrollDirection;
-
-  /// Only useful when [scrollDirection] is [Axis.horizontal]
-  /// Is passed to the [SingleChildScrollView]s [EzRow]
-  final bool reverseHands;
-
   final bool reverse;
-  final EdgeInsetsGeometry? padding;
+  final ScrollController? controller;
   final bool? primary;
   final ScrollPhysics? physics;
-  final ScrollController? controller;
-  final Widget? child;
-  final Clip clipBehavior;
+  final bool shrinkWrap;
+  final EdgeInsetsGeometry? padding;
+  final double? itemExtent;
+  final ItemExtentBuilder? itemExtentBuilder;
+  final Widget? prototypeItem;
+  final bool addAutomaticKeepAlives;
+  final bool addRepaintBoundaries;
+  final bool addSemanticIndexes;
+  final double? cacheExtent;
+  final List<Widget> children;
+  final int? semanticChildCount;
   final DragStartBehavior dragStartBehavior;
-  final String? restorationId;
   final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
-  final MainAxisSize mainAxisSize;
-  final MainAxisAlignment mainAxisAlignment;
-  final CrossAxisAlignment crossAxisAlignment;
-  final TextDirection? textDirection;
-  final TextBaseline? textBaseline;
-  final VerticalDirection verticalDirection;
+  final String? restorationId;
+  final Clip clipBehavior;
 
-  /// Replacement to original [child] parameter
-  /// [children] will be placed into an [EzRow] or [Column] based on [scrollDirection]
-  final List<Widget>? children;
-
-  /// [SingleChildScrollView] wrapper
-  /// If [children] are provided the original child parameter will be an [EzRow] or [Column] based on [scrollDirection]
-  /// Behaves like a standard [SingleChildScrollView] if [child] is provided
-  /// Parameters from both [SingleChildScrollView] and [EzRow]/[Column] are supported
-  /// [reverseHands] will only work if [children] are provided
-  const EzScrollView({
-    // SingleChildScrollView parameters //
-
+  /// [ListView] wrapper with custom defaults
+  EzScrollView({
     this.key,
     this.scrollDirection = Axis.vertical,
-    this.reverseHands = false,
     this.reverse = false,
-    this.padding,
-    this.primary,
-    this.physics = const BouncingScrollPhysics(),
     this.controller,
-    this.child,
-    this.clipBehavior = Clip.hardEdge,
+    this.primary,
+    this.physics,
+    this.shrinkWrap = false,
+    this.padding,
+    this.itemExtent,
+    this.itemExtentBuilder,
+    this.prototypeItem,
+    this.addAutomaticKeepAlives = true,
+    this.addRepaintBoundaries = true,
+    this.addSemanticIndexes = true,
+    this.cacheExtent,
+    required this.children,
+    this.semanticChildCount,
     this.dragStartBehavior = DragStartBehavior.start,
-    this.restorationId,
     this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
-
-    // EzRow/Column parameters //
-
-    this.mainAxisSize = MainAxisSize.max,
-    this.mainAxisAlignment = MainAxisAlignment.start,
-    this.crossAxisAlignment = CrossAxisAlignment.center,
-    this.textDirection,
-    this.textBaseline,
-    this.verticalDirection = VerticalDirection.down,
-    this.children,
-  }) : assert(
-          (child == null) != (children == null),
-          'Either child or children should be provided, but not both.',
-        );
-
-  Widget _buildCore() {
-    return (scrollDirection == Axis.vertical)
-        ? Column(
-            mainAxisSize: mainAxisSize,
-            mainAxisAlignment: mainAxisAlignment,
-            crossAxisAlignment: crossAxisAlignment,
-            textDirection: textDirection,
-            textBaseline: textBaseline,
-            verticalDirection: verticalDirection,
-            children: children!,
-          )
-        : EzRow(
-            mainAxisSize: mainAxisSize,
-            mainAxisAlignment: mainAxisAlignment,
-            crossAxisAlignment: crossAxisAlignment,
-            textDirection: textDirection,
-            textBaseline: textBaseline,
-            verticalDirection: verticalDirection,
-            children: children!,
-            reverseHands: reverseHands,
-          );
-  }
+    this.restorationId,
+    this.clipBehavior = Clip.hardEdge,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      key: key,
-      scrollDirection: scrollDirection,
-      reverse: reverse,
-      padding: padding,
-      primary: primary,
-      physics: physics,
-      controller: controller,
-      child: child ?? _buildCore(),
-      dragStartBehavior: dragStartBehavior,
-      clipBehavior: clipBehavior,
-      restorationId: restorationId,
-      keyboardDismissBehavior: keyboardDismissBehavior,
+    return Scrollbar(
+      child: ListView(
+        key: key,
+        scrollDirection: scrollDirection,
+        reverse: reverse,
+        padding: padding,
+        primary: primary,
+        physics: physics,
+        controller: controller,
+        shrinkWrap: shrinkWrap,
+        itemExtent: itemExtent,
+        itemExtentBuilder: itemExtentBuilder,
+        prototypeItem: prototypeItem,
+        addAutomaticKeepAlives: addAutomaticKeepAlives,
+        addRepaintBoundaries: addRepaintBoundaries,
+        addSemanticIndexes: addSemanticIndexes,
+        cacheExtent: cacheExtent,
+        children: children,
+        semanticChildCount: semanticChildCount,
+        dragStartBehavior: dragStartBehavior,
+        keyboardDismissBehavior: keyboardDismissBehavior,
+        restorationId: restorationId,
+        clipBehavior: clipBehavior,
+      ),
     );
   }
 }
