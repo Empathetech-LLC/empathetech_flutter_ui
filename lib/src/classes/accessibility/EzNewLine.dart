14b13d208f68684edbd3a23cabdafcd678fb2f32
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-19 16:00:24 -0500

    using text widget

--- lib/src/classes/accessibility/EzNewLine.dart
+++ lib/src/classes/accessibility/EzNewLine.dart
@@ -5,15 +5,41 @@
 
 import 'package:flutter/material.dart';
 
-class EzNewLine extends TextSpan {
+class EzNewLine extends StatelessWidget {
   final TextStyle? style;
+  final TextAlign? textAlign;
+  final TextDirection? textDirection;
+  final bool? softWrap;
+  final TextOverflow? overflow;
+  final TextScaler? textScaler;
+  final Color? selectionColor;
 
-  /// [TextSpan] with [text] set to an empty string.
-  const EzNewLine(this.style)
-      : super(
-          text: "",
-          style: style,
-          semanticsLabel: null,
-          spellOut: false,
-        );
+  /// [Text] wrapper with an empty string for easily creating new lines
+  /// In the passed [style]
+  const EzNewLine(
+    this.style, {
+    this.textAlign,
+    this.textDirection,
+    this.softWrap,
+    this.overflow,
+    this.textScaler,
+    this.selectionColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ExcludeSemantics(
+      child: Text(
+        "",
+        style: style,
+        textAlign: textAlign,
+        textDirection: textDirection,
+        softWrap: softWrap,
+        overflow: overflow,
+        textScaler: textScaler,
+        semanticsLabel: null,
+        selectionColor: selectionColor,
+      ),
+    );
+  }
 }
