8a05a36ee8b5517b992a76e1c2a8cd00c83efa45
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-26 16:25:39 -0500

    restored

---
+++ lib/src/classes/screen-reader-compliance/EzPlainText.dart
@@ -0,0 +1,49 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2023 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../../../empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+class EzPlainText extends TextSpan {
+  final String text;
+
+  /// [context] is required to access [EFUILang]
+  final BuildContext context;
+
+  final TextStyle? style;
+
+  /// Message that [EzRichText] will use for the full-block read
+  final String? semantics;
+
+  final MouseCursor mouseCursor;
+  final void Function(PointerEnterEvent)? onEnter;
+  final void Function(PointerExitEvent)? onExit;
+  final Locale? locale;
+  final bool spellOut;
+
+  /// [TextSpan] wrapper with custom [semantics] for use in [EzRichText]
+  EzPlainText(
+    this.text, {
+    required this.context,
+    this.style,
+    this.semantics,
+    this.mouseCursor = SystemMouseCursors.text,
+    this.onEnter,
+    this.onExit,
+    this.locale,
+    this.spellOut = false,
+  }) : super(
+          text: text,
+          style: style,
+          semanticsLabel: EFUILang.of(context)!.gContinue,
+          mouseCursor: mouseCursor,
+          onEnter: onEnter,
+          onExit: onExit,
+          locale: locale,
+          spellOut: spellOut,
+        );
+}
