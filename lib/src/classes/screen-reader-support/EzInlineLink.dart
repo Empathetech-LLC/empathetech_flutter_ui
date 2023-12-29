49ba841d5b8d7261ef52e5be63488b8ef7c034fa
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-29 12:05:26 -0500

    added missing params

--- lib/src/classes/screen-reader-support/EzInlineLink.dart
+++ lib/src/classes/screen-reader-support/EzInlineLink.dart
@@ -9,8 +9,11 @@ import 'package:flutter/material.dart';
 
 class EzInlineLink extends WidgetSpan {
   final String text;
+
   final Key? key;
   final TextStyle? style;
+  final Color? color;
+  final TextAlign? textAlign;
 
   /// Destination function
   final void Function()? onTap;
@@ -21,20 +24,29 @@ class EzInlineLink extends WidgetSpan {
   /// Message for screen readers
   final String? semanticsLabel;
 
+  /// Updates [text]'s [Semantics] readout in [EzRichText]
+  final String? textFix;
+
   /// Optional tooltip override
   final String? tooltip;
+
+  final MaterialStatesController? statesController;
 
   /// [WidgetSpan] wrapper with an [EzLink] for a [WidgetSpan.child]
   /// Seems to prefer Strings of length 5+
   /// Smaller strings can sometimes have issues with text spacing
   EzInlineLink(
     this.text, {
+    this.textFix,
     this.key,
     this.style,
+    this.color,
+    this.textAlign,
     this.onTap,
     this.url,
     this.semanticsLabel,
     this.tooltip,
+    this.statesController,
   })  : assert((onTap == null) != (url == null),
             'Either onTap or url should be provided, but not both.'),
         super(
@@ -42,10 +54,13 @@ class EzInlineLink extends WidgetSpan {
             text,
             key: key,
             style: style,
+            color: color,
+            textAlign: textAlign,
             onTap: onTap,
             url: url,
             semanticsLabel: semanticsLabel,
             tooltip: tooltip,
+            statesController: statesController,
           ),
           alignment: PlaceholderAlignment.baseline,
           baseline: TextBaseline.alphabetic,
