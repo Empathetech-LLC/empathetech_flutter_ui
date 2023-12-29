9ea68724a4e76e1fb45a8cb9653fc48cfd7c3f59
empathictech <105748722+empathictech@users.noreply.github.com>
2023-12-29 10:49:51 -0500

    removed unused

--- lib/src/classes/screen-reader-support/EzRichText.dart
+++ lib/src/classes/screen-reader-support/EzRichText.dart
@@ -3,55 +3,90 @@
  * See LICENSE for distribution and usage details.
  */
 
+import '../../../empathetech_flutter_ui.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class EzRichText extends RichText {
+class EzRichText extends StatelessWidget {
+  final List<InlineSpan> children;
   final Key? key;
-  final List<InlineSpan> textSpans;
+  final TextStyle? style;
   final TextAlign textAlign;
   final TextDirection? textDirection;
+  final Locale? locale;
   final bool softWrap;
   final TextOverflow overflow;
   final TextScaler textScaler;
   final int? maxLines;
-  final Locale? locale;
   final StrutStyle? strutStyle;
   final TextWidthBasis textWidthBasis;
   final TextHeightBehavior? textHeightBehavior;
   final SelectionRegistrar? selectionRegistrar;
   final Color? selectionColor;
 
-  /// [RichText] wrapper for cleaner development
+  /// [Text.rich] wrapper with custom [Semantics] behavior
+  /// Recommended to pair with [EzPlainText] and [EzInlineLink]
   EzRichText(
-    this.textSpans, {
+    this.children, {
     this.key,
+    this.style,
     this.textAlign = TextAlign.start,
     this.textDirection,
+    this.locale,
     this.softWrap = true,
     this.overflow = TextOverflow.clip,
     this.textScaler = TextScaler.noScaling,
     this.maxLines,
-    this.locale,
     this.strutStyle,
     this.textWidthBasis = TextWidthBasis.parent,
     this.textHeightBehavior,
     this.selectionRegistrar,
     this.selectionColor,
-  }) : super(
-          key: key,
-          text: TextSpan(children: textSpans),
-          textAlign: textAlign,
-          textDirection: textDirection,
-          softWrap: softWrap,
-          overflow: overflow,
-          textScaler: textScaler,
-          maxLines: maxLines,
-          locale: locale,
-          strutStyle: strutStyle,
-          textWidthBasis: textWidthBasis,
-          textHeightBehavior: textHeightBehavior,
-          selectionRegistrar: selectionRegistrar,
-          selectionColor: selectionColor,
-        );
+  });
+
+  String _semanticsLabel() {
+    StringBuffer label = StringBuffer("");
+
+    for (InlineSpan child in children) {
+      switch (child.runtimeType) {
+        case TextSpan:
+          TextSpan ogSpan = child as TextSpan;
+          label.writeAll([ogSpan.semanticsLabel ?? ogSpan.text, " "]);
+          break;
+        case EzInlineLink:
+          EzInlineLink linkSpan = child as EzInlineLink;
+          label.writeAll([linkSpan.text, " "]);
+          break;
+        default:
+          break;
+      }
+    }
+
+    return label.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: _semanticsLabel(),
+      child: Text.rich(
+        TextSpan(children: children),
+        key: key,
+        style: style,
+        strutStyle: strutStyle,
+        textAlign: textAlign,
+        textDirection: textDirection,
+        locale: locale,
+        softWrap: softWrap,
+        overflow: overflow,
+        textScaler: textScaler,
+        maxLines: maxLines,
+        semanticsLabel: null,
+        textWidthBasis: textWidthBasis,
+        textHeightBehavior: textHeightBehavior,
+        selectionColor: selectionColor,
+      ),
+    );
+  }
 }
