26ca0d429940e68d889d180e6409c4c34cd5c138
empathictech <105748722+empathictech@users.noreply.github.com>
2024-01-01 21:10:08 -0500

    reverted to text span, added custom semantics compute

--- lib/src/classes/screen-reader-support/EzPlainText.dart
+++ lib/src/classes/screen-reader-support/EzPlainText.dart
@@ -5,64 +5,53 @@
 
 import '../../../empathetech_flutter_ui.dart';
 
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 
-class EzPlainText extends WidgetSpan {
-  final String data;
-  final Key? key;
+class EzPlainText extends TextSpan {
+  final String? text;
   final TextStyle? style;
-  final StrutStyle? strutStyle;
-  final TextAlign? textAlign;
-  final TextDirection? textDirection;
-  final Locale? locale;
-  final bool? softWrap;
-  final TextOverflow? overflow;
-  final TextScaler? textScaler;
-  final int? maxLines;
+  final GestureRecognizer? recognizer;
+  final MouseCursor mouseCursor;
+  final PointerEnterEventListener? onEnter;
+  final PointerExitEventListener? onExit;
   final String? semanticsLabel;
-  final TextWidthBasis? textWidthBasis;
-  final TextHeightBehavior? textHeightBehavior;
-  final Color? selectionColor;
+  final Locale? locale;
+  final bool? spellOut;
 
-  /// [WidgetSpan] wrapper with a [Text] child and custom [Semantics] to pair with [EzInlineLink]s in [RichText] blocks
+  /// [TextSpan] wrapper with custom [Semantics] to pair with [EzInlineLink]s in [RichText] blocks
   EzPlainText(
-    this.data, {
-    this.key,
+    this.text, {
     this.style,
-    this.strutStyle,
-    this.textAlign,
-    this.textDirection,
-    this.locale,
-    this.softWrap,
-    this.overflow,
-    this.textScaler,
-    this.maxLines,
+    this.recognizer,
+    this.mouseCursor = MouseCursor.defer,
+    this.onEnter,
+    this.onExit,
     this.semanticsLabel,
-    this.textWidthBasis,
-    this.textHeightBehavior,
-    this.selectionColor,
+    this.locale,
+    this.spellOut,
   }) : super(
-          child: ExcludeSemantics(
-            child: Text(
-              data,
-              key: key,
-              style: style,
-              strutStyle: strutStyle,
-              textAlign: textAlign,
-              textDirection: textDirection,
-              locale: locale,
-              softWrap: softWrap,
-              overflow: overflow,
-              textScaler: textScaler,
-              maxLines: maxLines,
-              semanticsLabel: semanticsLabel,
-              textWidthBasis: textWidthBasis,
-              textHeightBehavior: textHeightBehavior,
-              selectionColor: selectionColor,
-            ),
-          ),
-          alignment: PlaceholderAlignment.baseline,
-          baseline: TextBaseline.alphabetic,
+          text: text,
           style: style,
+          recognizer: recognizer,
+          mouseCursor: mouseCursor,
+          onEnter: onEnter,
+          onExit: onExit,
+          semanticsLabel: semanticsLabel,
+          locale: locale,
+          spellOut: spellOut,
         );
+
+  @override
+  void computeSemanticsInformation(
+    List<InlineSpanSemanticsInformation> collector, {
+    ui.Locale? inheritedLocale,
+    bool inheritedSpellOut = false,
+  }) {
+    // Do nothing
+    // EzRichText will add this.text to it's semanticsLabel
+    // This will return no semantics information so it is skipped on EzRichText traversal
+  }
 }
