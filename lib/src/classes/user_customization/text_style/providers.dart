e4e6d48ef6836fb358cdd8208eefbd85b047f148
empathictech <105748722+empathictech@users.noreply.github.com>
2024-03-20 17:47:27 -0400

    reduce, re-use, recycle

--- lib/src/classes/user_customization/text_style/providers.dart
+++ lib/src/classes/user_customization/text_style/providers.dart
@@ -7,8 +7,10 @@ import '../../../../empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
 
-class DisplayTextStyleProvider extends ChangeNotifier {
-  TextStyle _style = buildDisplay();
+abstract class BaseTextStyleProvider extends ChangeNotifier {
+  TextStyle _style;
+
+  BaseTextStyleProvider(TextStyle initialStyle) : _style = initialStyle;
 
   TextStyle get value => _style;
 
@@ -64,230 +66,22 @@ class DisplayTextStyleProvider extends ChangeNotifier {
   }
 }
 
-class HeadlineTextStyleProvider extends ChangeNotifier {
-  TextStyle _style = buildHeadline();
-
-  TextStyle get value => _style;
-
-  /// Run [fuseWithGFont] on the current [TextStyle] with the passed [gFont]
-  void fuse(String gFont) {
-    _style = fuseWithGFont(starter: _style, gFont: gFont);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.fontSize] to the passed [size]
-  void resize(double size) {
-    _style = _style.copyWith(fontSize: size);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontWeight] based on the passed [bold]
-  void bold(bool bold) {
-    _style =
-        _style.copyWith(fontWeight: bold ? FontWeight.bold : FontWeight.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontStyle] based on the passed [italic]
-  void italic(bool italic) {
-    _style = _style.copyWith(
-        fontStyle: italic ? FontStyle.italic : FontStyle.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.decoration] based on the passed [underline]
-  void underline(bool underline) {
-    _style = _style.copyWith(
-        decoration: underline ? TextDecoration.underline : TextDecoration.none);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.letterSpacing] to the passed [spacing]
-  void setLetterSpacing(double spacing) {
-    _style = _style.copyWith(letterSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.wordSpacing] to the passed [spacing]
-  void setWordSpacing(double spacing) {
-    _style = _style.copyWith(wordSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.height] to the passed [height]
-  void setHeight(double height) {
-    _style = _style.copyWith(height: height);
-    notifyListeners();
-  }
+class DisplayTextStyleProvider extends BaseTextStyleProvider {
+  DisplayTextStyleProvider() : super(buildDisplay());
 }
 
-class TitleTextStyleProvider extends ChangeNotifier {
-  TextStyle _style = buildTitle();
-
-  TextStyle get value => _style;
-
-  /// Run [fuseWithGFont] on the current [TextStyle] with the passed [gFont]
-  void fuse(String gFont) {
-    _style = fuseWithGFont(starter: _style, gFont: gFont);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.fontSize] to the passed [size]
-  void resize(double size) {
-    _style = _style.copyWith(fontSize: size);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontWeight] based on the passed [bold]
-  void bold(bool bold) {
-    _style =
-        _style.copyWith(fontWeight: bold ? FontWeight.bold : FontWeight.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontStyle] based on the passed [italic]
-  void italic(bool italic) {
-    _style = _style.copyWith(
-        fontStyle: italic ? FontStyle.italic : FontStyle.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.decoration] based on the passed [underline]
-  void underline(bool underline) {
-    _style = _style.copyWith(
-        decoration: underline ? TextDecoration.underline : TextDecoration.none);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.letterSpacing] to the passed [spacing]
-  void setLetterSpacing(double spacing) {
-    _style = _style.copyWith(letterSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.wordSpacing] to the passed [spacing]
-  void setWordSpacing(double spacing) {
-    _style = _style.copyWith(wordSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.height] to the passed [height]
-  void setHeight(double height) {
-    _style = _style.copyWith(height: height);
-    notifyListeners();
-  }
+class HeadlineTextStyleProvider extends BaseTextStyleProvider {
+  HeadlineTextStyleProvider() : super(buildHeadline());
 }
 
-class BodyTextStyleProvider extends ChangeNotifier {
-  TextStyle _style = buildBody();
-
-  TextStyle get value => _style;
-
-  /// Run [fuseWithGFont] on the current [TextStyle] with the passed [gFont]
-  void fuse(String gFont) {
-    _style = fuseWithGFont(starter: _style, gFont: gFont);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.fontSize] to the passed [size]
-  void resize(double size) {
-    _style = _style.copyWith(fontSize: size);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontWeight] based on the passed [bold]
-  void bold(bool bold) {
-    _style =
-        _style.copyWith(fontWeight: bold ? FontWeight.bold : FontWeight.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontStyle] based on the passed [italic]
-  void italic(bool italic) {
-    _style = _style.copyWith(
-        fontStyle: italic ? FontStyle.italic : FontStyle.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.decoration] based on the passed [underline]
-  void underline(bool underline) {
-    _style = _style.copyWith(
-        decoration: underline ? TextDecoration.underline : TextDecoration.none);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.letterSpacing] to the passed [spacing]
-  void setLetterSpacing(double spacing) {
-    _style = _style.copyWith(letterSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.wordSpacing] to the passed [spacing]
-  void setWordSpacing(double spacing) {
-    _style = _style.copyWith(wordSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.height] to the passed [height]
-  void setHeight(double height) {
-    _style = _style.copyWith(height: height);
-    notifyListeners();
-  }
+class TitleTextStyleProvider extends BaseTextStyleProvider {
+  TitleTextStyleProvider() : super(buildTitle());
 }
 
-class LabelTextStyleProvider extends ChangeNotifier {
-  TextStyle _style = buildLabel();
+class BodyTextStyleProvider extends BaseTextStyleProvider {
+  BodyTextStyleProvider() : super(buildBody());
+}
 
-  TextStyle get value => _style;
-
-  /// Run [fuseWithGFont] on the current [TextStyle] with the passed [gFont]
-  void fuse(String gFont) {
-    _style = fuseWithGFont(starter: _style, gFont: gFont);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.fontSize] to the passed [size]
-  void resize(double size) {
-    _style = _style.copyWith(fontSize: size);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontWeight] based on the passed [bold]
-  void bold(bool bold) {
-    _style =
-        _style.copyWith(fontWeight: bold ? FontWeight.bold : FontWeight.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.fontStyle] based on the passed [italic]
-  void italic(bool italic) {
-    _style = _style.copyWith(
-        fontStyle: italic ? FontStyle.italic : FontStyle.normal);
-    notifyListeners();
-  }
-
-  /// Toggle the [TextStyle.decoration] based on the passed [underline]
-  void underline(bool underline) {
-    _style = _style.copyWith(
-        decoration: underline ? TextDecoration.underline : TextDecoration.none);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.letterSpacing] to the passed [spacing]
-  void setLetterSpacing(double spacing) {
-    _style = _style.copyWith(letterSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.wordSpacing] to the passed [spacing]
-  void setWordSpacing(double spacing) {
-    _style = _style.copyWith(wordSpacing: spacing);
-    notifyListeners();
-  }
-
-  /// Update the [TextStyle.height] to the passed [height]
-  void setHeight(double height) {
-    _style = _style.copyWith(height: height);
-    notifyListeners();
-  }
+class LabelTextStyleProvider extends BaseTextStyleProvider {
+  LabelTextStyleProvider() : super(buildLabel());
 }
