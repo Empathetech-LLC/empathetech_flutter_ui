7997d363a5530619eed5283d0cbfdd773ca80a24
empathictech <105748722+empathictech@users.noreply.github.com>
2024-04-21 23:08:09 -0400

    streamlined ez app provider

--- lib/src/classes/platform_availability/app_provider.dart
+++ lib/src/classes/platform_availability/app_provider.dart
@@ -9,24 +9,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzAppProvider extends StatelessWidget {
-  final Widget Function(BuildContext)? builder;
   final TargetPlatform? initialPlatform;
   final PlatformSettingsData? settings;
 
   /// Optionally provide a [PlatformApp] that will be wrapped in the Empathetech theme [builder]
-  final PlatformApp? app;
+  final Widget app;
 
   /// [PlatformProvider] wrapper with [ezThemeData] defaults
   EzAppProvider({
     super.key,
-    this.builder,
-    this.initialPlatform,
+    required this.app,
     this.settings,
-    this.app,
-  }) : assert(
-          (builder == null) != (app == null),
-          'Either builder or app should be provided, but not both.',
-        );
+    this.initialPlatform,
+  });
 
   // Gather the theme data //
 
@@ -46,20 +41,19 @@ class EzAppProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PlatformProvider(
-      builder: builder ??
-          (BuildContext context) => PlatformTheme(
-                builder: (BuildContext context) => app!,
-                themeMode: _initialTheme,
-                materialLightTheme: _materialLight,
-                materialDarkTheme: _materialDark,
-                cupertinoLightTheme: MaterialBasedCupertinoThemeData(
-                  materialTheme: _materialLight,
-                ),
-                cupertinoDarkTheme: MaterialBasedCupertinoThemeData(
-                  materialTheme: _materialDark,
-                ),
-                matchCupertinoSystemChromeBrightness: true,
-              ),
+      builder: (BuildContext context) => PlatformTheme(
+        builder: (BuildContext context) => app,
+        themeMode: _initialTheme,
+        materialLightTheme: _materialLight,
+        materialDarkTheme: _materialDark,
+        cupertinoLightTheme: MaterialBasedCupertinoThemeData(
+          materialTheme: _materialLight,
+        ),
+        cupertinoDarkTheme: MaterialBasedCupertinoThemeData(
+          materialTheme: _materialDark,
+        ),
+        matchCupertinoSystemChromeBrightness: true,
+      ),
       initialPlatform: initialPlatform,
       settings: settings ??
           PlatformSettingsData(
