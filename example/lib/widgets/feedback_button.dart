709e8232f062908590fa024b2fdba8977170c586
empathictech <105748722+empathictech@users.noreply.github.com>
2024-04-24 21:27:50 -0400

    reverted sharefiles

--- example/lib/widgets/feedback_button.dart
+++ example/lib/widgets/feedback_button.dart
@@ -12,8 +12,17 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class FeedbackButton extends StatelessWidget {
   final BuildContext parentContext;
+  final EFUILang l10n;
 
-  const FeedbackButton({super.key, required this.parentContext});
+  /// Recommended to provide [ColorScheme].surface
+  final Color bannerIconColor;
+
+  const FeedbackButton({
+    super.key,
+    required this.parentContext,
+    required this.l10n,
+    required this.bannerIconColor,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -33,50 +42,52 @@ class FeedbackButton extends StatelessWidget {
             return;
           }
 
-          late final XFile screenshot;
-          try {
-            screenshot = await saveImage(feedback.screenshot);
-          } catch (_) {
-            return;
-          }
+          final String screenshotFilePath =
+              await writeImageToStorage(feedback.screenshot);
 
           await Clipboard.setData(const ClipboardData(text: empathSupport));
 
-          await Share.shareXFiles(
-            <XFile>[screenshot],
-            subject: 'Open UI feedback',
-            text: feedback.text,
-          ).then((_) async {
-            scaffoldMessengerKey.currentState?.showMaterialBanner(
-              MaterialBanner(
-                content: Text(
-                  EFUILang.of(context)!.gCopiedEmail,
-                  textAlign: TextAlign.center,
-                ),
-                actions: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.close_outlined),
-                    onPressed: () => scaffoldMessengerKey.currentState
-                        ?.hideCurrentMaterialBanner(),
-                    color: Theme.of(context).colorScheme.surface,
-                  ),
-                ],
+          scaffoldMessengerKey.currentState?.showMaterialBanner(
+            MaterialBanner(
+              content: Text(
+                l10n.gCopiedEmail,
+                textAlign: TextAlign.center,
               ),
-            );
-          });
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.close,
+                    color: bannerIconColor,
+                  ),
+                  onPressed: () => scaffoldMessengerKey.currentState
+                      ?.hideCurrentMaterialBanner(),
+                ),
+              ],
+            ),
+          );
+
+          // ignore: deprecated_member_use
+          await Share.shareFiles(
+            <String>[screenshotFilePath],
+            text: feedback.text,
+          );
         });
       },
       leadingIcon: Icon(
         Icons.feedback,
         size: Theme.of(context).textTheme.titleLarge?.fontSize,
       ),
-      child: Text(EFUILang.of(context)!.gGiveFeedback),
+      child: Text(l10n.gGiveFeedback),
     );
   }
+}
 
-  Future<XFile> saveImage(Uint8List feedbackScreenshot) async {
-    final Directory output = await getTemporaryDirectory();
-    final String screenshotFilePath = '${output.path}/feedback.png';
-    return XFile(screenshotFilePath);
-  }
+/// Taken better_feedback's example app
+/// https://pub.dev/packages/feedback/example
+Future<String> writeImageToStorage(Uint8List feedbackScreenshot) async {
+  final Directory output = await getTemporaryDirectory();
+  final String screenshotFilePath = '${output.path}/feedback.png';
+  final File screenshotFile = File(screenshotFilePath);
+  await screenshotFile.writeAsBytes(feedbackScreenshot);
+  return screenshotFilePath;
 }
