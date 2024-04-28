297fe21c47d8e2d323a8d954e1eae59cb9889f14
empathictech <105748722+empathictech@users.noreply.github.com>
2024-04-28 18:32:45 -0400

    working rough draft

--- example/test/home_screen_test.dart
+++ example/test/home_screen_test.dart
@@ -1,13 +1,15 @@
 import './test_widgets.dart';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 void main() {
   testWidgets('Click all the home screen things', (WidgetTester tester) async {
     await tester.runAsync(() async {
-      EzConfig.test(prefs: empathetechConfig);
-      await tester.pumpWidget(testOpenUI());
+      final Widget testApp = await testOpenUI(prefs: empathetechConfig);
+
+      await tester.pumpWidget(testApp);
     });
   });
 }
--- example/test/test_widgets.dart
+++ example/test/test_widgets.dart
@@ -1,71 +1,18 @@
-import 'package:example/screens/screens.dart';
-import 'package:example/widgets/widgets.dart';
+import 'package:example/main.dart';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 
-Widget testOpenUI({TargetPlatform initialPlatform = TargetPlatform.android}) {
-  final GoRouter _router = GoRouter(
-    initialLocation: homePath,
-    routes: <RouteBase>[
-      GoRoute(
-        path: homePath,
-        builder: (BuildContext context, GoRouterState state) {
-          return const HomeScreen();
-        },
-        routes: <RouteBase>[
-          GoRoute(
-            path: textSettingsPath,
-            builder: (BuildContext context, GoRouterState state) {
-              return const TextSettingsScreen();
-            },
-          ),
-          GoRoute(
-            path: layoutSettingsPath,
-            builder: (BuildContext context, GoRouterState state) {
-              return const LayoutSettingsScreen();
-            },
-          ),
-          GoRoute(
-            path: colorSettingsPath,
-            builder: (BuildContext context, GoRouterState state) {
-              return const ColorSettingsScreen();
-            },
-          ),
-          GoRoute(
-            path: imageSettingsPath,
-            builder: (BuildContext context, GoRouterState state) {
-              return const ImageSettingsScreen();
-            },
-          ),
-        ],
-      ),
-    ],
+Future<Widget> testOpenUI({required Map<String, Object> prefs}) async {
+  SharedPreferences.setMockInitialValues(prefs);
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+  EzConfig.init(
+    preferences: preferences,
+    assetPaths: <String>{},
+    defaults: prefs,
   );
 
-  return EzAppProvider(
-    scaffoldMessengerKey: scaffoldMessengerKey,
-    app: PlatformApp.router(
-      debugShowCheckedModeBanner: false,
-
-      // Language handlers
-      localizationsDelegates: const <LocalizationsDelegate<dynamic>>{
-        LocaleNamesLocalizationsDelegate(),
-        ...EFUILang.localizationsDelegates,
-      },
-
-      // Supported languages
-      supportedLocales: EFUILang.supportedLocales,
-
-      // Current language
-      locale: EzConfig.getLocale(),
-
-      title: 'Open UI Test',
-      routerConfig: _router,
-    ),
-    initialPlatform: initialPlatform,
-  );
+  return const OpenUI();
 }
