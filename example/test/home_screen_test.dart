30be8c3be8a6a605428c215824bc8ca0702232e9
empathictech <105748722+empathictech@users.noreply.github.com>
2024-04-28 18:14:10 -0400

    using shared testing app

--- example/test/home_screen_test.dart
+++ example/test/home_screen_test.dart
@@ -1,25 +1,13 @@
-import 'package:example/main.dart';
-import 'package:example/screens/screens.dart';
-import 'package:example/widgets/widgets.dart';
+import './test_widgets.dart';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 void main() {
   testWidgets('Click all the home screen things', (WidgetTester tester) async {
     await tester.runAsync(() async {
-      EzConfig.init(
-        // Paths to any locally stored images the app uses
-        assetPaths: <String>{},
-
-        preferences: await SharedPreferences.getInstance(),
-
-        // Your brand colors, custom styling, etc
-        defaults: empathetechConfig,
-      );
-
-      await tester.pumpWidget(const OpenUI());
+      EzConfig.test(prefs: empathetechConfig);
+      await tester.pumpWidget(testOpenUI());
     });
   });
 }
