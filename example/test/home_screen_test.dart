966a45e9d10a1bdcdd9dc25c4417b36da41d8121
empathictech <105748722+empathictech@users.noreply.github.com>
2024-06-01 22:42:24 -0400

    using text

--- example/test/home_screen_test.dart
+++ example/test/home_screen_test.dart
@@ -7,9 +7,38 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 void main() {
   testWidgets('Click all the home screen things', (WidgetTester tester) async {
     await tester.runAsync(() async {
-      final Widget testApp = await testOpenUI(prefs: empathetechConfig);
+      final EFUILang enText = await EFUILang.delegate.load(const Locale('en'));
+
+      final Widget testApp = await testOpenUI(prefs: <String, Object>{
+        ...empathetechConfig,
+        localeKey: <String>['en'],
+      });
 
       await tester.pumpWidget(testApp);
+
+      // Navigation //
+
+      debugPrint('Navigating to Text settings');
+
+      final Finder textSettingsButton = find.text(enText.tsPageTitle);
+      await tester.tap(textSettingsButton);
+      await tester.pumpAndSettle();
+
+      debugPrint('Navigating to Layout settings');
+
+      debugPrint('Navigating to Color settings');
+
+      debugPrint('Navigating to Image settings');
+
+      // Functionality //
+
+      debugPrint('Toggling Dominant hand setting');
+
+      debugPrint('Selecting each theme mode');
+
+      debugPrint('Toggling Spanish language');
+
+      debugPrint('Selecting reset all');
     });
   });
 }
