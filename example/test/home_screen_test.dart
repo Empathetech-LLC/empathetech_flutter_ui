45950d6f0b8278f6b80814638587b3cc45cb58ae
empathictech <105748722+empathictech@users.noreply.github.com>
2024-06-03 15:02:06 -0400

    saving progress

--- example/test/home_screen_test.dart
+++ example/test/home_screen_test.dart
@@ -1,12 +1,17 @@
-import './test_widgets.dart';
+import 'test_widgets.dart';
+
+import 'package:example/screens/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 void main() {
   testWidgets('Click all the home screen things', (WidgetTester tester) async {
     await tester.runAsync(() async {
+      // Load the app //
+
       final EFUILang enText = await EFUILang.delegate.load(const Locale('en'));
 
       final Widget testApp = await testOpenUI(prefs: <String, Object>{
@@ -14,13 +19,27 @@ void main() {
         localeKey: <String>['en'],
       });
 
+      debugPrint('Loading Open UI');
+
       await tester.pumpWidget(testApp);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(PlatformApp), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomeScreen), findsOneWidget);
 
       // Navigation //
 
       debugPrint('Navigating to Text settings');
 
-      final Finder textSettingsButton = find.text(enText.tsPageTitle);
+      final Finder textSettingsButton = find.widgetWithText(
+        ElevatedButton,
+        enText.tsPageTitle,
+      );
+      expect(textSettingsButton, findsOneWidget);
+
       await tester.tap(textSettingsButton);
       await tester.pumpAndSettle();
 
