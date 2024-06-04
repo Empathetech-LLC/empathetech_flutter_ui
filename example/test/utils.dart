7bf1d96ca5c256ed5c696607b7dd673fd9c358c9
empathictech <105748722+empathictech@users.noreply.github.com>
2024-06-04 19:46:03 -0400

    consolidated

---
+++ example/test/utils.dart
@@ -0,0 +1,50 @@
+import 'package:example/main.dart';
+import 'package:example/utils/export.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_localized_locales/flutter_localized_locales.dart';
+
+Future<void> goBack(WidgetTester tester) async {
+  final Finder backButton = find.byIcon(Icons.arrow_back);
+  expect(backButton, findsOneWidget);
+  await tester.tap(backButton);
+  await tester.pumpAndSettle();
+}
+
+/// Find, touch, and settle a touch-point
+Future<void> touch(WidgetTester tester, Finder toFind) async {
+  await tester.ensureVisible(toFind);
+  await tester.tap(toFind);
+  await tester.pumpAndSettle();
+}
+
+Widget testOpenUI({
+  required Map<String, Object> prefs,
+  TargetPlatform platform = TargetPlatform.android,
+}) =>
+    EzAppProvider(
+      initialPlatform: platform,
+      app: PlatformApp.router(
+        // Production ready!
+        debugShowCheckedModeBanner: false,
+
+        // Language handlers
+        localizationsDelegates: <LocalizationsDelegate<dynamic>>{
+          const LocaleNamesLocalizationsDelegate(),
+          ...EFUILang.localizationsDelegates,
+          OpenUIFeedbackLocalizationsDelegate(),
+        },
+
+        // Supported languages
+        supportedLocales: EFUILang.supportedLocales,
+
+        // Current language
+        locale: EzConfig.getLocale(),
+
+        title: appTitle,
+        routerConfig: router,
+      ),
+    );
