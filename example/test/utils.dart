4b275afc585afe2efb3ca2c6f844471f798ba1fa
empathictech <105748722+empathictech@users.noreply.github.com>
2024-06-06 13:04:52 -0400

    updated for platforms

--- example/test/utils.dart
+++ example/test/utils.dart
@@ -2,14 +2,19 @@ import 'package:example/main.dart';
 import 'package:example/utils/export.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 
-Future<void> goBack(WidgetTester tester) async {
-  final Finder backButton = find.byIcon(Icons.arrow_back);
+Future<void> goBack({
+  required WidgetTester tester,
+  required EFUILang l10n,
+}) async {
+  final Finder backButton = find.byTooltip(l10n.gBack);
   expect(backButton, findsOneWidget);
+
   await tester.ensureVisible(backButton);
   await tester.tap(backButton);
   await tester.pumpAndSettle();
@@ -25,7 +30,7 @@ Future<void> touch(WidgetTester tester, Finder toFind) async {
 Widget testOpenUI({
   required String title,
   required Locale locale,
-  TargetPlatform platform = TargetPlatform.android,
+  required TargetPlatform platform,
 }) =>
     EzAppProvider(
       key: ValueKey<String>(title),
