70e8fca9b75da1ee913c04dd873d8935fb9a7cf4
empathictech <105748722+empathictech@users.noreply.github.com>
2024-06-07 15:53:17 -0400

    cleanup && added lefty tests

--- example/test/home_screen_test.dart
+++ example/test/home_screen_test.dart
@@ -44,17 +44,16 @@ void main() async {
 
   //// Run the tests ////
 
-  // Verify each platform //
+  // Test non-default platforms //
 
-  const List<TargetPlatform> platforms = <TargetPlatform>[
-    TargetPlatform.android,
+  const List<TargetPlatform> otherPlatforms = <TargetPlatform>[
     TargetPlatform.iOS,
     TargetPlatform.macOS,
     TargetPlatform.windows,
     TargetPlatform.linux,
   ];
 
-  for (final TargetPlatform platform in platforms) {
+  for (final TargetPlatform platform in otherPlatforms) {
     testSuite(
       title: 'Home screen platform test: ${platform.toString()}',
       locale: english,
@@ -64,8 +63,9 @@ void main() async {
     );
   }
 
-  // Verify each language //
-  for (int i = 0; i < locales.length; i++) {
+  // Test non-default languages //
+
+  for (int i = 1; i < locales.length; i++) {
     testSuite(
       title: 'Home screen language test: ${locales[i].languageCode}',
       locale: locales[i],
@@ -80,7 +80,7 @@ void main() async {
     );
   }
 
-  // Verify screen size and config smorgasbord //
+  // Test screen size && user config smorgasbord //
 
   const List<Size> screenSizes = <Size>[
     Size(375, 667), // Small phone (iPhone SE 3rd gen)
@@ -111,8 +111,6 @@ void main() async {
       localeNames: enNames,
       screenSize: screenSize,
       setup: () async {
-        await preferences.remove(localeKey);
-
         // Text settings //
 
         // Display
@@ -165,6 +163,141 @@ void main() async {
       localeNames: enNames,
       screenSize: screenSize,
       setup: () async {
+        // Text settings //
+
+        // Display
+        await preferences.setDouble(displayFontSizeKey, maxFontSize);
+        await preferences.setDouble(displayFontHeightKey, maxFontHeight);
+        await preferences.setDouble(
+            displayLetterSpacingKey, maxFontLetterSpacing);
+        await preferences.setDouble(displayWordSpacingKey, maxFontWordSpacing);
+
+        // Headline
+        await preferences.setDouble(headlineFontSizeKey, maxFontSize);
+        await preferences.setDouble(headlineFontHeightKey, maxFontHeight);
+        await preferences.setDouble(
+            headlineLetterSpacingKey, maxFontLetterSpacing);
+        await preferences.setDouble(headlineWordSpacingKey, maxFontWordSpacing);
+
+        // Title
+        await preferences.setDouble(titleFontSizeKey, maxFontSize);
+        await preferences.setDouble(titleFontHeightKey, maxFontHeight);
+        await preferences.setDouble(
+            titleLetterSpacingKey, maxFontLetterSpacing);
+        await preferences.setDouble(titleWordSpacingKey, maxFontWordSpacing);
+
+        // Body
+        await preferences.setDouble(bodyFontSizeKey, maxFontSize);
+        await preferences.setDouble(bodyFontHeightKey, maxFontHeight);
+        await preferences.setDouble(bodyLetterSpacingKey, maxFontLetterSpacing);
+        await preferences.setDouble(bodyWordSpacingKey, maxFontWordSpacing);
+
+        // Label
+        await preferences.setDouble(labelFontSizeKey, maxFontSize);
+        await preferences.setDouble(labelFontHeightKey, maxFontHeight);
+        await preferences.setDouble(
+            labelLetterSpacingKey, maxFontLetterSpacing);
+        await preferences.setDouble(labelWordSpacingKey, maxFontWordSpacing);
+
+        // Layout settings //
+        await preferences.setDouble(marginKey, maxMargin);
+        await preferences.setDouble(paddingKey, maxPadding);
+        await preferences.setDouble(spacingKey, maxSpacing);
+      },
+    );
+  }
+
+  // Ditto (sub-set) for left-handed users //
+
+  const List<Size> screenSubSet = <Size>[
+    Size(375, 667), // Small phone (iPhone SE 3rd gen)
+    Size(430, 932), // Large phone (iPhone 14 Pro Max)
+    Size(1920, 1080), // Standard desktop monitor (16:9)
+  ];
+
+  for (final Size screenSize in screenSubSet) {
+    // Default config
+    testSuite(
+      title:
+          'Home screen layout test: Lefty default config on ${screenSize.width}x${screenSize.height}',
+      locale: english,
+      l10n: enText,
+      localeNames: enNames,
+      screenSize: screenSize,
+      setup: () async {
+        await preferences.setBool(isLeftyKey, true);
+      },
+    );
+
+    // Minimum config
+    testSuite(
+      title:
+          'Home screen layout test: Lefty minimum config on ${screenSize.width}x${screenSize.height}',
+      locale: english,
+      l10n: enText,
+      localeNames: enNames,
+      screenSize: screenSize,
+      setup: () async {
+        // Global settings //
+
+        await preferences.setBool(isLeftyKey, true);
+
+        // Text settings //
+
+        // Display
+        await preferences.setDouble(displayFontSizeKey, minFontSize);
+        await preferences.setDouble(displayFontHeightKey, minFontHeight);
+        await preferences.setDouble(
+            displayLetterSpacingKey, minFontLetterSpacing);
+        await preferences.setDouble(displayWordSpacingKey, minFontWordSpacing);
+
+        // Headline
+        await preferences.setDouble(headlineFontSizeKey, minFontSize);
+        await preferences.setDouble(headlineFontHeightKey, minFontHeight);
+        await preferences.setDouble(
+            headlineLetterSpacingKey, minFontLetterSpacing);
+        await preferences.setDouble(headlineWordSpacingKey, minFontWordSpacing);
+
+        // Title
+        await preferences.setDouble(titleFontSizeKey, minFontSize);
+        await preferences.setDouble(titleFontHeightKey, minFontHeight);
+        await preferences.setDouble(
+            titleLetterSpacingKey, minFontLetterSpacing);
+        await preferences.setDouble(titleWordSpacingKey, minFontWordSpacing);
+
+        // Body
+        await preferences.setDouble(bodyFontSizeKey, minFontSize);
+        await preferences.setDouble(bodyFontHeightKey, minFontHeight);
+        await preferences.setDouble(bodyLetterSpacingKey, minFontLetterSpacing);
+        await preferences.setDouble(bodyWordSpacingKey, minFontWordSpacing);
+
+        // Label
+        await preferences.setDouble(labelFontSizeKey, minFontSize);
+        await preferences.setDouble(labelFontHeightKey, minFontHeight);
+        await preferences.setDouble(
+            labelLetterSpacingKey, minFontLetterSpacing);
+        await preferences.setDouble(labelWordSpacingKey, minFontWordSpacing);
+
+        // Layout settings //
+        await preferences.setDouble(marginKey, minMargin);
+        await preferences.setDouble(paddingKey, minPadding);
+        await preferences.setDouble(spacingKey, minSpacing);
+      },
+    );
+
+    // Maximum config
+    testSuite(
+      title:
+          'Home screen layout test: Lefty maximum config on ${screenSize.width}x${screenSize.height}',
+      locale: english,
+      l10n: enText,
+      localeNames: enNames,
+      screenSize: screenSize,
+      setup: () async {
+        // Global settings //
+
+        await preferences.setBool(isLeftyKey, true);
+
         // Text settings //
 
         // Display
