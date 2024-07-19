30571e75ffdb01c232e8c537e158f493a9f368de
empathictech <105748722+empathictech@users.noreply.github.com>
2024-07-19 11:28:30 -0400

    looking good

--- lib/src/widgets/user_customization/text_theme/font_family_batch_setting.dart
+++ lib/src/widgets/user_customization/text_theme/font_family_batch_setting.dart
@@ -6,9 +6,11 @@
 import '../../../../empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzFontFamilyBatchSetting extends StatefulWidget {
-  final List<String> configKeys;
+  /// All [EzConfig] keys being edited paired with their default values
+  final Map<String, String> keysNDefaults;
 
   /// Use this to live update the [TextStyle] on your UI
   /// Recommended to use [fuseWithGFont]
@@ -16,11 +18,11 @@ class EzFontFamilyBatchSetting extends StatefulWidget {
 
   final String? tooltip;
 
-  /// Standardized tool for updating the [TextStyle.fontFamily] for the passed [configKey]
+  /// Standardized tool for batch updating the [TextStyle.fontFamily] for the passed [keysNDefaults]
   /// [EzFontFamilyBatchSetting] options are built from [googleStyles]
   const EzFontFamilyBatchSetting({
     super.key,
-    required this.configKeys,
+    required this.keysNDefaults,
     required this.notifierCallback,
     this.tooltip,
   });
@@ -33,16 +35,53 @@ class EzFontFamilyBatchSetting extends StatefulWidget {
 class _FontFamilyBatchSettingState extends State<EzFontFamilyBatchSetting> {
   // Gather the theme data //
 
-  late final String defaultFontFamily =
-      EzConfig.getDefault(widget.configKeys.first);
-
-  late String currFontFamily =
-      //  EzConfig.get(widget.configKey) ??
-      defaultFontFamily;
-
   late final ThemeData theme = Theme.of(context);
+  late final EFUILang l10n = EFUILang.of(context)!;
+
+  // Define the build data //
+
+  late final Map<String, String> startingFonts = widget.keysNDefaults.map(
+    (String key, String value) => MapEntry<String, String>(
+      key,
+      EzConfig.get(key) ?? value,
+    ),
+  );
+
+  late bool isUniform = startingFonts.values
+      .every((String font) => font == startingFonts.values.first);
+
+  late String currFont = isUniform
+      ? startingFonts.values.first
+      : mostCommonOrBodyFont(startingFonts.values.toList());
 
   // Define button functions //
+
+  /// Lazily returns the most common font, or the body font if all are unique
+  String mostCommonOrBodyFont(List<String> fonts) {
+    final String body = EzConfig.getDefault(bodyFontFamilyKey);
+
+    if (fonts.isEmpty) return body;
+
+    final Map<String, int> fontCount = <String, int>{};
+    String? mostCommon;
+    int highestCount = 0;
+
+    for (final String font in fonts) {
+      final int count = (fontCount[font] ?? 0) + 1;
+      fontCount[font] = count;
+
+      if (count > highestCount) {
+        mostCommon = font;
+        highestCount = count;
+      }
+    }
+
+    if (highestCount <= 1) {
+      return body;
+    } else {
+      return mostCommon!;
+    }
+  }
 
   /// Builds an [EzAlertDialog] with [googleStyles] mapped to a list of [ElevatedButton]s
   late final List<DropdownMenuEntry<String>> entries =
@@ -56,6 +95,41 @@ class _FontFamilyBatchSettingState extends State<EzFontFamilyBatchSetting> {
     );
   }).toList();
 
+  /// Only activated if the user has already edited the font families in the advanced settings, and the changes aren't uniform
+  // Confirm that the user wants to continue with batch editing, which will force uniformity
+  Future<bool> confirmBatchOverride() async {
+    return await showPlatformDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        void onConfirm() => Navigator.of(dialogContext).pop(true);
+
+        void onDeny() => Navigator.of(dialogContext).pop(false);
+
+        return EzAlertDialog(
+          title: Text(
+            l10n.ssLanguages,
+            textAlign: TextAlign.center,
+          ),
+          content: Text(
+            l10n.tsBatchOverride(l10n.tsFontFamily),
+            textAlign: TextAlign.center,
+          ),
+          materialActions: ezMaterialActions(
+            context: dialogContext,
+            onConfirm: onConfirm,
+            onDeny: onDeny,
+          ),
+          cupertinoActions: ezCupertinoActions(
+            context: dialogContext,
+            onConfirm: onConfirm,
+            onDeny: onDeny,
+          ),
+          needsClose: false,
+        );
+      },
+    );
+  }
+
   // Return the build //
 
   @override
@@ -63,16 +137,32 @@ class _FontFamilyBatchSettingState extends State<EzFontFamilyBatchSetting> {
     return Tooltip(
       message: widget.tooltip ?? EFUILang.of(context)!.tsFontFamily,
       child: DropdownMenu<String>(
-        initialSelection: currFontFamily,
+        initialSelection: currFont,
         dropdownMenuEntries: entries,
-        onSelected: (String? fontFamily) {
+        onSelected: (String? fontFamily) async {
           if (fontFamily == null) return;
-          currFontFamily = fontFamily;
+
+          if (!isUniform) {
+            final bool override = await confirmBatchOverride();
+
+            if (override) {
+              isUniform = true;
+            } else {
+              return;
+            }
+          }
+
+          currFont = fontFamily;
           widget.notifierCallback(fontFamily);
-          // EzConfig.setString(widget.configKey, fontFamily);
+
+          for (final MapEntry<String, String> entry
+              in widget.keysNDefaults.entries) {
+            EzConfig.setString(entry.key, fontFamily);
+          }
+
           setState(() {});
         },
-        textStyle: googleStyles[currFontFamily]?.copyWith(
+        textStyle: googleStyles[currFont]?.copyWith(
           color: theme.colorScheme.onSurface,
         ),
         width: smallBreakpoint / 4,
