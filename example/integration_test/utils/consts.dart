f7d05a25a3d64596ef5e2efbf246b880ef5c72fe
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-01 13:57:17 -0400

    added shared consts

---
+++ example/integration_test/utils/consts.dart
@@ -0,0 +1,10 @@
+/* open_ui
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:flutter/material.dart';
+
+const Locale english = Locale('en');
+const Locale spanish = Locale('es');
+const Locale french = Locale('fr');
