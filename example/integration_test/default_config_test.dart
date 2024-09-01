2ef1f0ded078b80e15bbf71ff72b8c8a83b53064
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-01 18:20:06 -0400

    are you not entertained?!

--- example/integration_test/default_config_test.dart
+++ example/integration_test/default_config_test.dart
@@ -8,7 +8,9 @@ import 'screens/text_settings.dart' as text;
 import 'screens/layout_settings.dart' as layout;
 import 'screens/color_settings.dart' as color;
 import 'screens/image_settings.dart' as image;
+import 'utils/export.dart';
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -16,100 +18,65 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 
-void main() async {
+const String parentTest = 'default-config';
+
+void main() {
   // Setup the test environment //
 
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
-  const Locale english = Locale('en');
-  const Locale spanish = Locale('es');
-  const Locale french = Locale('fr');
+  late final EFUILang enText;
+  late final List<LocaleNames> l10nNames;
 
-  final EFUILang enText = await EFUILang.delegate.load(english);
+  final Completer<bool> completer = Completer<bool>();
 
-  final LocaleNames enNames =
-      await const LocaleNamesLocalizationsDelegate().load(english);
-  final LocaleNames esNames =
-      await const LocaleNamesLocalizationsDelegate().load(spanish);
-  final LocaleNames frNames =
-      await const LocaleNamesLocalizationsDelegate().load(french);
-  final List<LocaleNames> l10nNames = <LocaleNames>[enNames, esNames, frNames];
+  setUpAll(() async {
+    enText = await EFUILang.delegate.load(english);
 
-  // Set mock values //
+    final LocaleNames enNames =
+        await const LocaleNamesLocalizationsDelegate().load(english);
+    final LocaleNames esNames =
+        await const LocaleNamesLocalizationsDelegate().load(spanish);
+    final LocaleNames frNames =
+        await const LocaleNamesLocalizationsDelegate().load(french);
 
-  SharedPreferences.setMockInitialValues(empathetechConfig);
-  final SharedPreferences preferences = await SharedPreferences.getInstance();
+    l10nNames = <LocaleNames>[enNames, esNames, frNames];
 
-  EzConfig.init(
-    preferences: preferences,
-    assetPaths: <String>{},
-    defaults: empathetechConfig,
-  );
+    // Set mock values //
 
-  // Run the test suites //
+    SharedPreferences.setMockInitialValues(empathetechConfig);
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
 
-  const String parentTest = 'default-config';
+    EzConfig.init(
+      preferences: preferences,
+      assetPaths: <String>{},
+      defaults: empathetechConfig,
+    );
+
+    completer.complete(true);
+
+    return completer.future;
+  });
+
+  // Run test suites //
 
   // Settings home
-  group(home.name, () async {
-    await home.testSuite(
+  group(
+    home.name,
+    () => home.testSuite(
       title: '${home.name}-$parentTest',
       locale: english,
       l10n: enText,
       localeNames: l10nNames,
-    );
-  });
+    ),
+  );
 
-  group(home.name, () async {
-    await home.testSuite(
-      title: '${home.name}-$parentTest',
-      locale: english,
-      l10n: enText,
-      localeNames: l10nNames,
-    );
-  });
+  // Text settings
 
-  // // Text settings
-  // group(
-  //   text.name,
-  //   () => text.testSuite(
-  //     title: '${text.name}-$parentTest',
-  //     locale: english,
-  //     l10n: enText,
-  //     localeNames: l10nNames,
-  //   ),
-  // );
+  // Layout settings
 
-  // // Layout settings
-  // group(
-  //   layout.name,
-  //   () => layout.testSuite(
-  //     title: '${layout.name}-$parentTest',
-  //     locale: english,
-  //     l10n: enText,
-  //     localeNames: l10nNames,
-  //   ),
-  // );
+  // Color settings
 
-  // // Color settings
-  // group(
-  //   color.name,
-  //   () => color.testSuite(
-  //     title: '${color.name}-$parentTest',
-  //     locale: english,
-  //     l10n: enText,
-  //     localeNames: l10nNames,
-  //   ),
-  // );
-
-  // // Image settings
-  // group(
-  //   image.name,
-  //   () => image.testSuite(
-  //     title: '${image.name}-$parentTest',
-  //     locale: english,
-  //     l10n: enText,
-  //     localeNames: l10nNames,
-  //   ),
-  // );
+  // Image settings
 }
