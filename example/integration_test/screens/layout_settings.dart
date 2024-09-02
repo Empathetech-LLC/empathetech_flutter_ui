0e68ae253026103b3a43a3104f9e94903df0eb58
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-02 09:11:08 -0400

    removed extra confusion while debugging

--- example/integration_test/screens/layout_settings.dart
+++ example/integration_test/screens/layout_settings.dart
@@ -3,43 +3,4 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../utils/export.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-import 'package:flutter_localized_locales/flutter_localized_locales.dart';
-
 const String name = 'layout-settings-screen';
-
-void testSuite({
-  required String title,
-  required Locale locale,
-  required EFUILang l10n,
-  required List<LocaleNames> localeNames,
-  bool isLefty = false,
-}) =>
-    testWidgets(title, (WidgetTester tester) async {
-      // Load the app //
-
-      final Widget testApp = testOpenUI(title: title, locale: locale);
-
-      await tester.pumpWidget(testApp);
-      await tester.pumpAndSettle();
-
-      // Navigate to layout settings //
-
-      final Finder layoutSettingsButton = find.widgetWithText(
-        ElevatedButton,
-        l10n.lsPageTitle,
-      );
-
-      expect(layoutSettingsButton, findsOneWidget);
-      await touch(tester: tester, finder: layoutSettingsButton);
-
-      await goBack(tester: tester, l10n: l10n); // Remove me!
-
-      //// Verify text loaded ////
-
-      //// Test functionality ////
-    });
--- example/integration_test/screens/text_settings.dart
+++ example/integration_test/screens/text_settings.dart
@@ -3,43 +3,4 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../utils/export.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-import 'package:flutter_localized_locales/flutter_localized_locales.dart';
-
 const String name = 'text-settings-screen';
-
-void testSuite({
-  required String title,
-  required Locale locale,
-  required EFUILang l10n,
-  required List<LocaleNames> localeNames,
-  bool isLefty = false,
-}) =>
-    testWidgets(title, (WidgetTester tester) async {
-      // Load the app //
-
-      final Widget testApp = testOpenUI(title: title, locale: locale);
-
-      await tester.pumpWidget(testApp);
-      await tester.pumpAndSettle();
-
-      // Navigate to text settings //
-
-      final Finder textSettingsButton = find.widgetWithText(
-        ElevatedButton,
-        l10n.tsPageTitle,
-      );
-
-      expect(textSettingsButton, findsOneWidget);
-      await touch(tester: tester, finder: textSettingsButton);
-
-      await goBack(tester: tester, l10n: l10n); // Remove me!
-
-      //// Verify text loaded ////
-
-      //// Test functionality ////
-    });
