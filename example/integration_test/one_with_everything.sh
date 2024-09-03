149aa7a49fa16ebfdb5f32a9414260f261ef3133
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-03 15:05:22 -0400

    testing full suite script

---
+++ example/integration_test/one_with_everything.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/default_config_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/minimum_config_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/maximum_config_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/es_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/fr_test.dart
