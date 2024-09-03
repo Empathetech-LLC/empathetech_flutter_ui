e81d057694e725261e078d1462f8e04952a9ef31
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-03 17:01:59 -0400

    renamed test

--- example/integration_test/one_with_everything.sh
+++ example/integration_test/one_with_everything.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/default_config_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/minimum_config_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/maximum_config_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/default_test.dart
 flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/es_test.dart
 flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/fr_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/minimum_config_test.dart
+flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/maximum_config_test.dart
