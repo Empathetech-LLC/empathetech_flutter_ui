1114391b6cf708ad27f6de08c073a6e1ccee448d
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-03 15:02:19 -0400

    I AM COMPLETE

--- example/integration_test/screens/home.dart
+++ example/integration_test/screens/home.dart
@@ -3,6 +3,11 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
 import '../utils/export.dart';
 
 import 'package:example/main.dart';
@@ -25,20 +30,8 @@ void testSuite({
 
       debugPrint('Loading localizations');
       final EFUILang l10n = await EFUILang.delegate.load(locale);
-
-      final LocaleNames enNames =
-          await const LocaleNamesLocalizationsDelegate().load(english);
-      final LocaleNames esNames =
-          await const LocaleNamesLocalizationsDelegate().load(spanish);
-      final LocaleNames frNames =
-          await const LocaleNamesLocalizationsDelegate().load(french);
-
-      /// [english, spanish, french]
-      final List<LocaleNames> l10nNames = <LocaleNames>[
-        enNames,
-        esNames,
-        frNames,
-      ];
+      final LocaleNames l10nNames =
+          await const LocaleNamesLocalizationsDelegate().load(locale);
 
       // Load the app //
 
@@ -75,7 +68,7 @@ void testSuite({
       await touch(tester: tester, finder: find.byType(DropdownMenu<bool>).last);
 
       if (isLefty) {
-        debugPrint('Verifying DHS right hand layout');
+        debugPrint('Right hand layout');
 
         // Activate righty layout
         await tester.tapAt(tester.getCenter(find.text(l10n.gRight).last));
@@ -84,12 +77,14 @@ void testSuite({
         List<Widget> handButtonsChildren =
             (tester.widget(find.byType(Row).at(1)) as Row).children;
 
+        expect(handButtonsChildren.length, 3);
+
         // Verify righty layout
         expect(handButtonsChildren[0], isA<Flexible>());
         expect(handButtonsChildren[1], isA<EzSpacer>());
         expect(handButtonsChildren[2], isA<DropdownMenu<bool>>());
 
-        debugPrint('Verifying DHS left hand layout');
+        debugPrint('Left hand layout');
 
         // Activate lefty layout
         await touch(
@@ -108,7 +103,7 @@ void testSuite({
         expect(handButtonsChildren[1], isA<EzSpacer>());
         expect(handButtonsChildren[2], isA<Flexible>());
       } else {
-        debugPrint('Verifying DHS left hand layout');
+        debugPrint('Left hand layout');
 
         // Activate lefty layout
         await tester.tapAt(tester.getCenter(find.text(l10n.gLeft).last));
@@ -117,12 +112,14 @@ void testSuite({
         List<Widget> handButtonsChildren =
             (tester.widget(find.byType(Row).at(1)) as Row).children;
 
+        expect(handButtonsChildren.length, 3);
+
         // Verify lefty layout
         expect(handButtonsChildren[0], isA<DropdownMenu<bool>>());
         expect(handButtonsChildren[1], isA<EzSpacer>());
         expect(handButtonsChildren[2], isA<Flexible>());
 
-        debugPrint('Verifying DHS right hand layout');
+        debugPrint('Right hand layout');
 
         // Activate righty layout
         await touch(
@@ -142,149 +139,124 @@ void testSuite({
         expect(handButtonsChildren[2], isA<DropdownMenu<bool>>());
       }
 
-      // // Theme mode //
+      // Theme mode //
 
-      // debugPrint('\nTesting theme mode setting (TMS)');
-      // final Finder themeModeButton = find.byType(DropdownMenu<ThemeMode>);
+      debugPrint('\nTesting theme mode setting (TMS)');
 
-      // debugPrint('Testing TMS exists');
-      // expect(themeModeButton, findsOneWidget);
-      // await touch(tester: tester, text: themeModeButton);
+      // Verify  layout
+      final List<Widget> themeButtonsChildren =
+          (tester.widget(find.byType(EzRow).at(1)) as EzRow).children;
 
-      // // Verify  layout
-      // final Finder themeButtonsRowFinder = find.byType(Row).at(2);
-      // final Row themeButtonsRow = tester.widget(themeButtonsRowFinder);
-      // final List<Widget> themeButtonsChildren = themeButtonsRow.children;
+      expect(themeButtonsChildren.length, 3);
+      if (isLefty) {
+        expect(themeButtonsChildren[0], isA<DropdownMenu<ThemeMode>>());
+        expect(themeButtonsChildren[1], isA<EzSpacer>());
+        expect(themeButtonsChildren[2], isA<Flexible>());
+      } else {
+        expect(themeButtonsChildren[0], isA<Flexible>());
+        expect(themeButtonsChildren[1], isA<EzSpacer>());
+        expect(themeButtonsChildren[2], isA<DropdownMenu<ThemeMode>>());
+      }
 
-      // if (isLefty) {
-      //   expect(themeButtonsChildren[0], isA<DropdownMenu<ThemeMode>>());
-      //   expect(themeButtonsChildren[1], isA<EzSpacer>());
-      //   expect(themeButtonsChildren[2], isA<Flexible>());
-      // } else {
-      //   expect(themeButtonsChildren[0], isA<Flexible>());
-      //   expect(themeButtonsChildren[1], isA<EzSpacer>());
-      //   expect(themeButtonsChildren[2], isA<DropdownMenu<ThemeMode>>());
-      // }
+      // Activate light theme
+      debugPrint('Light');
+      await touch(tester: tester, finder: find.byType(DropdownMenu<ThemeMode>));
+      await tester.tapAt(tester.getCenter(find.text(l10n.gLight).last));
+      await tester.pumpAndSettle();
 
-      // debugPrint('Testing TMS options appear');
-      // final Finder systemButton = find.text(l10n.gSystem).last;
-      // final Finder lightButton = find.text(l10n.gLight).last;
-      // final Finder darkButton = find.text(l10n.gDark).last;
+      // Activate system theme
+      debugPrint('System');
+      await touch(tester: tester, finder: find.byType(DropdownMenu<ThemeMode>));
+      await tester.tapAt(tester.getCenter(find.text(l10n.gSystem).last));
+      await tester.pumpAndSettle();
 
-      // expect(systemButton, findsOneWidget);
-      // expect(lightButton, findsOneWidget);
-      // expect(darkButton, findsOneWidget);
+      // Activate dark theme
+      debugPrint('Dark');
+      await touch(tester: tester, finder: find.byType(DropdownMenu<ThemeMode>));
+      await tester.tapAt(tester.getCenter(find.text(l10n.gDark).last));
+      await tester.pumpAndSettle();
 
-      // debugPrint('Testing TMS menu is dismissible');
-      // await dismissTap(tester);
-      // await touch(tester: tester, text: themeModeButton);
+      // Language //
 
-      // debugPrint('Testing DHS functionality');
+      debugPrint('\nTesting language setting button (LSB)');
 
-      // // Activate light theme
-      // await tester.tap(lightButton);
-      // await tester.pumpAndSettle();
+      // Activate Spanish localizations
+      debugPrint('Spanish');
+      await touch(tester: tester, finder: find.byType(EzLocaleSetting));
+      await tester.tap(find.text(l10nNames.nameOf('es')!).last);
+      await tester.pumpAndSettle();
 
-      // // Activate dark theme
-      // await touch(tester: tester, text: themeModeButton);
-      // await tester.tap(darkButton);
-      // await tester.pumpAndSettle();
+      // Activate French localizations
+      debugPrint('French');
+      await touch(tester: tester, finder: find.byType(EzLocaleSetting));
+      await tester.tap(find.text(l10nNames.nameOf('fr')!).last);
+      await tester.pumpAndSettle();
 
-      // // Activate system theme
-      // await touch(tester: tester, text: themeModeButton);
-      // await tester.tap(systemButton);
-      // await tester.pumpAndSettle();
+      // Activate English localizations
+      debugPrint('English');
+      await touch(tester: tester, finder: find.byType(EzLocaleSetting));
+      await tester.tap(find.text(l10nNames.nameOf('en')!).last);
+      await tester.pumpAndSettle();
 
-      // // Language //
+      // Activate English localizations
+      debugPrint('Close');
+      await touch(tester: tester, finder: find.byType(EzLocaleSetting));
+      await tester.tap(find.text(l10n.gClose).last);
+      await tester.pumpAndSettle();
 
-      // debugPrint('\nTesting language setting button (LSB)');
-      // final Finder languageButton = find.byType(EzLocaleSetting);
+      // Reset //
 
-      // debugPrint('Testing LSB exists');
-      // expect(languageButton, findsOneWidget);
-      // await touch(tester: tester, text: languageButton);
+      debugPrint('\nTesting reset button (RB)');
+      await touch(tester: tester, finder: find.byType(EzResetButton));
 
-      // debugPrint('Testing LSB options appear');
-      // final Finder englishButton =
-      //     find.text(l10nNames.first.nameOf('en')!).last;
-      // final Finder spanishButton =
-      //     find.text(l10nNames.first.nameOf('es')!).last;
-      // final Finder frenchButton = find.text(l10nNames.first.nameOf('fr')!).last;
-      // final Finder closeBUtton = find.text(l10n.gClose).last;
+      // Verify layout
+      final bool isCupertino = !kIsWeb && (Platform.isIOS || Platform.isMacOS);
 
-      // expect(englishButton, findsOneWidget);
-      // expect(spanishButton, findsOneWidget);
-      // expect(frenchButton, findsOneWidget);
-      // expect(closeBUtton, findsOneWidget);
+      late final List<TextButton>? materialActions;
+      late final List<CupertinoDialogAction>? cupertinoActions;
 
-      // debugPrint('Testing LSB menu is dismissible');
-      // await dismissTap(tester);
-      // await touch(tester: tester, text: languageButton);
+      if (isCupertino) {
+        cupertinoActions =
+            (tester.widget(find.byType(EzAlertDialog)) as EzAlertDialog)
+                .cupertinoActions!;
+      } else {
+        materialActions =
+            (tester.widget(find.byType(EzAlertDialog)) as EzAlertDialog)
+                .materialActions! as List<TextButton>;
+      }
 
-      // debugPrint('Testing LSB functionality');
+      if (isCupertino) {
+        final List<CupertinoDialogAction> actions = cupertinoActions!;
 
-      // // Activate Spanish localizations
-      // await tester.tap(spanishButton);
-      // await tester.pumpAndSettle();
+        expect(actions.length, 2);
+        if (isLefty) {
+          expect(actions[0].child.toString(), Text(l10n.gYes).toString());
+          expect(actions[1].child.toString(), Text(l10n.gNo).toString());
+        } else {
+          expect(actions[0].child.toString(), Text(l10n.gNo).toString());
+          expect(actions[1].child.toString(), Text(l10n.gYes).toString());
+        }
+      } else {
+        final List<TextButton> actions = materialActions!;
 
-      // await touch(tester: tester, text: languageButton);
+        expect(actions.length, 2);
+        if (isLefty) {
+          expect(actions[0].child.toString(), Text(l10n.gYes).toString());
+          expect(actions[1].child.toString(), Text(l10n.gNo).toString());
+        } else {
+          expect(actions[0].child.toString(), Text(l10n.gNo).toString());
+          expect(actions[1].child.toString(), Text(l10n.gYes).toString());
+        }
+      }
 
-      // // Activate French localizations
-      // await tester.tap(frenchButton);
-      // await tester.pumpAndSettle();
+      debugPrint('No');
+      await tester.tapAt(tester.getCenter(find.text(l10n.gNo).last));
+      await tester.pumpAndSettle();
 
-      // await touch(tester: tester, text: languageButton);
-
-      // // Activate English localizations
-      // await tester.tap(englishButton);
-      // await tester.pumpAndSettle();
-
-      // // Reset //
-
-      // debugPrint('\nTesting reset button (RB)');
-      // final Finder resetButton = find.byType(EzResetButton);
-
-      // debugPrint('Testing RB exists');
-      // expect(resetButton, findsOneWidget);
-
-      // debugPrint('Testing RB activation');
-      // await touch(tester: tester, text: resetButton);
-
-      // debugPrint('Testing RB dialog options appear');
-      // final Finder noButton = find.text(l10n.gNo).last;
-      // final Finder yesButton = find.text(l10n.gYes).last;
-
-      // expect(noButton, findsOneWidget);
-      // expect(yesButton, findsOneWidget);
-
-      // // Verify layout
-      // final AlertDialog alertDialog = tester.widget(find.byType(AlertDialog));
-      // final List<Widget> actions = alertDialog.actions!;
-      // expect(actions.length, 2);
-
-      // if (isLefty) {
-      //   expect(actions[0], isA<TextButton>());
-      //   expect((actions[0] as TextButton).child!.toString(), l10n.gYes);
-      //   expect(actions[1], isA<TextButton>());
-      //   expect((actions[1] as TextButton).child!.toString(), l10n.gNo);
-      // } else {
-      //   expect(actions[0], isA<TextButton>());
-      //   expect((actions[0] as TextButton).child!.toString(), l10n.gNo);
-      //   expect(actions[1], isA<TextButton>());
-      //   expect((actions[1] as TextButton).child!.toString(), l10n.gYes);
-      // }
-
-      // debugPrint('Testing RB dialog dismiss options');
-      // await dismissTap(tester);
-      // await touch(tester: tester, text: resetButton);
-
-      // await tester.tap(noButton);
-      // await tester.pumpAndSettle();
-      // await touch(tester: tester, text: resetButton);
-
-      // debugPrint('Testing RB functionality');
-      // await tester.tap(yesButton);
-      // await tester.pumpAndSettle();
+      debugPrint('Yes');
+      await touch(tester: tester, finder: find.byType(EzResetButton));
+      await tester.tapAt(tester.getCenter(find.text(l10n.gYes).last));
+      await tester.pumpAndSettle();
 
       // Reset for next test suite  //
 
