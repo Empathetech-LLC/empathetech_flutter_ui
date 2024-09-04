33e9f6b7fb21f4fb6dbe0ca09be36227e472559f
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-03 23:41:39 -0400

    added downloadable image

--- example/integration_test/utils/consts.dart
+++ example/integration_test/utils/consts.dart
@@ -8,3 +8,6 @@ import 'package:flutter/material.dart';
 const Locale english = Locale('en');
 const Locale spanish = Locale('es');
 const Locale french = Locale('fr');
+
+const String etechGHProfileLink =
+    'https://avatars.githubusercontent.com/u/105748845?s=400&u=f239d55e2f2a37070dcdf1f3b31a579c2b0d484e&v=4';
