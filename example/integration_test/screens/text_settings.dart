6a13205533e2e8a86dc896c5a2b93c465a8bf327
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-04 22:58:38 -0400

    saving progress

--- example/integration_test/screens/text_settings.dart
+++ example/integration_test/screens/text_settings.dart
@@ -31,22 +31,108 @@ void testSuite({
 
       debugPrint('\nTesting navigation');
 
-      final Finder tsButton = find.widgetWithText(
-        ElevatedButton,
-        l10n.tsPageTitle,
+      await touch(
+        tester,
+        find.widgetWithText(ElevatedButton, l10n.tsPageTitle),
       );
-
-      expect(tsButton, findsOneWidget);
-      await touch(tester, tsButton);
 
       // Verify text loaded //
 
       debugPrint('\nValidating text');
-      debugPrint(
-        "This will probably be different because it's rich text or whatever",
+      await validateText(
+        tester,
+        l10n.tsDisplayP1 + l10n.tsDisplayLink + l10n.tsDisplayP2,
+      );
+      await validateText(
+        tester,
+        l10n.tsHeadlineP1 + l10n.tsHeadlineLink + l10n.tsHeadlineP2,
+      );
+      await validateText(
+        tester,
+        l10n.tsTitleP1 + l10n.tsTitleLink,
+      );
+      await validateText(
+        tester,
+        l10n.tsBodyP1 + l10n.tsBodyLink + l10n.tsBodyP2,
+      );
+      await validateText(
+        tester,
+        l10n.tsLabelP1 + l10n.tsLabelLink + l10n.tsLabelP2,
       );
 
-      //// Test functionality ////
+      //// Test functionality: Quick settings ////
+
+      debugPrint('\nTesting quick settings');
+
+      // Batch font updates //
+
+      debugPrint('Batch font updater');
+      await touch(tester, find.byType(DropdownMenu<String>));
+      // ToDo: Verify expected font family names
+      await dismissTap(tester);
+
+      // Batch size updates //
+
+      debugPrint('Batch font size: max');
+
+      debugPrint('Batch font size: min');
+
+      // Reset //
+
+      await testResetButton(
+        tester,
+        type: RBType.text,
+        l10n: l10n,
+        isLefty: isLefty,
+      );
+
+      //// Test functionality: Advanced settings ////
+
+      debugPrint('\nTesting advanced settings');
+
+      debugPrint('Navigation');
+      await touchAtText(tester, l10n.gAdvanced);
+
+      debugPrint('Font families');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Text sizes');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Bolding');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Italicizing');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Underlining');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Spacing letters');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Spacing words');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      debugPrint('Spacing lines');
+      debugPrint('Dropdown && max');
+      debugPrint('Link && min');
+
+      // Reset //
+
+      await testResetButton(
+        tester,
+        type: RBType.text,
+        l10n: l10n,
+        isLefty: isLefty,
+      );
 
       // Reset for next test suite  //
 
