e34dc0825ae2d6b9c01757122a95b8321c07a9f6
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-05 14:08:12 -0400

    added swap widget

--- lib/empathetech_flutter_ui.dart
+++ lib/empathetech_flutter_ui.dart
@@ -56,6 +56,7 @@ export 'src/widgets/platform_availability/back_action.dart';
 // Responsive design
 export 'src/widgets/responsive_design/row_col.dart';
 export 'src/widgets/responsive_design/swap_scaffold.dart';
+export 'src/widgets/responsive_design/swap_widget.dart';
 export 'src/widgets/responsive_design/transitions.dart';
 
 // Screen reader support
