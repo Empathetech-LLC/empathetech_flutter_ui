f733b682cad0b0380129bed8daef656e349a1831
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-04 23:31:03 -0400

    saving progress

--- example/integration_test/screens/text_settings.dart
+++ example/integration_test/screens/text_settings.dart
@@ -7,7 +7,10 @@ import '../utils/export.dart';
 
 import 'package:example/main.dart';
 
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
@@ -63,19 +66,32 @@ void testSuite({
       //// Test functionality: Quick settings ////
 
       debugPrint('\nTesting quick settings');
+      final bool isCupertino = !kIsWeb && (Platform.isIOS || Platform.isMacOS);
 
       // Batch font updates //
 
       debugPrint('Batch font updater');
       await touch(tester, find.byType(DropdownMenu<String>));
-      // ToDo: Verify expected font family names
+      await touchAtText(tester, roboto);
       await dismissTap(tester);
 
       // Batch size updates //
 
       debugPrint('Batch font size: max');
+      for (int i = 0; i < 15; i++) {
+        await touchAt(
+          tester,
+          find.byIcon(isCupertino ? CupertinoIcons.add : Icons.add),
+        );
+      }
 
       debugPrint('Batch font size: min');
+      for (int i = 0; i < 15; i++) {
+        await touchAt(
+          tester,
+          find.byIcon(isCupertino ? CupertinoIcons.minus : Icons.remove),
+        );
+      }
 
       // Reset //
 
@@ -90,40 +106,53 @@ void testSuite({
 
       debugPrint('\nTesting advanced settings');
 
-      debugPrint('Navigation');
+      debugPrint('\nNavigation');
       await touchAtText(tester, l10n.gAdvanced);
 
-      debugPrint('Font families');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
+      debugPrint('\nDisplay');
+      await touch(tester, find.byType(DropdownMenu<String>).first);
+      await touchAtText(tester, l10n.tsDisplay.toLowerCase());
+      await touch(
+        tester,
+        find.widgetWithText(EzLink, l10n.tsDisplay.toLowerCase()),
+      );
+      await testAdvancedOptions(tester);
 
-      debugPrint('Text sizes');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
+      debugPrint('\nHeadline');
+      await touch(tester, find.byType(DropdownMenu<String>).first);
+      await touchAtText(tester, l10n.tsHeadline.toLowerCase());
+      await touch(
+        tester,
+        find.widgetWithText(EzLink, l10n.tsHeadline.toLowerCase()),
+      );
+      await testAdvancedOptions(tester);
 
-      debugPrint('Bolding');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
+      debugPrint('\nTitle');
+      await touch(tester, find.byType(DropdownMenu<String>).first);
+      await touchAtText(tester, l10n.tsTitle.toLowerCase());
+      await touch(
+        tester,
+        find.widgetWithText(EzLink, l10n.tsTitle.toLowerCase()),
+      );
+      await testAdvancedOptions(tester);
 
-      debugPrint('Italicizing');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
+      debugPrint('\nBody');
+      await touch(tester, find.byType(DropdownMenu<String>).first);
+      await touchAtText(tester, l10n.tsBody.toLowerCase());
+      await touch(
+        tester,
+        find.widgetWithText(EzLink, l10n.tsBody.toLowerCase()),
+      );
+      await testAdvancedOptions(tester);
 
-      debugPrint('Underlining');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
-
-      debugPrint('Spacing letters');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
-
-      debugPrint('Spacing words');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
-
-      debugPrint('Spacing lines');
-      debugPrint('Dropdown && max');
-      debugPrint('Link && min');
+      debugPrint('\nLabel');
+      await touch(tester, find.byType(DropdownMenu<String>).first);
+      await touchAtText(tester, l10n.tsLabel.toLowerCase());
+      await touch(
+        tester,
+        find.widgetWithText(EzLink, l10n.tsLabel.toLowerCase()),
+      );
+      await testAdvancedOptions(tester);
 
       // Reset //
 
@@ -139,3 +168,18 @@ void testSuite({
       await goBack(tester, l10n);
       debugPrint('\nText settings test suite complete\n');
     });
+
+Future<void> testAdvancedOptions(WidgetTester tester) async {
+  debugPrint('Font family');
+  await touch(tester, find.byType(DropdownMenu<String>).last);
+  await touchAtText(tester, roboto);
+  await dismissTap(tester);
+
+  debugPrint('Font size');
+  debugPrint('Bold');
+  debugPrint('Italics');
+  debugPrint('Underline');
+  debugPrint('Letter spacing');
+  debugPrint('Word spacing');
+  debugPrint('Line height');
+}
