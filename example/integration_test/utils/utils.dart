7afadca2749ac8c9865c27380e2a39bc25ee4ff2
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-04 21:13:13 -0400

    added hold functions

--- example/integration_test/utils/utils.dart
+++ example/integration_test/utils/utils.dart
@@ -40,10 +40,24 @@ Future<void> touch(WidgetTester tester, Finder finder) async {
   await tester.pumpAndSettle();
 }
 
+/// Find, touch, hold, and settle a touch-point
+Future<void> hold(WidgetTester tester, Finder finder) async {
+  await tester.ensureVisible(finder);
+  await tester.longPress(finder);
+  await tester.pumpAndSettle();
+}
+
 /// Find, touch, and settle a target
 Future<void> touchAt(WidgetTester tester, Finder finder) async {
   await tester.ensureVisible(finder);
   await tester.tapAt(tester.getCenter(finder));
+  await tester.pumpAndSettle();
+}
+
+/// Find, touch, hold, and settle a target
+Future<void> holdAt(WidgetTester tester, Finder finder) async {
+  await tester.ensureVisible(finder);
+  await tester.longPressAt(tester.getCenter(finder));
   await tester.pumpAndSettle();
 }
 
@@ -52,6 +66,14 @@ Future<void> touchAtText(WidgetTester tester, String text) async {
   final Finder finder = find.text(text).last;
   await tester.ensureVisible(finder);
   await tester.tapAt(tester.getCenter(finder));
+  await tester.pumpAndSettle();
+}
+
+/// Find, touch, hold, and settle a text target
+Future<void> holdAtText(WidgetTester tester, String text) async {
+  final Finder finder = find.text(text).last;
+  await tester.ensureVisible(finder);
+  await tester.longPressAt(tester.getCenter(finder));
   await tester.pumpAndSettle();
 }
 
