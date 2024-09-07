199c79007944d8784d5a4043ed6904edf1030173
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-07 18:26:31 -0400

    added locales

---
+++ lib/src/functions/testing/consts.dart
@@ -0,0 +1,15 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:flutter/material.dart';
+
+/// 'en'
+const Locale english = Locale('en');
+
+/// 'es'
+const Locale spanish = Locale('es');
+
+/// 'fr'
+const Locale french = Locale('fr');
