fae846894c8af9fd56837a4b72e5593f03055475
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-14 15:23:55 -0400

    optional device flag

--- example/integration_test/one_with_everything.sh
+++ example/integration_test/one_with_everything.sh
@@ -2,11 +2,30 @@
 
 set -e
 
-prefix=$HOME/repos/empathetech_flutter_ui/example
+## Setup ##
 
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/default_test.dart
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/es_test.dart
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/fr_test.dart
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/lefty_test.dart
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/minimum_config_test.dart
-flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/maximum_config_test.dart
+prefix=$HOME/repos/empathetech_flutter_ui/example
+device=""
+
+# Gather flag variables
+while [[ "$1" != "" ]]; do
+  case $1 in
+    --device ) shift
+               device="-d $1"
+               ;;
+    * ) echo "Invalid input. Aborting."; exit 1
+  esac
+  shift
+done
+
+## Tests ##
+
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/default_test.dart $device
+
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/es_test.dart $device
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/fr_test.dart $device
+
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/lefty_test.dart $device
+
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/minimum_config_test.dart $device
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/maximum_config_test.dart $device
