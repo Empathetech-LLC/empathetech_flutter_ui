0ec09bd5c1ca60de607d6a89405c5f4b5604541d
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-13 21:17:54 -0400

    exact paths

--- example/integration_test/one_with_everything.sh
+++ example/integration_test/one_with_everything.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/default_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/es_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/fr_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/lefty_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/minimum_config_test.dart
-flutter drive --driver=test_driver/integration_test_driver.dart --target=integration_test/maximum_config_test.dart
+prefix=$HOME/repos/empathetech_flutter_ui/example
+
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/default_test.dart
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/es_test.dart
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/fr_test.dart
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/lefty_test.dart
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/minimum_config_test.dart
+flutter drive --driver=$prefix/test_driver/integration_test_driver.dart --target=$prefix/integration_test/maximum_config_test.dart
