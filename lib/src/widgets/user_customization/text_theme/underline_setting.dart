03f720ab6a0ceb6d68ba2f269b0ebfb351e988d5
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-23 12:33:04 -0400

    matched bold updates

--- lib/src/widgets/user_customization/text_theme/underline_setting.dart
+++ lib/src/widgets/user_customization/text_theme/underline_setting.dart
@@ -31,11 +31,13 @@ class EzUnderlineSetting extends StatefulWidget {
 class _EzUnderlineSettingState extends State<EzUnderlineSetting> {
   // Gather the theme data //
 
-  late bool isUnderlined = EzConfig.get(widget.configKey) ?? false;
-
   late final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
+  late bool isUnderlined = EzConfig.get(widget.configKey) ?? false;
+
   // Define the build //
+
+  late final String tooltip = EFUILang.of(context)!.tsUnderline;
 
   void swapState() async {
     isUnderlined = !isUnderlined;
@@ -44,26 +46,30 @@ class _EzUnderlineSettingState extends State<EzUnderlineSetting> {
     setState(() {});
   }
 
-  late final String tooltip = EFUILang.of(context)!.tsUnderline;
-
   // Return the build //
 
   @override
   Widget build(BuildContext context) {
     return isUnderlined
         ? IconButton(
-            icon: const Icon(Icons.format_underline),
-            color: colorScheme.primary,
-            iconSize: widget.size,
+            style: IconButton.styleFrom(
+              foregroundColor: colorScheme.primary,
+              side: BorderSide(color: colorScheme.primaryContainer),
+              iconSize: widget.size,
+            ),
             onPressed: swapState,
             tooltip: tooltip,
+            icon: const Icon(Icons.format_underline),
           )
         : IconButton(
-            icon: const Icon(Icons.format_underline_outlined),
-            color: colorScheme.outline,
-            iconSize: widget.size,
+            style: IconButton.styleFrom(
+              foregroundColor: colorScheme.outline,
+              side: BorderSide(color: colorScheme.primaryContainer),
+              iconSize: widget.size,
+            ),
             onPressed: swapState,
             tooltip: tooltip,
+            icon: const Icon(Icons.format_underline_outlined),
           );
   }
 }
