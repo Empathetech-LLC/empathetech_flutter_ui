8356531c0a5b1b37a23a6268bb1ca43c3fb05a9e
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-29 17:47:50 -0400

    supports is lefty

--- example/lib/widgets/byo.dart
+++ example/lib/widgets/byo.dart
@@ -9,22 +9,26 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class BYOButton extends StatelessWidget {
-  final EFUILang l10n;
-
-  const BYOButton(this.l10n, {super.key});
+  const BYOButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final EFUILang l10n = EFUILang.of(context)!;
+    final bool isLefty = EzConfig.get(isLeftyKey) ?? false;
+
     final String text = l10n.gBYO;
+
+    final Widget icon = Icon(
+      LineIcons.github,
+      size: Theme.of(context).textTheme.titleLarge?.fontSize,
+      color: Theme.of(context).colorScheme.primary,
+    );
 
     return MenuItemButton(
       onPressed: () => launchUrl(Uri.parse(efuiGitHub)),
       semanticsLabel: '$text: ${l10n.gEFUISourceHint}',
-      leadingIcon: Icon(
-        LineIcons.github,
-        size: Theme.of(context).textTheme.titleLarge?.fontSize,
-        color: Theme.of(context).colorScheme.primary,
-      ),
+      leadingIcon: isLefty ? icon : null,
+      trailingIcon: isLefty ? null : icon,
       child: Text(text),
     );
   }
