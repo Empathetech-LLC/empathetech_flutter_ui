115f0a7bc432ae75b94b665a88bf19f8f1ff66f8
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-29 12:11:41 -0400

    simplified

--- lib/src/widgets/helpers/sizing.dart
+++ lib/src/widgets/helpers/sizing.dart
@@ -7,56 +7,21 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
-class EzMargin extends EdgeInsets {
-  /// EdgeInsets.all(EzConfig margin)
-  EzMargin() : super.all(EzConfig.get(marginKey));
+class EzInsets extends EdgeInsets {
+  /// [EdgeInsets].symmetric(horizontal: [base], vertical: [base] / 2)
+  const EzInsets.col(double base)
+      : super.symmetric(horizontal: base, vertical: base / 2);
 
-  /// EdgeInsets.symmetric(horizontal: full margin, vertical: half margin)
-  EzMargin.col()
-      : super.symmetric(
-          horizontal: EzConfig.get(marginKey),
-          vertical: EzConfig.get(marginKey) / 2,
-        );
+  /// [EdgeInsets].symmetric(horizontal: [base] / 2, vertical: [base])
+  const EzInsets.row(double base)
+      : super.symmetric(horizontal: base / 2, vertical: base);
 
-  /// EdgeInsets.symmetric(horizontal: half margin, vertical: full margin)
-  EzMargin.row()
-      : super.symmetric(
-          horizontal: EzConfig.get(marginKey) / 2,
-          vertical: EzConfig.get(marginKey),
-        );
+  /// [EdgeInsets].all([base] / 2)
+  const EzInsets.wrap(double base) : super.all(base / 2);
 
-  /// EdgeInsets.all(Half EzConfig margin)
-  EzMargin.wrap() : super.all(EzConfig.get(marginKey) / 2);
-}
-
-class EzPadding extends EdgeInsets {
-  /// EdgeInsets.all(EzConfig padding)
-  EzPadding() : super.all(EzConfig.get(paddingKey));
-
-  /// EdgeInsets.symmetric(horizontal: full padding, vertical: half padding)
-  EzPadding.col()
-      : super.symmetric(
-          horizontal: EzConfig.get(paddingKey),
-          vertical: EzConfig.get(paddingKey) / 2,
-        );
-
-  /// EdgeInsets.symmetric(horizontal: half padding, vertical: full padding)
-  EzPadding.row()
-      : super.symmetric(
-          horizontal: EzConfig.get(paddingKey) / 2,
-          vertical: EzConfig.get(paddingKey),
-        );
-
-  /// EdgeInsets.all(Half EzConfig padding)
-  EzPadding.wrap() : super.all(EzConfig.get(paddingKey) / 2);
-
-  /// EdgeInsets padding everywhere but bottom
-  EzPadding.menu()
-      : super.only(
-          left: EzConfig.get(paddingKey),
-          right: EzConfig.get(paddingKey),
-          top: EzConfig.get(paddingKey),
-        );
+  /// [EdgeInsets] .only(left: [width], right: [width], top: [height])
+  const EzInsets.menu({required double width, required double height})
+      : super.only(left: width, right: width, top: height);
 }
 
 /// BorderRadius.all(Radius.circular(4.0))
@@ -65,7 +30,7 @@ const BorderRadius ezRoundEdge = BorderRadius.all(Radius.circular(4.0));
 /// BorderRadius.all(Radius.circular(64.0))
 const BorderRadius ezPillShape = BorderRadius.all(Radius.circular(64.0));
 
-/// threeQs = [widthOf] context * (2 / 3)
+/// threeQs = [widthOf] context * 0.75
 /// min: threeQs, max: min(threeQs, [smallBreakpoint])
 BoxConstraints textFieldConstraints(BuildContext context) {
   final double threeQs = widthOf(context) * 0.75;
