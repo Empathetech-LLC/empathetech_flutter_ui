a76d9f8d68b8a00da754d5ccfe034d954754d1e1
empathictech <105748722+empathictech@users.noreply.github.com>
2024-10-03 13:48:25 -0400

    reverted

--- lib/src/functions/dialogs.dart
+++ lib/src/functions/dialogs.dart
@@ -99,9 +99,6 @@ Future<dynamic> ezColorPicker(
           opacityThumbRadius: padding,
           opacityTrackHeight: padding * 2,
           showColorCode: true,
-          copyPasteBehavior: const ColorPickerCopyPasteBehavior(
-            editFieldCopyButton: false,
-          ),
         ),
         materialActions: ezMaterialActions(
           context: context,
