cab4c79088098ac40e5bbd5ad7d3123085f4174f
empathictech <105748722+empathictech@users.noreply.github.com>
2024-10-07 15:36:02 -0400

    added box fit strings

--- lib/src/consts/config_keys.dart
+++ lib/src/consts/config_keys.dart
@@ -583,9 +583,6 @@ const String lightBackgroundImageKey = 'lightBackgroundImage';
 const String darkColorSchemeImageKey = 'darkColorSchemeImage';
 const String darkBackgroundImageKey = 'darkBackgroundImage';
 
-/// 'Fit'
-const String boxFitSuffix = 'Fit';
-
 /// [light, dark]
 ///      X
 /// [colorSchemeImageKey, pageImageKey]
@@ -599,8 +596,34 @@ const Map<String, Type> imageKeys = <String, Type>{
   darkBackgroundImageKey: String,
 };
 
+// Image settings' values //
+
 /// Allows for app images whose default is an asset to be "null"
 const String noImageValue = 'noImage';
+
+/// 'Fit'
+const String boxFitSuffix = 'Fit';
+
+/// 'contain'
+const String contain = 'contain';
+
+/// 'cover'
+const String cover = 'cover';
+
+/// 'fill'
+const String fill = 'fill';
+
+/// 'fitWidth'
+const String fitWidth = 'fitWidth';
+
+/// 'fitHeight'
+const String fitHeight = 'fitHeight';
+
+/// 'none'
+const String none = 'none';
+
+/// 'scaleDown'
+const String scaleDown = 'scaleDown';
 
 // Global trackers //
 
