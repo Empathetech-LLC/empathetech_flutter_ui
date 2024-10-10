4b7a694047dcff50b26d19b0dbfa221be7af94f5
empathictech <105748722+empathictech@users.noreply.github.com>
2024-10-10 17:22:44 -0400

    shareable consts

--- lib/src/widgets/user_customization/color_scheme/mono_chrome_colors_setting.dart
+++ lib/src/widgets/user_customization/color_scheme/mono_chrome_colors_setting.dart
@@ -8,6 +8,68 @@ import '../../../../empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
+/// Custom [ColorScheme.highContrastDark]
+const ColorScheme ezHighContrastDark = ColorScheme.highContrastDark(
+  // Primary
+  primary: Colors.white,
+  onPrimary: Colors.black,
+  onPrimaryContainer: Colors.black,
+  onPrimaryFixed: Colors.black,
+  onPrimaryFixedVariant: Colors.black,
+
+  // Secondary
+  secondary: Colors.white,
+  onSecondary: Colors.black,
+  onSecondaryContainer: Colors.black,
+  onSecondaryFixed: Colors.black,
+  onSecondaryFixedVariant: Colors.black,
+
+  // Tertiary
+  tertiary: Colors.white,
+  onTertiary: Colors.black,
+  onTertiaryContainer: Colors.black,
+  onTertiaryFixed: Colors.black,
+  onTertiaryFixedVariant: Colors.black,
+
+  // Surface
+  surface: Colors.black,
+  onSurface: Colors.white,
+  surfaceContainer: empathOffBlack,
+  inversePrimary: Colors.white,
+  surfaceTint: Colors.transparent,
+);
+
+/// Custom [ColorScheme.highContrastLight]
+const ColorScheme ezHighContrastLight = ColorScheme.highContrastLight(
+  // Primary
+  primary: Colors.black,
+  onPrimary: Colors.white,
+  onPrimaryContainer: Colors.white,
+  onPrimaryFixed: Colors.white,
+  onPrimaryFixedVariant: Colors.white,
+
+  // Secondary
+  secondary: Colors.black,
+  onSecondary: Colors.white,
+  onSecondaryContainer: Colors.white,
+  onSecondaryFixed: Colors.white,
+  onSecondaryFixedVariant: Colors.white,
+
+  // Tertiary
+  tertiary: Colors.black,
+  onTertiary: Colors.white,
+  onTertiaryContainer: Colors.white,
+  onTertiaryFixed: Colors.white,
+  onTertiaryFixedVariant: Colors.white,
+
+  // Surface
+  surface: Colors.white,
+  onSurface: Colors.black,
+  surfaceContainer: empathOffWhite,
+  inversePrimary: Colors.black,
+  surfaceTint: Colors.transparent,
+);
+
 class EzMonoChromeColorsSetting extends StatelessWidget {
   final ColorScheme dark;
   final ColorScheme light;
@@ -16,64 +78,8 @@ class EzMonoChromeColorsSetting extends StatelessWidget {
   /// Uses flutter_platform_widgets, specifically [PlatformTheme]
   const EzMonoChromeColorsSetting({
     super.key,
-    this.dark = const ColorScheme.highContrastDark(
-      // Primary
-      primary: Colors.white,
-      onPrimary: Colors.black,
-      onPrimaryContainer: Colors.black,
-      onPrimaryFixed: Colors.black,
-      onPrimaryFixedVariant: Colors.black,
-
-      // Secondary
-      secondary: Colors.white,
-      onSecondary: Colors.black,
-      onSecondaryContainer: Colors.black,
-      onSecondaryFixed: Colors.black,
-      onSecondaryFixedVariant: Colors.black,
-
-      // Tertiary
-      tertiary: Colors.white,
-      onTertiary: Colors.black,
-      onTertiaryContainer: Colors.black,
-      onTertiaryFixed: Colors.black,
-      onTertiaryFixedVariant: Colors.black,
-
-      // Surface
-      surface: Colors.black,
-      onSurface: Colors.white,
-      surfaceContainer: empathOffBlack,
-      inversePrimary: Colors.white,
-      surfaceTint: Colors.transparent,
-    ),
-    this.light = const ColorScheme.highContrastLight(
-      // Primary
-      primary: Colors.black,
-      onPrimary: Colors.white,
-      onPrimaryContainer: Colors.white,
-      onPrimaryFixed: Colors.white,
-      onPrimaryFixedVariant: Colors.white,
-
-      // Secondary
-      secondary: Colors.black,
-      onSecondary: Colors.white,
-      onSecondaryContainer: Colors.white,
-      onSecondaryFixed: Colors.white,
-      onSecondaryFixedVariant: Colors.white,
-
-      // Tertiary
-      tertiary: Colors.black,
-      onTertiary: Colors.white,
-      onTertiaryContainer: Colors.white,
-      onTertiaryFixed: Colors.white,
-      onTertiaryFixedVariant: Colors.white,
-
-      // Surface
-      surface: Colors.white,
-      onSurface: Colors.black,
-      surfaceContainer: empathOffWhite,
-      inversePrimary: Colors.black,
-      surfaceTint: Colors.transparent,
-    ),
+    this.dark = ezHighContrastDark,
+    this.light = ezHighContrastLight,
   });
 
   @override
