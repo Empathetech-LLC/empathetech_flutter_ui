8e4bb174cf6cde7d629a777ff3fc628f7d1a17a5
empathictech <105748722+empathictech@users.noreply.github.com>
2024-10-19 13:39:59 -0400

    updated imports

--- lib/src/functions/testing/widgets/locale_setting.dart
+++ lib/src/functions/testing/widgets/locale_setting.dart
@@ -3,7 +3,6 @@
  * See LICENSE for distribution and usage details.
  */
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
