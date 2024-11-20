09bf1bf8419301023ca86b60828bba45a9dcc240
empathictech <105748722+empathictech@users.noreply.github.com>
2024-11-20 11:18:53 -0500

    added settings home sample

---
+++ lib/src/sample_screens/settings_home.dart
@@ -0,0 +1,177 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../../empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingsHome extends StatefulWidget {
+  /// For [EzScreen.useImageDecoration]
+  final bool useImageDecoration;
+
+  /// Optional [EzWarning.header] passthrough
+  final Widget? warningHeader;
+
+  /// Widgets to be added below language and above any present routes
+  /// Do not include a trailing spacer, one will be provided
+  final List<Widget>? additionalSettings;
+
+  /// Nullable [goNamed] path to the text settings screen
+  /// If null, no button will appear
+  final String? textSettingsPath;
+
+  /// Nullable [goNamed] path to the layout settings screen
+  /// If null, no button will appear
+  final String? layoutSettingsPath;
+
+  /// Nullable [goNamed] path to the color settings screen
+  /// If null, no button will appear
+  final String? colorSettingsPath;
+
+  /// Nullable [goNamed] path to the image settings screen
+  /// If null, no button will appear
+  final String? imageSettingsPath;
+
+  /// Widgets to be added below any present routes and above randomize
+  /// Do not include a trailing spacer, one will be provided
+  final List<Widget>? additionalRoutes;
+
+  /// Widgets to be added below reset
+  /// Do not include a trailing spacer, one will be provided
+  final List<Widget>? footer;
+
+  const SettingsHome({
+    super.key,
+    this.useImageDecoration = true,
+    this.warningHeader,
+    this.additionalSettings,
+    required this.textSettingsPath,
+    required this.layoutSettingsPath,
+    required this.colorSettingsPath,
+    required this.imageSettingsPath,
+    this.additionalRoutes,
+    this.footer,
+  });
+
+  @override
+  State<SettingsHome> createState() => _SettingsHomeState();
+}
+
+class _SettingsHomeState extends State<SettingsHome> {
+  // Gather the theme data //
+
+  static const EzSpacer spacer = EzSpacer();
+  static const EzSeparator separator = EzSeparator();
+
+  late final EFUILang l10n = EFUILang.of(context)!;
+
+  // Set the page title //
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setPageTitle(l10n.ssPageTitle, Theme.of(context).colorScheme.primary);
+  }
+
+  // Return the build //
+
+  @override
+  Widget build(BuildContext context) {
+    return EzScreen(
+      child: EzScrollView(
+        children: <Widget>[
+          // Functionality disclaimer
+          EzWarning(
+            header: widget.warningHeader,
+            body: kIsWeb ? l10n.ssSettingsGuideWeb : l10n.ssSettingsGuide,
+          ),
+          separator,
+
+          // Global settings
+          const EzDominantHandSwitch(),
+          spacer,
+
+          const EzThemeModeSwitch(),
+          spacer,
+
+          const EzLocaleSetting(),
+          separator,
+
+          if (widget.additionalSettings != null) ...<Widget>[
+            ...widget.additionalSettings!,
+            separator,
+          ],
+
+          // Text settings
+          if (widget.textSettingsPath != null) ...<Widget>[
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(widget.textSettingsPath!),
+              icon: const Icon(Icons.navigate_next),
+              label: l10n.tsPageTitle,
+            ),
+            spacer,
+          ],
+
+          // Layout settings
+          if (widget.layoutSettingsPath != null) ...<Widget>[
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(widget.layoutSettingsPath!),
+              icon: const Icon(Icons.navigate_next),
+              label: l10n.lsPageTitle,
+            ),
+            spacer,
+          ],
+
+          // Color settings
+          if (widget.colorSettingsPath != null) ...<Widget>[
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(widget.colorSettingsPath!),
+              icon: const Icon(Icons.navigate_next),
+              label: l10n.csPageTitle,
+            ),
+            spacer,
+          ],
+
+          // Image settings
+          if (widget.imageSettingsPath != null) ...<Widget>[
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(widget.imageSettingsPath!),
+              icon: const Icon(Icons.navigate_next),
+              label: l10n.isPageTitle,
+            ),
+            spacer,
+          ],
+
+          // Turn trailing spacer (if present) into a separator
+          if (widget.textSettingsPath != null ||
+              widget.layoutSettingsPath != null ||
+              widget.colorSettingsPath != null ||
+              widget.imageSettingsPath != null)
+            spacer,
+
+          if (widget.additionalRoutes != null) ...<Widget>[
+            ...widget.additionalRoutes!,
+            separator,
+          ],
+
+          // Feeling lucky
+          const EzConfigRandomizer(),
+          separator,
+
+          // Reset button
+          const EzResetButton(),
+          separator,
+
+          if (widget.footer != null) ...<Widget>[
+            ...widget.footer!,
+            separator,
+          ],
+        ],
+      ),
+    );
+  }
+}
