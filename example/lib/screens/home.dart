7917c761333491e0b677a2c2588efca86975840d
empathictech <105748722+empathictech@users.noreply.github.com>
2024-11-20 13:24:58 -0500

    saving progress

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -3,6 +3,7 @@
  * See LICENSE for distribution and usage details.
  */
 
+import '../utils/export.dart';
 import '../widgets/export.dart';
 
 import 'package:flutter/material.dart';
@@ -18,16 +19,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // Gather the theme data //
 
+  static const EzSpacer spacer = EzSpacer();
+  static const EzSpacer rowSpacer = EzSpacer(vertical: false);
   static const EzSeparator separator = EzSeparator();
 
   late final EFUILang l10n = EFUILang.of(context)!;
+
+  // Define build data //
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController orgController = TextEditingController();
 
   // Set the page title //
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    setPageTitle(efuiL, Theme.of(context).colorScheme.primary);
+    setPageTitle(appTitle, Theme.of(context).colorScheme.primary);
   }
 
   // Return the build //
@@ -35,20 +43,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return OpenUIScaffold(
+      title: 'Builder',
       body: EzScreen(
         child: EzScrollView(
           children: <Widget>[
-            Center(
-              child: Text(
-                'Sup',
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
+            Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // App name
+                  EzRow(
+                    children: <Widget>[
+                      const Text('App Name:'),
+                      rowSpacer,
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your app name',
+                        ),
+                      ),
+                    ],
+                  ),
+                  spacer,
+
+                  // Organization name
+                  EzRow(
+                    children: <Widget>[
+                      const Text('Organization Name:'),
+                      rowSpacer,
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your organization name',
+                        ),
+                      ),
+                    ],
+                  ),
+                  separator,
+                ],
               ),
-            ),
-            separator,
+            )
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    orgController.dispose();
+    super.dispose();
   }
 }
