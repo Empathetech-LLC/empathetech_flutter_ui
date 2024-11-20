cc7f3ecb44dd4a23d795e6c52a3307a7bb11583e
empathictech <105748722+empathictech@users.noreply.github.com>
2024-11-20 11:18:30 -0500

    new home screen coming

---
+++ example/lib/screens/settings/settings_home.dart
@@ -0,0 +1,26 @@
+/* open_ui
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:example/screens/export.dart';
+
+import '../../widgets/export.dart';
+
+import 'package:flutter/material.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+class SettingsHomeScreen extends StatelessWidget {
+  const SettingsHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const OpenUIScaffold(
+        body: SettingsHome(
+          warningHeader: EzUpdater(),
+          textSettingsPath: textSettingsPath,
+          layoutSettingsPath: layoutSettingsPath,
+          colorSettingsPath: colorSettingsPath,
+          imageSettingsPath: imageSettingsPath,
+        ),
+      );
+}
