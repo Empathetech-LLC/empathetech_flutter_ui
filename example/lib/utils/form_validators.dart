cc0f50fd06e458212a5afa508c5b091bb66307c6
empathictech <105748722+empathictech@users.noreply.github.com>
2024-11-20 14:30:50 -0500

    initial commit

---
+++ example/lib/utils/form_validators.dart
@@ -0,0 +1,20 @@
+/* open_ui
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+/// Allows letters (upper and lower case) and underscores
+String? validateAppName(String? value) {
+  final RegExp pattern = RegExp(r'^[a-z0-9_]+$');
+
+  return (value != null && !pattern.hasMatch(value))
+      ? 'Only lowercase letters, numbers, and underscores are allowed'
+      : null;
+}
+
+/// Validates name.extension domains
+String? validateDomain(String? value) {
+  final RegExp pattern = RegExp(r'^[a-z0-9_]+\.[a-z]+$');
+
+  return (value != null && !pattern.hasMatch(value)) ? 'Invalid domain' : null;
+}
