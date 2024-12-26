b69029a5bedf9175ce7d9b70813b2c807154f984
empathictech <105748722+empathictech@users.noreply.github.com>
2024-12-26 13:04:10 -0800

    saving progress

--- example/lib/structs/eag_config.dart
+++ example/lib/structs/eag_config.dart
@@ -3,22 +3,82 @@
  * See LICENSE for distribution and usage details.
  */
 
-import 'dart:convert';
-import 'package:flutter/foundation.dart';
+const String appNameKey = 'appName';
+const String publisherNameKey = 'publisherName';
+const String domainNameKey = 'domainName';
+const String textSettingsKey = 'textSettings';
+const String layoutSettingsKey = 'layoutSettings';
+const String colorSettingsKey = 'colorSettings';
+const String imageSettingsKey = 'imageSettings';
+const String vsCodeConfigKey = 'vsCodeConfig';
+const String appDefaultsKey = 'appDefaults';
 
 class EAGConfig {
-  EAGConfig();
+  final String appName;
+  final String publisherName;
+  final String domainName;
+
+  final bool textSettings;
+  final bool layoutSettings;
+  final bool colorSettings;
+  final bool imageSettings;
+
+  final String? vsCodeConfig;
+
+  final Map<String, dynamic> appDefaults;
+
+  EAGConfig({
+    required this.appName,
+    required this.publisherName,
+    required this.domainName,
+    required this.textSettings,
+    required this.layoutSettings,
+    required this.colorSettings,
+    required this.imageSettings,
+    this.vsCodeConfig,
+    required this.appDefaults,
+  });
 
   Map<String, dynamic> toJson() {
-    return <String, dynamic>{};
+    return <String, dynamic>{
+      appNameKey: appName,
+      publisherNameKey: publisherName,
+      domainNameKey: domainName,
+      textSettingsKey: textSettings,
+      layoutSettingsKey: layoutSettings,
+      colorSettingsKey: colorSettings,
+      imageSettingsKey: imageSettings,
+      vsCodeConfigKey: vsCodeConfig,
+      appDefaultsKey: ,
+    };
   }
 
   factory EAGConfig.fromJson(dynamic json) {
-    return EAGConfig();
+    return EAGConfig(
+      appName: json[appNameKey] as String,
+      publisherName: json[publisherNameKey] as String,
+      domainName: json[domainNameKey] as String,
+      textSettings: json[textSettingsKey] as bool,
+      layoutSettings: json[layoutSettingsKey] as bool,
+      colorSettings: json[colorSettingsKey] as bool,
+      imageSettings: json[imageSettingsKey] as bool,
+      vsCodeConfig: json[vsCodeConfigKey] as String,
+      appDefaults: ,
+    );
   }
 
   @override
   String toString() {
-    return super.toString();
+    return '''{
+  $appNameKey: $appName,
+  $publisherNameKey: $publisherName,
+  $domainNameKey: $domainName,
+  $textSettingsKey: $textSettings,
+  $layoutSettingsKey: $layoutSettings,
+  $colorSettingsKey: $colorSettings,
+  $imageSettingsKey: $imageSettings,
+  $vsCodeConfigKey: $vsCodeConfig,
+  $appDefaultsKey: BLARG,
+}''';
   }
 }
