f37ff0f1cefc7cdd665cb366a8e247c877a7852d
empathictech <105748722+empathictech@users.noreply.github.com>
2025-01-08 12:27:16 -0800

    made it actually work

--- lib/src/functions/helpers_io.dart
+++ lib/src/functions/helpers_io.dart
@@ -3,8 +3,20 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 /// Get the current [TargetPlatform]
-TargetPlatform getHostPlatform(BuildContext context) =>
-    Theme.of(context).platform;
+TargetPlatform getHostPlatform(BuildContext context) {
+  if (Platform.isAndroid) {
+    return TargetPlatform.android;
+  } else if (Platform.isIOS) {
+    return TargetPlatform.iOS;
+  } else if (Platform.isMacOS) {
+    return TargetPlatform.macOS;
+  } else if (Platform.isWindows) {
+    return TargetPlatform.windows;
+  } else {
+    return TargetPlatform.linux;
+  }
+}
