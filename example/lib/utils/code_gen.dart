5a5dfdec3472a4978c471ba236e4b85ceddf0e1c
empathictech <105748722+empathictech@users.noreply.github.com>
2025-01-14 16:24:53 -0800

    version bump

--- example/lib/utils/code_gen.dart
+++ example/lib/utils/code_gen.dart
@@ -196,7 +196,7 @@ dependencies:
   url_launcher: ^6.3.1
 
   # Community
-  empathetech_flutter_ui: ^8.0.0-dev.4
+  empathetech_flutter_ui: ^8.0.0-dev.5
   ${config.supportEmail != null ? 'feedback: ^3.1.0' : ''}
   flutter_localized_locales: ^2.0.5
   flutter_platform_widgets: ^7.0.1
