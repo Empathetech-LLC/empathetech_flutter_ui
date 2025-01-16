7aeeee5e07b7cdd2df1d58f1d80bc5191980d20a
empathictech <105748722+empathictech@users.noreply.github.com>
2025-01-16 13:31:28 -0800

    added text background

--- lib/src/widgets/helpers/tool_tipper.dart
+++ lib/src/widgets/helpers/tool_tipper.dart
@@ -8,30 +8,46 @@ import '../../../empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-class EzToolTipper extends StatefulWidget {
+class EzToolTipper extends StatelessWidget {
+  /// [message] to be displayed in the [Tooltip]
   final String message;
 
+  /// Whether to include an [EzTextBackground]
+  final bool textBackground;
+
   /// Classic question mark tool tip
-  const EzToolTipper(this.message, {super.key});
+  const EzToolTipper(this.message, {super.key, this.textBackground = true});
 
-  @override
-  State<EzToolTipper> createState() => _EzToolTipperState();
-}
-
-class _EzToolTipperState extends State<EzToolTipper> {
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      waitDuration: Duration.zero,
-      enableTapToDismiss: false,
-      message: widget.message,
-      child: Container(
-        decoration: const BoxDecoration(shape: BoxShape.circle),
-        child: EzIcon(
-          PlatformIcons(context).helpOutline,
-          color: Theme.of(context).colorScheme.outline,
-        ),
-      ),
-    );
+    return textBackground
+        ? EzTextBackground(
+            Tooltip(
+              waitDuration: Duration.zero,
+              enableTapToDismiss: false,
+              message: message,
+              child: Container(
+                decoration: const BoxDecoration(shape: BoxShape.circle),
+                child: EzIcon(
+                  PlatformIcons(context).helpOutline,
+                  color: Theme.of(context).colorScheme.outline,
+                ),
+              ),
+            ),
+            useSurface: true,
+            borderRadius: ezPillShape,
+          )
+        : Tooltip(
+            waitDuration: Duration.zero,
+            enableTapToDismiss: false,
+            message: message,
+            child: Container(
+              decoration: const BoxDecoration(shape: BoxShape.circle),
+              child: EzIcon(
+                PlatformIcons(context).helpOutline,
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
+          );
   }
 }
