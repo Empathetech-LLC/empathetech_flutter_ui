8d9bb1458577f484f31c7bc5b5278529a41ef09b
empathictech <105748722+empathictech@users.noreply.github.com>
2025-01-16 12:42:33 -0800

    simplified

--- lib/src/functions/images.dart
+++ lib/src/functions/images.dart
@@ -6,10 +6,8 @@
 import '../../empathetech_flutter_ui.dart';
 
 import 'dart:io';
-import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 
 /// Provide the [path] to an [Image] and we'll handle the rest
 ImageProvider provideImage(String path) {
@@ -22,26 +20,18 @@ ImageProvider provideImage(String path) {
   }
 }
 
-/// Save (and/or overwrite) the [Image] stored in [prefsPath] from [source]
-Future<String?> saveImage({
+/// Pick an [Image] and return the path [String]
+Future<String?> pickImage({
   required BuildContext context,
   required String prefsPath,
   required ImageSource source,
 }) async {
-  // Load image picker and save the result
   try {
     final XFile? picked = await ImagePicker().pickImage(source: source);
     if (picked == null) return null;
 
-    // Build the path
-    final Directory directory = await getApplicationDocumentsDirectory();
-    final String imageName = basename(picked.path);
-    final File image = File('${directory.path}/$imageName');
-
-    // Save the new image
-    await File(picked.path).copy(image.path);
-    await EzConfig.setString(prefsPath, image.path);
-    return image.path;
+    await EzConfig.setString(prefsPath, picked.path);
+    return picked.path;
   } on Exception catch (e) {
     if (context.mounted) {
       final String errorMsg = EFUILang.of(context)!.isSetFailed(e.toString());
