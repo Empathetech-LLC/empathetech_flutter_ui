b6435199da0eb3aec707bd0a31d59c641a39bf4a
empathictech <105748722+empathictech@users.noreply.github.com>
2025-01-26 10:00:09 -0800

    renamed and added ez icon button

---
+++ lib/src/widgets/helpers/icon_button.dart
@@ -0,0 +1,167 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2022-2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../../../empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+
+class EzIcon extends Icon {
+  /// [Icon] wrapper that responds to [EzConfig]s [iconSizeKey]
+  /// [ThemeData.iconTheme] does not seem to be consumed properly at time of writing
+  /// Jan 2025
+  EzIcon(
+    super.icon, {
+    super.key,
+    super.fill,
+    super.weight,
+    super.grade,
+    super.opticalSize,
+    super.color,
+    super.shadows,
+    super.semanticLabel,
+    super.textDirection,
+    super.applyTextScaling,
+    super.blendMode,
+  }) : super(size: EzConfig.get(iconSizeKey));
+}
+
+class EzIconButton extends StatelessWidget {
+  /// [IconButton.iconSize] passthrough
+  /// Defaults to [EzConfig.get]s [iconSizeKey]
+  final double? iconSize;
+
+  /// [IconButton.visualDensity] passthrough
+  final VisualDensity? visualDensity;
+
+  /// [IconButton.padding] passthrough
+  final EdgeInsetsGeometry? padding;
+
+  /// [IconButton.alignment] passthrough
+  final AlignmentGeometry? alignment;
+
+  /// [IconButton.splashRadius] passthrough
+  final double? splashRadius;
+
+  /// [IconButton.color] passthrough
+  final Color? color;
+
+  /// [IconButton.focusColor] passthrough
+  final Color? focusColor;
+
+  /// [IconButton.hoverColor] passthrough
+  final Color? hoverColor;
+
+  /// [IconButton.highlightColor] passthrough
+  final Color? highlightColor;
+
+  /// [IconButton.splashColor] passthrough
+  final Color? splashColor;
+
+  /// [IconButton.disabledColor] passthrough
+  final Color? disabledColor;
+
+  /// [IconButton.onPressed] passthrough
+  final VoidCallback? onPressed;
+
+  /// [IconButton.mouseCursor] passthrough
+  final MouseCursor? mouseCursor;
+
+  /// [IconButton.focusNode] passthrough
+  final FocusNode? focusNode;
+
+  /// [IconButton.autofocus] passthrough
+  final bool autofocus;
+
+  /// [IconButton.tooltip] passthrough
+  final String? tooltip;
+
+  /// [IconButton.enableFeedback] passthrough
+  final bool? enableFeedback;
+
+  /// [IconButton.constraints] passthrough
+  final BoxConstraints? constraints;
+
+  /// [IconButton.style] passthrough
+  final ButtonStyle? style;
+
+  /// [IconButton.isSelected] passthrough
+  final bool? isSelected;
+
+  /// [IconButton.selectedIcon] passthrough
+  final Widget? selectedIcon;
+
+  /// [IconButton.icon] passthrough
+  final Widget icon;
+
+  /// [IconButton] wrapper with custom styling
+  /// All [AppBar] IconButtons use [ThemeData.iconButtonTheme] for styling... and are super annoying to overwrite
+  /// But, IMO [AppBar] IconBUttons and "regular" ones shouldn't behave the same
+  /// So, it's recommended to use regular [IconButton]s in the [AppBar] and [EzIconButton]s everywhere else
+  const EzIconButton({
+    super.key,
+    this.iconSize,
+    this.visualDensity,
+    this.padding,
+    this.alignment,
+    this.splashRadius,
+    this.color,
+    this.focusColor,
+    this.hoverColor,
+    this.highlightColor,
+    this.splashColor,
+    this.disabledColor,
+    this.onPressed,
+    this.mouseCursor,
+    this.focusNode,
+    this.autofocus = false,
+    this.tooltip,
+    this.enableFeedback,
+    this.constraints,
+    this.style,
+    this.isSelected,
+    this.selectedIcon,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return IconButton(
+      iconSize: iconSize ?? EzConfig.get(iconSizeKey),
+      visualDensity: visualDensity,
+      padding: padding,
+      alignment: alignment,
+      splashRadius: splashRadius,
+      color: color,
+      focusColor: focusColor,
+      hoverColor: hoverColor,
+      highlightColor: highlightColor,
+      splashColor: splashColor,
+      disabledColor: disabledColor,
+      onPressed: onPressed,
+      mouseCursor: mouseCursor,
+      focusNode: focusNode,
+      autofocus: autofocus,
+      tooltip: tooltip,
+      enableFeedback: enableFeedback,
+      constraints: constraints,
+      style: style ??
+          IconButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            foregroundColor: colorScheme.primary,
+            disabledForegroundColor: colorScheme.outline,
+            overlayColor: colorScheme.primary,
+            side: BorderSide(color: colorScheme.primaryContainer),
+            iconSize: iconSize,
+            alignment: Alignment.center,
+            padding: EzInsets.wrap(EzConfig.get(paddingKey)),
+          ),
+      isSelected: isSelected,
+      selectedIcon: selectedIcon,
+      icon: icon,
+    );
+  }
+}
