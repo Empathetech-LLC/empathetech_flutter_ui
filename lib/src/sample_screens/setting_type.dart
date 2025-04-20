4a6ab16cb11b8d94a72ef8eae725da02f817470e
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-20 14:11:37 -0700

    split setting types

--- lib/src/sample_screens/setting_type.dart
+++ lib/src/sample_screens/setting_type.dart
@@ -3,17 +3,57 @@
  * See LICENSE for distribution and usage details.
  */
 
+/// Text setting types
 /// [quick] || [advanced]
-enum EzSettingType { quick, advanced }
+enum EzTSType { quick, advanced }
 
-/// [EzSettingType] path name
-extension EST on EzSettingType {
+/// [EzTSType] path name
+extension TextPaths on EzTSType {
   String get path {
     switch (this) {
-      case EzSettingType.quick:
+      case EzTSType.quick:
         return 'quick';
-      case EzSettingType.advanced:
+      case EzTSType.advanced:
         return 'advanced';
+    }
+  }
+}
+
+extension TextNames on EzTSType {
+  String get name {
+    switch (this) {
+      case EzTSType.quick:
+        return 'quick_text_settings';
+      case EzTSType.advanced:
+        return 'advanced_text_settings';
+    }
+  }
+}
+
+/// Color setting types
+/// [quick] || [advanced]
+enum EzCSType { quick, advanced }
+
+/// [EzCSType] path name
+extension ColorPaths on EzCSType {
+  String get path {
+    switch (this) {
+      case EzCSType.quick:
+        return 'quick';
+      case EzCSType.advanced:
+        return 'advanced';
+    }
+  }
+}
+
+/// [EzCSType] path name
+extension ColorNames on EzCSType {
+  String get name {
+    switch (this) {
+      case EzCSType.quick:
+        return 'quick_color_settings';
+      case EzCSType.advanced:
+        return 'advanced_color_settings';
     }
   }
 }
