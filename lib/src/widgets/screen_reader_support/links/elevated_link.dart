2234d50cfb4c80f551397a6c7e748e0dcf7158d0
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-11 15:16:17 -0700

    copy of link widget (for now)

---
+++ lib/src/widgets/screen_reader_support/links/elevated_link.dart
@@ -0,0 +1,82 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../../../../empathetech_flutter_ui.dart';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+
+class EzElevatedLink extends StatelessWidget {
+  /// Destination function
+  /// Provide [onTap] or [url], but not both
+  final void Function()? onTap;
+
+  /// Destination URL
+  /// Provide [onTap] or [url], but not both
+  final Uri? url;
+
+  /// What is it?
+  final String label;
+
+  /// Is it unique?
+  final String? value;
+
+  /// Is this an image?
+  final bool isImage;
+
+  /// What does it do?
+  final String hint;
+
+  /// [Tooltip.message] passthrough
+  final String tooltip;
+
+  /// [InkWell.child] passthrough
+  final Widget child;
+
+  /// [InkWell] wrapper that either opens an internal link via [onTap]
+  /// Or an external link to [url]
+  const EzElevatedLink({
+    super.key,
+    this.onTap,
+    this.url,
+    required this.tooltip,
+    required this.label,
+    this.value,
+    this.isImage = false,
+    required this.hint,
+    required this.child,
+  }) : assert((onTap == null) != (url == null),
+            'Either onTap or url should be provided, but not both.');
+
+  @override
+  Widget build(BuildContext context) {
+    final Color focusColor =
+        Theme.of(context).colorScheme.primary.withValues(alpha: focusOpacity);
+
+    return Tooltip(
+      message: tooltip,
+      excludeFromSemantics: true,
+      child: Semantics(
+        label: label,
+        value: value,
+        link: true,
+        image: isImage,
+        hint: hint,
+        child: ExcludeSemantics(
+          child: onTap != null
+              ? InkWell(focusColor: focusColor, onTap: onTap, child: child)
+              : Link(
+                  uri: url,
+                  builder: (_, FollowLink? followLink) => InkWell(
+                    focusColor: focusColor,
+                    onTap: followLink,
+                    child: child,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}
