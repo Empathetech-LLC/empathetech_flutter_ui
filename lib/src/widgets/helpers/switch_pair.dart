5a4f7fd5a75cf1ca3321baed4c3b01d41551fbdd
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-21 11:46:35 -0700

    comments

--- lib/src/widgets/helpers/switch_pair.dart
+++ lib/src/widgets/helpers/switch_pair.dart
@@ -8,56 +8,129 @@ import '../../../empathetech_flutter_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
-// TODO: Better semantics
-
 class EzSwitchPair extends StatelessWidget {
-  // Row
+  /// [EzRow.reverseHands] passthrough
   final bool reverseHands;
+
+  /// [EzRow.mainAxisSize] passthrough
   final MainAxisSize mainAxisSize;
+
+  /// [EzRow.mainAxisAlignment] passthrough
   final MainAxisAlignment mainAxisAlignment;
+
+  /// [EzRow.crossAxisAlignment] passthrough
   final CrossAxisAlignment crossAxisAlignment;
 
-  // Text
+  /// [EzText.data] passthrough
   final String text;
+
+  /// [EzText.useSurface] passthrough
   final bool useSurface;
+
+  /// [EzText.style] passthrough
   final TextStyle? style;
+
+  /// [EzText.strutStyle] passthrough
   final StrutStyle? strutStyle;
+
+  /// [EzText.textAlign] passthrough
   final TextAlign? textAlign;
+
+  /// [EzText.textDirection] passthrough
   final TextDirection? textDirection;
+
+  /// [EzText.locale] passthrough
   final Locale? locale;
+
+  /// [EzText.softWrap] passthrough
   final bool? softWrap;
+
+  /// [EzText.overflow] passthrough
   final TextOverflow? overflow;
+
+  /// [EzText.textScaler] passthrough
   final TextScaler? textScaler;
+
+  /// [EzText.maxLines] passthrough
   final int? maxLines;
+
+  /// [EzText.semanticsLabel] passthrough
   final String? semanticsLabel;
+
+  /// [EzText.textWidthBasis] passthrough
   final TextWidthBasis? textWidthBasis;
+
+  /// [EzText.textHeightBehavior] passthrough
   final TextHeightBehavior? textHeightBehavior;
+
+  /// [EzText.selectionColor] passthrough
   final Color? selectionColor;
+
+  /// [EzText.backgroundColor] passthrough
   final Color? backgroundColor;
 
-  // Switch
+  /// [Switch.value] passthrough
   final bool value;
+
+  /// [Switch.onChanged] passthrough
   final ValueChanged<bool?>? onChanged;
+
+  /// [Switch.activeColor] passthrough
   final Color? activeColor;
+
+  /// [Switch.activeTrackColor] passthrough
   final Color? activeTrackColor;
+
+  /// [Switch.inactiveThumbColor] passthrough
   final Color? inactiveThumbColor;
+
+  /// [Switch.inactiveTrackColor] passthrough
   final Color? inactiveTrackColor;
+
+  /// [Switch.activeThumbImage] passthrough
   final ImageProvider<Object>? activeThumbImage;
+
+  /// [Switch.onActiveThumbImageError] passthrough
   final ImageErrorListener? onActiveThumbImageError;
+
+  /// [Switch.inactiveThumbImage] passthrough
   final ImageProvider<Object>? inactiveThumbImage;
+
+  /// [Switch.onInactiveThumbImageError] passthrough
   final ImageErrorListener? onInactiveThumbImageError;
+
+  /// [Switch.materialTapTargetSize] passthrough
   final MaterialTapTargetSize? materialTapTargetSize;
+
+  /// [Switch.dragStartBehavior] passthrough
   final DragStartBehavior dragStartBehavior;
+
+  /// [Switch.mouseCursor] passthrough
   final MouseCursor? mouseCursor;
+
+  /// [Switch.focusColor] passthrough
   final Color? focusColor;
+
+  /// [Switch.hoverColor] passthrough
   final Color? hoverColor;
+
+  /// [Switch.splashRadius] passthrough
   final double? splashRadius;
+
+  /// [Switch.focusNode] passthrough
   final FocusNode? focusNode;
+
+  /// [Switch.onFocusChange] passthrough
   final ValueChanged<bool>? onFocusChange;
+
+  /// [Switch.autofocus] passthrough
   final bool autofocus;
+
+  /// [Switch.padding] passthrough
   final EdgeInsetsGeometry? padding;
 
-  /// [EzRow] with [Text], an [EzSpacer], and a [Switch]
+  /// [EzRow] with [EzText] and a [Switch]
+  /// The [text] is [Flexible]
   const EzSwitchPair({
     super.key,
     // Row
