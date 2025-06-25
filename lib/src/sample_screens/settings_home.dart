5a9397b1f99222a2f5f763fee8c16aa97edc9a60
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-24 17:04:07 -0700

    more options

--- lib/src/sample_screens/settings_home.dart
+++ lib/src/sample_screens/settings_home.dart
@@ -13,7 +13,7 @@ class EzSettingsHome extends StatefulWidget {
   /// [EzScreen.useImageDecoration] passthrough
   final bool useImageDecoration;
 
-  /// Remove the 'Have fun!' part of the settings disclaimer
+  /// Optionally remove 'Have fun!' part of the settings disclaimer
   /// There are some apps where fun is not appropriate
   final bool notFun;
 
@@ -21,9 +21,16 @@ class EzSettingsHome extends StatefulWidget {
   /// Defaults to [english] to not dupe [americanEnglish]
   final Set<Locale>? skipLocales;
 
-  /// Widgets to be added below language and above any present routes
-  /// Do not include a trailing spacer, one will be provided
+  /// Spacer between the [EzLocaleSetting] and the next block
+  /// [additionalSettings] if present, the navigation buttons if not
+  final Widget localeSpacer;
+
+  /// [Widget]s to be added below the [EzLocaleSetting] and above the navigation buttons
+  /// See [localeSpacer] and [preNavSpacer] for layout tuning
   final List<Widget>? additionalSettings;
+
+  /// If [additionalSettings] is not null, the spacer between it and the navigation buttons
+  final Widget preNavSpacer;
 
   /// [GoRouter.goNamed] path to the text settings screen
   /// If null, no button will appear
@@ -42,12 +49,21 @@ class EzSettingsHome extends StatefulWidget {
   final String? imageSettingsPath;
 
   /// Widgets to be added below any present routes and above randomize
-  /// Do not include a trailing spacer, one will be provided
+  /// See [randomSpacer] or [resetSpacer] for layout tuning
   final List<Widget>? additionalRoutes;
 
-  /// Whether to allow settings randomization
-  /// We think it's fun, but it might not always be appropriate
-  final bool allowRandom;
+  /// Spacer after the navigation buttons and before the [EzConfigRandomizer]
+  /// If null, [EzConfigRandomizer] will not be included
+  final Widget? randomSpacer;
+
+  /// Spacer between the [EzResetButton] and the navigation buttons, or [EzConfigRandomizer] if present
+  final Widget resetSpacer;
+
+  /// [EzResetButton.skip] passthrough
+  final Set<String>? skipKeys;
+
+  /// Spacer before the [footer], if present
+  final Widget footerSpacer;
 
   /// Widgets to be added below reset
   /// Do not include a trailing spacer, one will be provided
@@ -61,13 +77,18 @@ class EzSettingsHome extends StatefulWidget {
     this.useImageDecoration = true,
     this.notFun = false,
     this.skipLocales,
+    this.localeSpacer = const EzSeparator(),
     this.additionalSettings,
+    this.preNavSpacer = const EzSeparator(),
     required this.textSettingsPath,
     required this.layoutSettingsPath,
     required this.colorSettingsPath,
     required this.imageSettingsPath,
     this.additionalRoutes,
-    this.allowRandom = true,
+    this.randomSpacer = const EzSeparator(),
+    this.resetSpacer = const EzSeparator(),
+    this.skipKeys,
+    this.footerSpacer = const EzSeparator(),
     this.footer,
   });
 
@@ -115,11 +136,11 @@ class _EzSettingsHomeState extends State<EzSettingsHome> {
           spacer,
 
           EzLocaleSetting(skip: widget.skipLocales ?? <Locale>{english}),
-          separator,
+          widget.localeSpacer,
 
           if (widget.additionalSettings != null) ...<Widget>[
             ...widget.additionalSettings!,
-            separator,
+            widget.preNavSpacer,
           ],
 
           // Text settings
@@ -162,33 +183,23 @@ class _EzSettingsHomeState extends State<EzSettingsHome> {
             spacer,
           ],
 
-          // Turn trailing spacer (if present) into a separator
-          if (widget.additionalRoutes == null &&
-              (widget.textSettingsPath != null ||
-                  widget.layoutSettingsPath != null ||
-                  widget.colorSettingsPath != null ||
-                  widget.imageSettingsPath != null))
-            spacer,
-
-          if (widget.additionalRoutes != null) ...<Widget>[
-            ...widget.additionalRoutes!,
-            separator,
-          ],
+          if (widget.additionalRoutes != null) ...widget.additionalRoutes!,
 
           // Feeling lucky
-          if (widget.allowRandom) ...<Widget>[
+          if (widget.randomSpacer != null) ...<Widget>[
+            widget.randomSpacer!,
             const EzConfigRandomizer(),
-            separator,
           ],
 
           // Reset button
-          const EzResetButton(),
-          separator,
+          widget.resetSpacer,
+          EzResetButton(skip: widget.skipKeys),
 
           if (widget.footer != null) ...<Widget>[
+            widget.footerSpacer,
             ...widget.footer!,
-            separator,
           ],
+          separator,
         ],
       ),
     );
