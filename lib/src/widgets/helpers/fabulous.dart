7685c96ba3d3267e673c5a22e70e22af15de6fee
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-21 09:35:08 -0700

    added config fab

--- lib/src/widgets/helpers/fabulous.dart
+++ lib/src/widgets/helpers/fabulous.dart
@@ -5,7 +5,9 @@
 
 import '../../../empathetech_flutter_ui.dart';
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:file_saver/file_saver.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzBackFAB extends FloatingActionButton {
@@ -18,5 +20,61 @@ class EzBackFAB extends FloatingActionButton {
               : PlatformIcons(context).back),
           onPressed: () => Navigator.of(context).pop(),
           tooltip: ezL10n(context).gBack,
+        );
+}
+
+class EzConfigFAB extends FloatingActionButton {
+  /// [FloatingActionButton] that opens the configuration screen
+  EzConfigFAB(
+    BuildContext context, {
+    super.key,
+    required String packageName,
+    required String appName,
+    List<String>? extraKeys,
+  }) : super(
+          heroTag: 'config_fab',
+          child: const Icon(Icons.save),
+          onPressed: () async {
+            final List<String> keys = <String>[
+              ...allKeys.keys,
+              if (extraKeys != null) ...extraKeys,
+            ];
+
+            final Map<String, dynamic> config =
+                Map<String, dynamic>.fromEntries(keys.map(
+              (String key) => MapEntry<String, dynamic>(
+                key,
+                EzConfig.get(key),
+              ),
+            ));
+
+            late final String savedConfig;
+            try {
+              savedConfig = await FileSaver.instance.saveFile(
+                name: '${appName}_settings.json',
+                bytes: utf8.encode(jsonEncode(config)),
+                mimeType: MimeType.json,
+              );
+            } catch (e) {
+              if (context.mounted) ezLogAlert(context, message: e.toString());
+            }
+
+            if (context.mounted) {
+              savedConfig.endsWith('.json')
+                  ? ezSnackBar(
+                      context: context,
+                      message: ezL10n(context).ssConfigSaved(archivePath(
+                        packageName: packageName,
+                        appName: appName,
+                      )),
+                    )
+                  : ezLogAlert(
+                      context,
+                      message:
+                          '${ezL10n(context).ssWrongConfigExt} .json...\n\n$savedConfig',
+                    );
+            }
+          },
+          tooltip: ezL10n(context).gConfig,
         );
 }
