1fb859ae43ce8cee626b058e9d86467cb2469c60
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-23 12:04:56 -0700

    better controls, state not quite working

--- lib/src/sample_screens/image_editor.dart
+++ lib/src/sample_screens/image_editor.dart
@@ -5,11 +5,12 @@
 
 import '../../empathetech_flutter_ui.dart';
 
-// TODO: l10n
+// TODO: l10n && tooltips
 // TODO: Get the preview space to be a live ratio of the window
 // TODO: Actually update the image path on completion
 
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
@@ -34,6 +35,10 @@ class _EzImageEditorState extends State<EzImageEditor> {
   final double spacing = EzConfig.get(spacingKey);
   final double iconSize = EzConfig.get(iconSizeKey);
 
+  // Define the build data //
+
+  final ImageEditorController _editorController = ImageEditorController();
+
   // Define custom functions //
 
   Widget keyIcon(IconData icon, Color color, String name) => Column(
@@ -44,7 +49,8 @@ class _EzImageEditorState extends State<EzImageEditor> {
           EzText(
             name,
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.labelLarge,
+            style:
+                Theme.of(context).textTheme.labelLarge?.copyWith(color: color),
           )
         ],
       );
@@ -57,63 +63,106 @@ class _EzImageEditorState extends State<EzImageEditor> {
 
     // Return the build //
 
-    return EzAdaptiveScaffold(
-      small: Scaffold(
-        body: EzScreen(
-          Column(children: <Widget>[
-            Expanded(
-              child: ExtendedImage.file(
-                File(widget.imagePath),
-                fit: BoxFit.contain,
-                mode: ExtendedImageMode.editor,
+    return Scaffold(
+      body: EzScreen(
+        Column(children: <Widget>[
+          // Preview
+          Expanded(
+            child: ExtendedImage.file(
+              File(widget.imagePath),
+              fit: BoxFit.contain,
+              mode: ExtendedImageMode.editor,
+              enableLoadState: true,
+              initEditorConfigHandler: (_) => EditorConfig(
+                cropRectPadding: EdgeInsets.only(
+                  top: spacing,
+                  left: spacing,
+                  right: spacing,
+                ),
+                hitTestSize: max(padding, kMinInteractiveDimension),
+                controller: _editorController,
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(spacing),
-              child: EzScrollView(
-                mainAxisSize: MainAxisSize.min,
-                scrollDirection: Axis.horizontal,
-                startCentered: true,
-                children: <Widget>[
-                  // Drag
-                  keyIcon(Icons.touch_app, colorScheme.outline, 'Drag'),
-                  spacer,
+          ),
 
-                  // Pinch
-                  keyIcon(Icons.pinch, colorScheme.outline, 'Pinch'),
-                  spacer,
+          // Key && controls
+          Padding(
+            padding: EdgeInsets.all(spacing),
+            child: EzScrollView(
+              mainAxisSize: MainAxisSize.min,
+              scrollDirection: Axis.horizontal,
+              startCentered: true,
+              children: <Widget>[
+                // Drag
+                keyIcon(Icons.touch_app, colorScheme.outline, 'Drag'),
+                spacer,
 
-                  // Swipe
-                  keyIcon(Icons.swipe, colorScheme.outline, 'Swipe'),
+                // Pinch
+                keyIcon(Icons.pinch, colorScheme.outline, 'Pinch'),
+                spacer,
 
-                  // Divider
-                  SizedBox(
-                    height: iconSize + padding,
-                    child: VerticalDivider(
-                      width: spacing * 4,
-                      color: colorScheme.secondary,
-                    ),
+                // Swipe
+                keyIcon(Icons.swipe, colorScheme.outline, 'Swipe'),
+
+                // Divider
+                SizedBox(
+                  height: iconSize + padding,
+                  child: VerticalDivider(
+                    width: spacing * 4,
+                    color: colorScheme.secondary,
                   ),
+                ),
 
-                  // Done
-                  EzIconButton(
-                    onPressed: () => Navigator.pop(context, widget.imagePath),
-                    icon: EzIcon(Icons.check),
-                  ),
-                  spacer,
+                // Done
+                EzIconButton(
+                  onPressed: () => Navigator.pop(context, widget.imagePath),
+                  icon: EzIcon(Icons.check),
+                ),
+                spacer,
 
-                  // Cancel
-                  EzIconButton(
-                    onPressed: () => Navigator.pop(context, null),
-                    icon: EzIcon(PlatformIcons(context).delete),
-                  ),
-                ],
-              ),
-            )
-          ]),
-          margin: EdgeInsets.zero,
-          useImageDecoration: false,
-        ),
+                // Undo
+                EzIconButton(
+                  enabled: _editorController.canUndo,
+                  onPressed: () {
+                    _editorController.undo();
+                    setState(() {});
+                  },
+                  icon: EzIcon(Icons.undo),
+                ),
+                spacer,
+
+                // Redo
+                EzIconButton(
+                  enabled: _editorController.canRedo,
+                  onPressed: () {
+                    _editorController.redo();
+                    setState(() {});
+                  },
+                  icon: EzIcon(Icons.redo),
+                ),
+                spacer,
+
+                // Reset
+                EzIconButton(
+                  onPressed: () {
+                    _editorController.reset();
+                    setState(() {});
+                  },
+                  icon: EzIcon(PlatformIcons(context).refresh),
+                ),
+                spacer,
+
+                // Cancel
+                EzIconButton(
+                  onPressed: () => Navigator.pop(context, null),
+                  icon: EzIcon(PlatformIcons(context).delete),
+                ),
+              ],
+            ),
+          )
+        ]),
+        margin: EdgeInsets.zero,
+        useImageDecoration: false,
       ),
     );
   }
