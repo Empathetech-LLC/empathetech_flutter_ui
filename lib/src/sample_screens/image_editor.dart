74af626d123225d68f4ec60eadd96bd5031f34da
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-24 10:39:26 -0700

    almost done

--- lib/src/sample_screens/image_editor.dart
+++ lib/src/sample_screens/image_editor.dart
@@ -5,8 +5,6 @@
 
 import '../../empathetech_flutter_ui.dart';
 
-// TODO: l10n && tooltips
-// TODO: Get the preview space to be a live ratio of the window
 // TODO: Actually update the image path on completion
 
 import 'dart:io';
@@ -16,11 +14,36 @@ import 'package:extended_image/extended_image.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class EzImageEditor extends StatefulWidget {
+  /// File path of the image being edited
   final String imagePath;
+
+  /// [ExtendedImageMode.editor] cropAspectRatio passthrough
+  /// If null, it will mirror the window's aspect ratio
+  /// Use [CropAspectRatios.custom] for freeform
+  final double? cropAspectRatio;
+
+  /// [ExtendedImageMode.editor] initialCropAspectRatio passthrough
+  /// If null, it will mirror the window's aspect ratio
+  /// Use [CropAspectRatios.custom] for freeform
+  final double? initialCropAspectRatio;
+
+  /// [ExtendedImageMode.editor] initCropRectType passthrough
+  final InitCropRectType initCropRectType;
 
   /// Allows the user to crop/zoom/rotate the image
   /// Has it's own [Scaffold], so it can (should) be used as a standalone screen
-  const EzImageEditor(this.imagePath, {super.key});
+  const EzImageEditor(
+    this.imagePath, {
+    super.key,
+    this.initCropRectType = InitCropRectType.imageRect,
+    this.cropAspectRatio,
+    this.initialCropAspectRatio,
+  }) : assert(
+          cropAspectRatio == null ||
+              initialCropAspectRatio == null ||
+              cropAspectRatio == initialCropAspectRatio,
+          'If both cropAspectRatio and initialCropAspectRatio are provided, they must be equal.',
+        );
 
   @override
   State<EzImageEditor> createState() => _EzImageEditorState();
@@ -35,25 +58,56 @@ class _EzImageEditorState extends State<EzImageEditor> {
   final double spacing = EzConfig.get(spacingKey);
   final double iconSize = EzConfig.get(iconSizeKey);
 
+  late final EFUILang l10n = ezL10n(context);
+
   // Define the build data //
 
   final ImageEditorController _editorController = ImageEditorController();
 
-  // Define custom functions //
+  // Define custom functions && widgets //
 
-  Widget keyIcon(IconData icon, Color color, String name) => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          EzIcon(icon, color: color),
-          EzMargin(),
-          EzText(
-            name,
-            textAlign: TextAlign.center,
-            style:
-                Theme.of(context).textTheme.labelLarge?.copyWith(color: color),
-          )
-        ],
+  double liveAspectRatio() {
+    final Size size = MediaQuery.of(context).size;
+    return size.width / size.height;
+  }
+
+  Widget keyIcon({
+    required IconData icon,
+    required Color color,
+    required String name,
+    required String tooltip,
+  }) =>
+      Tooltip(
+        message: tooltip,
+        excludeFromSemantics: false,
+        child: ExcludeSemantics(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              EzIcon(icon, color: color),
+              EzMargin(),
+              EzText(
+                name,
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge
+                    ?.copyWith(color: color),
+              )
+            ],
+          ),
+        ),
       );
+
+  // Init //
+
+  void updateState() => setState(() {});
+
+  @override
+  void initState() {
+    super.initState();
+    _editorController.addListener(updateState);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +128,10 @@ class _EzImageEditorState extends State<EzImageEditor> {
               mode: ExtendedImageMode.editor,
               enableLoadState: true,
               initEditorConfigHandler: (_) => EditorConfig(
+                cropAspectRatio: widget.cropAspectRatio ?? liveAspectRatio(),
+                initialCropAspectRatio:
+                    widget.initialCropAspectRatio ?? liveAspectRatio(),
+                initCropRectType: widget.initCropRectType,
                 cropRectPadding: EdgeInsets.only(
                   top: spacing,
                   left: spacing,
@@ -94,15 +152,30 @@ class _EzImageEditorState extends State<EzImageEditor> {
               startCentered: true,
               children: <Widget>[
                 // Drag
-                keyIcon(Icons.touch_app, colorScheme.outline, 'Drag'),
-                spacer,
-
-                // Pinch
-                keyIcon(Icons.pinch, colorScheme.outline, 'Pinch'),
+                keyIcon(
+                  icon: Icons.touch_app,
+                  color: colorScheme.outline,
+                  name: l10n.isDrag,
+                  tooltip: l10n.isDragHint,
+                ),
                 spacer,
 
                 // Swipe
-                keyIcon(Icons.swipe, colorScheme.outline, 'Swipe'),
+                keyIcon(
+                  icon: Icons.swipe,
+                  color: colorScheme.outline,
+                  name: l10n.isSwipe,
+                  tooltip: l10n.isSwipeHint,
+                ),
+
+                // Pinch
+                keyIcon(
+                  icon: Icons.pinch,
+                  color: colorScheme.outline,
+                  name: l10n.isPinch,
+                  tooltip: l10n.isPinchHint,
+                ),
+                spacer,
 
                 // Divider
                 SizedBox(
@@ -165,5 +238,12 @@ class _EzImageEditorState extends State<EzImageEditor> {
         useImageDecoration: false,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _editorController.removeListener(updateState);
+    _editorController.dispose();
+    super.dispose();
   }
 }
