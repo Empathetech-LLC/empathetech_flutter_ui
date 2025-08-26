22e8fa9c402653b7c631a8144ff38e3243d5301e
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-25 21:10:32 -0700

    renamed

--- example/lib/screens/settings/design_settings.dart
+++ example/lib/screens/settings/design_settings.dart
@@ -8,13 +8,13 @@ import '../../widgets/export.dart';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
-class ImageSettingsScreen extends StatelessWidget {
-  const ImageSettingsScreen({super.key});
+class DesignSettingsScreen extends StatelessWidget {
+  const DesignSettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) => OpenUIScaffold(
-        title: ezL10n(context).isPageTitle,
+        title: ezL10n(context).dsPageTitle,
         showSettings: false,
-        body: const EzScreen(EzImageSettings()),
+        body: const EzScreen(EzDesignSettings()),
       );
 }
