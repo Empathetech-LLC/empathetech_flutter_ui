326fdcc4bd1d253bcd5acdb073204d521053e7bf
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-25 20:55:44 -0700

    renamed

--- example/lib/screens/export.dart
+++ example/lib/screens/export.dart
@@ -12,10 +12,10 @@ export 'generator/generate.dart';
 export 'generator/archive.dart';
 
 export 'settings/settings_home.dart';
-export 'settings/text_settings.dart';
-export 'settings/image_settings.dart';
 export 'settings/color_settings.dart';
+export 'settings/design_settings.dart';
 export 'settings/layout_settings.dart';
+export 'settings/text_settings.dart';
 
 // Route names //
 
@@ -28,14 +28,14 @@ const String archiveScreenPath = 'config-archiver';
 /// 'settings-home'
 const String settingsHomePath = 'settings-home';
 
-/// 'text-settings'
-const String textSettingsPath = 'text-settings';
-
-/// 'image-settings'
-const String imageSettingsPath = 'image-settings';
-
 /// 'color-settings'
 const String colorSettingsPath = 'color-settings';
 
+/// 'design-settings'
+const String designSettingsPath = 'design-settings';
+
 /// 'layout-settings'
 const String layoutSettingsPath = 'layout-settings';
+
+/// 'text-settings'
+const String textSettingsPath = 'text-settings';
