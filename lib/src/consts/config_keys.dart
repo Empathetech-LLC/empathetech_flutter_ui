1653107d44f6651d2b0925da02573d9f8186a6de
empathictech <105748722+empathictech@users.noreply.github.com>
2025-08-28 15:14:55 -0700

    reverted

--- lib/src/consts/config_keys.dart
+++ lib/src/consts/config_keys.dart
@@ -428,7 +428,7 @@ const String animationDurationKey = 'animationDuration';
 
 /// [animationDurationKey]
 const Map<String, Type> globalDesignKeys = <String, Type>{
-  animationDurationKey: int,
+  animationDurationKey: double,
 };
 
 // Dark theme //
