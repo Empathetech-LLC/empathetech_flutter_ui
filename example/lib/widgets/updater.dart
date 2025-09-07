7fac9caf5e4d20e19c1b57511e9d47b2387b3767
empathictech <105748722+empathictech@users.noreply.github.com>
2025-09-07 13:18:18 -0700

    version bump

--- example/lib/widgets/updater.dart
+++ example/lib/widgets/updater.dart
@@ -23,8 +23,8 @@ const String _github =
 const String _versionSource =
     'https://raw.githubusercontent.com/Empathetech-LLC/empathetech_flutter_ui/refs/heads/main/example/APP_VERSION';
 
-/// '2.2.3'
-const String _appVersion = '2.2.3';
+/// '2.3.0'
+const String _appVersion = '2.3.0';
 
 class EzUpdater extends StatefulWidget {
   /// Checks for Open UI updates
