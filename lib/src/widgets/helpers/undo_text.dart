6bc8864f743de805c5b2394c38356169b5f63d76
empathictech <105748722+empathictech@users.noreply.github.com>
2025-12-01 20:40:51 -0800

    cleanup

--- lib/src/widgets/helpers/undo_text.dart
+++ lib/src/widgets/helpers/undo_text.dart
@@ -5,17 +5,15 @@
 
 import '../../../empathetech_flutter_ui.dart';
 
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:file_saver/file_saver.dart';
 
 /// Remind the user that reset/randomize/etc cannot be undone automatically
 /// Includes and [EzInlineLink] to save current config to JSON
-EzRichText ezRichUndoWarning({
-  required BuildContext context,
-  required List<String>? extraKeys,
+EzRichText ezRichUndoWarning(
+  BuildContext context, {
+  List<String>? extraKeys,
   required String appName,
-  required String androidPackage,
+  String? androidPackage,
 }) {
   final EFUILang l10n = ezL10n(context);
   final TextTheme textTheme = Theme.of(context).textTheme;
@@ -28,43 +26,12 @@ EzRichText ezRichUndoWarning({
       ),
       EzInlineLink(
         l10n.gSave,
-        onTap: () async {
-          final List<String> keys = <String>[
-            ...ezConfigKeys.keys,
-            if (extraKeys != null) ...extraKeys,
-          ];
-
-          final Map<String, dynamic> config =
-              Map<String, dynamic>.fromEntries(keys.map(
-            (String key) => MapEntry<String, dynamic>(
-              key,
-              EzConfig.get(key),
-            ),
-          ));
-
-          try {
-            await FileSaver.instance.saveFile(
-              name: '${ezTitleToSnake(appName)}_settings.json',
-              bytes: utf8.encode(jsonEncode(config)),
-              mimeType: MimeType.json,
-            );
-          } catch (e) {
-            if (context.mounted) {
-              ezLogAlert(context, message: e.toString());
-            }
-            return;
-          }
-
-          if (context.mounted) {
-            ezSnackBar(
-              context: context,
-              message: l10n.ssConfigSaved(archivePath(
-                appName: appName,
-                androidPackage: androidPackage,
-              )),
-            );
-          }
-        },
+        onTap: () => ezConfigSaver(
+          context,
+          extraKeys: extraKeys,
+          appName: appName,
+          androidPackage: androidPackage,
+        ),
         hint:
             '', // TODO: Add a hint? Make this nullable? If yes (to nullable), audit others.
         style: textTheme.bodyLarge,
