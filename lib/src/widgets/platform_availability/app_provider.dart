357740a7e31914f7f07532a99b62b43b040647de
empathictech <105748722+empathictech@users.noreply.github.com>
2025-12-20 19:43:44 -0800

    split in twain

--- lib/src/widgets/platform_availability/app_provider.dart
+++ lib/src/widgets/platform_availability/app_provider.dart
@@ -8,7 +8,6 @@ import 'package:empathetech_flutter_ui/src/functions/helpers_io.dart';
 import '../../../empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
@@ -48,96 +47,6 @@ class EzAppProvider extends StatelessWidget {
           scaffoldMessengerKey: scaffoldMessengerKey,
         ),
       );
-}
-
-class EzThemeProvider extends ChangeNotifier {
-  // Construct //
-
-  final bool _cupertino;
-  bool _ltr;
-  late ThemeMode _themeMode;
-
-  late ThemeData _darkMaterial;
-  late ThemeData _lightMaterial;
-
-  late CupertinoThemeData? _darkCupertino;
-  late CupertinoThemeData? _lightCupertino;
-
-  late EzSpacer _spacer;
-  late EzSeparator _separator;
-  late EzDivider _divider;
-
-  EzThemeProvider({required bool useCupertino, required bool isLTR})
-      : _cupertino = useCupertino,
-        _ltr = isLTR {
-    _themeMode = _getMode;
-    _buildTheme();
-  }
-
-  void _buildTheme() {
-    _darkMaterial = ezThemeData(Brightness.dark, _ltr);
-    _lightMaterial = ezThemeData(Brightness.light, _ltr);
-
-    if (_cupertino) {
-      _darkCupertino =
-          MaterialBasedCupertinoThemeData(materialTheme: _darkMaterial);
-      _lightCupertino =
-          MaterialBasedCupertinoThemeData(materialTheme: _lightMaterial);
-    } else {
-      _darkCupertino = null;
-      _lightCupertino = null;
-    }
-
-    _spacer = const EzSpacer();
-    _separator = const EzSeparator();
-    _divider = const EzDivider();
-  }
-
-  // Get //
-
-  ThemeMode get _getMode {
-    final bool? savedDark = EzConfig.get(isDarkThemeKey);
-
-    return (savedDark == null)
-        ? ThemeMode.system
-        : (savedDark == true)
-            ? ThemeMode.dark
-            : ThemeMode.light;
-  }
-
-  bool get isCupertino => _cupertino;
-  bool get isLTR => _ltr;
-  ThemeMode get themeMode => _themeMode;
-
-  ThemeData get darkMaterial => _darkMaterial;
-  ThemeData get lightMaterial => _lightMaterial;
-
-  CupertinoThemeData? get darkCupertino => _darkCupertino;
-  CupertinoThemeData? get lightCupertino => _lightCupertino;
-
-  EzSpacer get spacer => _spacer;
-  EzSeparator get separator => _separator;
-  EzDivider get divider => _divider;
-
-  // Set //
-
-  void setTextDirection(bool isLTR, {void Function()? onComplete}) {
-    _ltr = isLTR;
-    rebuild(onComplete: onComplete);
-  }
-
-  void setThemeMode({void Function()? onComplete}) {
-    _themeMode = _getMode;
-    notifyListeners();
-    onComplete?.call();
-  }
-
-  void rebuild({void Function()? onComplete}) {
-    _themeMode = _getMode;
-    _buildTheme();
-    notifyListeners();
-    onComplete?.call();
-  }
 }
 
 class _ProviderSquared extends StatelessWidget {
---
+++ lib/src/widgets/platform_availability/theme_provider.dart
@@ -0,0 +1,128 @@
+/* empathetech_flutter_ui
+ * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../../../empathetech_flutter_ui.dart';
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class EzThemeProvider extends ChangeNotifier {
+  // Construct //
+
+  final bool _cupertino;
+  bool _ltr;
+
+  int _seed;
+  late ThemeMode _themeMode;
+
+  late ThemeData _darkMaterial;
+  late ThemeData _lightMaterial;
+
+  late CupertinoThemeData? _darkCupertino;
+  late CupertinoThemeData? _lightCupertino;
+
+  late EzLayoutWidgets _layout;
+
+  EzThemeProvider({required bool useCupertino, required bool isLTR})
+      : _cupertino = useCupertino,
+        _ltr = isLTR,
+        _seed = Random().nextInt(rMax) {
+    _themeMode = _getMode;
+    _buildTheme();
+  }
+
+  void _buildTheme() {
+    _darkMaterial = ezThemeData(Brightness.dark, _ltr);
+    _lightMaterial = ezThemeData(Brightness.light, _ltr);
+
+    if (_cupertino) {
+      _darkCupertino =
+          MaterialBasedCupertinoThemeData(materialTheme: _darkMaterial);
+      _lightCupertino =
+          MaterialBasedCupertinoThemeData(materialTheme: _lightMaterial);
+    } else {
+      _darkCupertino = null;
+      _lightCupertino = null;
+    }
+
+    _layout = EzLayoutWidgets(
+      margin: EzMargin(),
+      rowMargin: EzMargin(vertical: false),
+      spacer: const EzSpacer(),
+      rowSpacer: const EzSpacer(vertical: false),
+      separator: const EzSeparator(),
+      divider: const EzDivider(),
+    );
+  }
+
+  // Get //
+
+  ThemeMode get _getMode {
+    final bool? savedDark = EzConfig.get(isDarkThemeKey);
+
+    return (savedDark == null)
+        ? ThemeMode.system
+        : (savedDark == true)
+            ? ThemeMode.dark
+            : ThemeMode.light;
+  }
+
+  bool get isCupertino => _cupertino;
+  bool get isLTR => _ltr;
+
+  int get seed => _seed;
+  ThemeMode get themeMode => _themeMode;
+  EzLayoutWidgets get layout => _layout;
+
+  ThemeData get darkMaterial => _darkMaterial;
+  ThemeData get lightMaterial => _lightMaterial;
+
+  CupertinoThemeData? get darkCupertino => _darkCupertino;
+  CupertinoThemeData? get lightCupertino => _lightCupertino;
+
+  // Set //
+
+  void redraw() {
+    _seed = Random().nextInt(rMax);
+    notifyListeners();
+  }
+
+  void setTextDirection(bool isLTR, {void Function()? onComplete}) {
+    _ltr = isLTR;
+    rebuild(onComplete: onComplete);
+  }
+
+  void setThemeMode({void Function()? onComplete}) {
+    _themeMode = _getMode;
+    redraw();
+    onComplete?.call();
+  }
+
+  void rebuild({void Function()? onComplete}) {
+    _themeMode = _getMode;
+    _buildTheme();
+    redraw();
+    onComplete?.call();
+  }
+}
+
+class EzLayoutWidgets {
+  final EzMargin margin;
+  final EzMargin rowMargin;
+  final EzSpacer spacer;
+  final EzSpacer rowSpacer;
+  final EzSeparator separator;
+  final EzDivider divider;
+
+  EzLayoutWidgets({
+    required this.margin,
+    required this.rowMargin,
+    required this.spacer,
+    required this.rowSpacer,
+    required this.separator,
+    required this.divider,
+  });
+}
