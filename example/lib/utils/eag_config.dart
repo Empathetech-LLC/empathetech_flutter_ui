172fd1d5b6734817132b33382c14dd8bdac9d764
empathictech <105748722+empathictech@users.noreply.github.com>
2026-01-13 10:27:52 -0500

    new year new copyright

--- example/lib/utils/eag_config.dart
+++ example/lib/utils/eag_config.dart
@@ -1,5 +1,5 @@
 /* open_ui
- * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * Copyright (c) 2026 Empathetech LLC. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
 
