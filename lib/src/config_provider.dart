f17089564dadc548223daf90b05a8cdcc291a245
empathictech <105748722+empathictech@users.noreply.github.com>
2026-01-25 12:20:44 -0500

    comments and cleanup

--- lib/src/config_provider.dart
+++ lib/src/config_provider.dart
@@ -41,6 +41,7 @@ class EzConfigProvider extends ChangeNotifier {
     _buildTheme();
   }
 
+  /// Gather and set [_themeMode] from storage
   void _buildMode() {
     final bool? savedDark = EzConfig.get(isDarkThemeKey);
 
@@ -51,6 +52,7 @@ class EzConfigProvider extends ChangeNotifier {
             : ThemeMode.light;
   }
 
+  /// Builds fresh themes and config caches
   void _buildTheme() {
     _darkTheme = ezThemeData(Brightness.dark, _ltr);
     _lightTheme = ezThemeData(Brightness.light, _ltr);
@@ -94,57 +96,86 @@ class EzConfigProvider extends ChangeNotifier {
 
   // Get //
 
+  /// Track [redraw] and [rebuild] (randomized on each call)
   int get seed => _seed;
+
+  /// Text direction for the [locale]
   bool get isLTR => _ltr;
 
+  /// Current language for the app
   Locale get locale => _locale;
+
+  /// EFUI localizations for the [locale]
   EFUILang get l10n => _l10n;
 
+  /// Current [ThemeMode]
   ThemeMode get themeMode => _themeMode;
+
+  /// Whether the current [themeMode] uses [Brightness.dark]
   bool get isDark => _isDark;
 
+  /// Cache of frequently used design config values
   EzDesignCache get design => _design;
+
+  /// Cache of frequently used layout config values
   EzLayoutCache get layout => _layout;
+
+  /// Cache of frequently used text config values
   EzTextCache get text => _text;
 
+  /// Current dark theme
   ThemeData get darkTheme => _darkTheme;
+
+  /// Current light theme
   ThemeData get lightTheme => _lightTheme;
 
   // Set //
 
+  /// Randomizes the [seed] and notifies listeners
+  /// Optionally calls [onComplete] after notifying
+  /// HIGHLY recommended to wrap [Scaffold] Widgets in a [Consumer] for [EzConfigProvider]
+  /// Simply set the [Scaffold.key] to an int [ValueKey] of the consumed [EzConfigProvider.seed] and all [EzConfig] updates will be live!
   void redraw({void Function()? onComplete}) {
     _seed = Random().nextInt(rMax);
     notifyListeners();
     onComplete?.call();
   }
 
+  /// Rebuilds the apps [ThemeData] and updates the config caches
+  /// Then calls [redraw] with [onComplete]
+  /// HIGHLY recommended to wrap [Scaffold] Widgets in a [Consumer] for [EzConfigProvider]
+  /// Simply set the [Scaffold.key] to an int [ValueKey] of the consumed [EzConfigProvider.seed] and all [EzConfig] updates will be live!
   void rebuild({void Function()? onComplete}) {
     _buildMode();
     _buildTheme();
     redraw(onComplete: onComplete);
   }
 
+  /// Set the text direction for the app and [rebuild] with [onComplete]
   void setTextDirection(bool isLTR, {void Function()? onComplete}) {
     _ltr = isLTR;
     rebuild(onComplete: onComplete);
   }
 
+  /// Toggle between dark/light themes and [redraw] with [onComplete]
   void toggleTheme({void Function()? onComplete}) {
     _isDark = !_isDark;
     redraw(onComplete: onComplete);
   }
 
-  void setThemeMode({void Function()? onComplete}) {
+  /// Set the apps [ThemeMode] from storage and [redraw] with [onComplete]
+  void buildThemeMode({void Function()? onComplete}) {
     _buildMode();
     redraw(onComplete: onComplete);
   }
 
-  Future<void> setLocale({void Function()? onComplete}) async {
-    try {
-      final Locale locale = getStoredLocale();
-      final EFUILang localization = await EFUILang.delegate.load(locale);
+  /// Set the apps [Locale] from storage and load corresponding localizations
+  Future<void> buildLocale({void Function()? onComplete}) async {
+    final Locale locale = getStoredLocale();
+    _locale = locale;
 
-      _locale = locale;
+    try {
+      final EFUILang localization = await EFUILang.delegate.load(locale);
       _l10n = localization;
     } catch (_) {
       _l10n = EzConfig.l10nFallback;
@@ -157,6 +188,8 @@ class EzConfigProvider extends ChangeNotifier {
 class EzDesignCache {
   final int animDur;
 
+  /// Theme aware tracker for frequently used design values...
+  /// Animation duration
   EzDesignCache({required this.animDur});
 }
 
@@ -174,6 +207,9 @@ class EzLayoutCache {
 
   final bool hideScroll;
 
+  /// Theme aware tracker for frequently used layout values...
+  /// Margin, padding, and spacing
+  /// Config values and default Widgets
   EzLayoutCache({
     required this.marginVal,
     required this.padding,
@@ -191,5 +227,8 @@ class EzLayoutCache {
 class EzTextCache {
   final double iconSize;
 
+  /// Theme aware tracker for frequently used text values...
+  /// Margin, padding, and spacing
+  /// Icon size
   EzTextCache({required this.iconSize});
 }
