ada92625dfc82f2cebf39704e58507cd455db1ff
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-01 06:50:07 -0500

    renamed func

--- lib/src/widgets/user_customization/theme_mode_switch.dart
+++ lib/src/widgets/user_customization/theme_mode_switch.dart
@@ -77,7 +77,7 @@ class _ThemeModeSwitchState extends State<EzThemeModeSwitch> {
                 await EzConfig.remove(isDarkThemeKey);
                 break;
             }
-            await EzConfig.provider.buildThemeMode();
+            await EzConfig.provider.rebuildThemeMode();
           },
         ),
       ],
