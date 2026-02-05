b4047be5958bf7b47b376b3e1764a0b9ec371b69
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-04 20:16:31 -0500

    added settings dupe/update both

--- example/lib/screens/settings/color.dart
+++ example/lib/screens/settings/color.dart
@@ -9,15 +9,23 @@ import '../../widgets/export.dart';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
-class ColorSettingsScreen extends StatelessWidget {
+class ColorSettingsScreen extends StatefulWidget {
   final EzCSType? target;
 
   ColorSettingsScreen({this.target}) : super(key: ValueKey<int>(EzConfig.seed));
 
   @override
+  State<ColorSettingsScreen> createState() => _ColorSettingsScreenState();
+}
+
+class _ColorSettingsScreenState extends State<ColorSettingsScreen> {
+  bool updateBoth = false;
+
+  @override
   Widget build(BuildContext context) => OpenUIScaffold(
         EzScreen(EzColorSettings(
-          target: target,
+          target: widget.target,
+          updateBoth: updateBoth,
           appName: appName,
           androidPackage: androidPackage,
         )),
@@ -25,10 +33,9 @@ class ColorSettingsScreen extends StatelessWidget {
         showSettings: false,
         fabs: <Widget>[
           EzConfig.spacer,
-          EzConfigFAB(
-            context,
-            appName: appName,
-            androidPackage: androidPackage,
+          EzSettingsDupeFAB(
+            updateBoth,
+            () => setState(() => updateBoth = !updateBoth),
           ),
         ],
       );
