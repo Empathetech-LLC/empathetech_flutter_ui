c743d82ced2b44634457e2e86f7510944b160263
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-10 10:24:54 -0500

    tracking needs rebuild

--- example/lib/screens/settings/design.dart
+++ example/lib/screens/settings/design.dart
@@ -7,6 +7,7 @@ import '../../utils/export.dart';
 import '../../widgets/export.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class DesignSettingsScreen extends StatefulWidget {
@@ -20,25 +21,27 @@ class _DesignSettingsScreenState extends State<DesignSettingsScreen> {
   bool updateBoth = false;
 
   @override
-  Widget build(BuildContext context) => OpenUIScaffold(
-        EzScreen(EzDesignSettings(
-          onUpdate: () => setState(() {}),
-          updateBoth: updateBoth,
-          appName: appName,
-          androidPackage: androidPackage,
-        )),
-        title: EzConfig.l10n.dsPageTitle,
-        showSettings: false,
-        fabs: <Widget>[
-          if (EzConfig.needsRebuild) ...<Widget>[
-            EzConfig.spacer,
-            EzRebuildFAB(() => setState(() {})),
+  Widget build(BuildContext context) => Consumer<EzConfigProvider>(
+        builder: (_, EzConfigProvider config, __) => (OpenUIScaffold(
+          EzScreen(EzDesignSettings(
+            onUpdate: () => setState(() {}),
+            updateBoth: updateBoth,
+            appName: appName,
+            androidPackage: androidPackage,
+          )),
+          title: config.l10n.dsPageTitle,
+          showSettings: false,
+          fabs: <Widget>[
+            if (config.needsRebuild) ...<Widget>[
+              config.layout.spacer,
+              EzRebuildFAB(() => setState(() {})),
+            ],
+            config.layout.spacer,
+            EzSettingsDupeFAB(
+              updateBoth,
+              () => setState(() => updateBoth = !updateBoth),
+            ),
           ],
-          EzConfig.spacer,
-          EzSettingsDupeFAB(
-            updateBoth,
-            () => setState(() => updateBoth = !updateBoth),
-          ),
-        ],
+        )),
       );
 }
