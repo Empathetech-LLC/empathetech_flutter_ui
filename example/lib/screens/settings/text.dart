e82842986eab1bede2936fa60d73399d726621fe
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-11 12:08:55 -0500

    added rebuild fab

--- example/lib/screens/settings/text.dart
+++ example/lib/screens/settings/text.dart
@@ -7,6 +7,7 @@ import '../../utils/export.dart';
 import '../../widgets/export.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class TextSettingsScreen extends StatefulWidget {
@@ -22,22 +23,28 @@ class _TextSettingsScreenState extends State<TextSettingsScreen> {
   bool updateBoth = false;
 
   @override
-  Widget build(BuildContext context) => OpenUIScaffold(
-        EzScreen(EzTextSettings(
-          target: widget.target,
-          onUpdate: () => setState(() {}),
-          updateBoth: updateBoth,
-          appName: appName,
-          androidPackage: androidPackage,
-        )),
-        title: EzConfig.l10n.tsPageTitle,
-        showSettings: false,
-        fabs: <Widget>[
-          EzConfig.spacer,
-          EzSettingsDupeFAB(
-            updateBoth,
-            () => setState(() => updateBoth = !updateBoth),
-          ),
-        ],
+  Widget build(BuildContext context) => Consumer<EzConfigProvider>(
+        builder: (_, EzConfigProvider config, __) => OpenUIScaffold(
+          EzScreen(EzTextSettings(
+            target: widget.target,
+            onUpdate: () => setState(() {}),
+            updateBoth: updateBoth,
+            appName: appName,
+            androidPackage: androidPackage,
+          )),
+          title: config.l10n.tsPageTitle,
+          showSettings: false,
+          fabs: <Widget>[
+            if (config.needsRebuild) ...<Widget>[
+              config.layout.spacer,
+              EzRebuildFAB(() => setState(() {})),
+            ],
+            config.layout.spacer,
+            EzSettingsDupeFAB(
+              updateBoth,
+              () => setState(() => updateBoth = !updateBoth),
+            ),
+          ],
+        ),
       );
 }
