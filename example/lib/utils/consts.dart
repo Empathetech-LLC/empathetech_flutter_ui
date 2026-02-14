698fa9fc4d2a91cc797abfba2e6dd95600186cec
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-14 10:33:24 -0500

    moved lonely values here

--- example/lib/utils/consts.dart
+++ example/lib/utils/consts.dart
@@ -3,8 +3,23 @@
  * See LICENSE for distribution and usage details.
  */
 
+// App config //
+
 /// Open UI
 const String appName = 'Open UI';
 
 /// net.empathetech.open_ui
 const String androidPackage = 'net.empathetech.open_ui';
+
+// App generator //
+
+enum GeneratorState { running, successful, failed }
+
+// App form //
+
+final RegExp appNamePattern = RegExp(r'^[a-z0-9_]+$');
+
+final RegExp domainPattern = RegExp(r'^[a-z0-9_]+\.[a-z]+$');
+
+/// https://docs.flutter.dev/get-started/install
+const String installFlutter = 'https://docs.flutter.dev/get-started/install';
