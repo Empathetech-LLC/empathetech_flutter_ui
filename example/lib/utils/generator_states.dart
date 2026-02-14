86579cc0592b9cf893578c02bbe817893de5d917
empathictech <105748722+empathictech@users.noreply.github.com>
2026-02-14 10:32:29 -0500

    moved

--- example/lib/utils/generator_states.dart
+++
@@ -1,6 +0,0 @@
-/* open_ui
- * Copyright (c) 2026 Empathetech LLC. All rights reserved.
- * See LICENSE for distribution and usage details.
- */
-
-enum GeneratorState { running, successful, failed }
