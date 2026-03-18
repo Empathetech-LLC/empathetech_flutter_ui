0bfaf57541b82e469ab81e774a0a05137cac68e3
empathictech <105748722+empathictech@users.noreply.github.com>
2026-03-17 20:43:04 -0400

    saving progress with favorites (v1)

--- lib/src/functions/helpers.dart
+++ lib/src/functions/helpers.dart
@@ -490,12 +490,17 @@ Future<bool> isGPlayInstall() async {
 /// Opens an [ezModal] with links to each of the sub-settings pages and a common setting from that page
 /// TODO: how can I gracefully add more? or should I just rebuild it?
 /// TODO: make sure it works with urls
-Future<void> openEzFavorites(
-  BuildContext context, {
+Future<void> openEzFavorites({
+  required BuildContext context,
+  required void Function() onComplete,
   required String colorSettingsPath,
   required String designSettingsPath,
   required String layoutSettingsPath,
   required String textSettingsPath,
+  required String appName,
+  required String? androidPackage,
+  required Set<String> saveSkip,
+  required Set<String> resetSkip,
 }) async =>
     ezModal(
       context: context,
@@ -503,8 +508,19 @@ Future<void> openEzFavorites(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           EzConfig.margin,
+          // Global scroll
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              EzThemeModeSwitch(onComplete),
+            ],
+          ),
+          EzConfig.divider,
 
-          // Color
+          // Color link
           EzLink(
             EzConfig.l10n.csPageTitle,
             onTap: () {
@@ -513,10 +529,21 @@ Future<void> openEzFavorites(
             },
             hint: EzConfig.l10n.gOpenLink,
           ),
-          // TODO: high vis/monochromne
+          EzConfig.margin,
+
+          // Color scroll
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              EzMonoChromeColorsSetting(onComplete, both: false),
+            ],
+          ),
           EzConfig.divider,
 
-          // Design
+          // Design link
           EzLink(
             EzConfig.l10n.dsPageTitle,
             onTap: () {
@@ -525,10 +552,34 @@ Future<void> openEzFavorites(
             },
             hint: EzConfig.l10n.gOpenLink,
           ),
-          // TODO: animation duration
+
+          // Design scroll
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              EzConfig.isDark
+                  ? EzImageSetting(
+                      onComplete,
+                      configKey: darkBackgroundImageKey,
+                      label:
+                          EzConfig.l10n.dsBackgroundImg.replaceAll(' ', '\n'),
+                      updateBrightness: Brightness.dark,
+                    )
+                  : EzImageSetting(
+                      onComplete,
+                      configKey: lightBackgroundImageKey,
+                      label:
+                          EzConfig.l10n.dsBackgroundImg.replaceAll(' ', '\n'),
+                      updateBrightness: Brightness.light,
+                    ),
+            ],
+          ),
           EzConfig.divider,
 
-          // Layout
+          // Layout link
           EzLink(
             EzConfig.l10n.lsPageTitle,
             onTap: () {
@@ -537,11 +588,36 @@ Future<void> openEzFavorites(
             },
             hint: EzConfig.l10n.gOpenLink,
           ),
-          // TODO: the two switches
+
           // TODO: Add conditional left/right padding to switch pairs
+          // Layout scroll
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              // Show back FAB
+              EzSwitchPair(
+                valueKey:
+                    EzConfig.isDark ? darkShowBackFABKey : lightShowBackFABKey,
+                afterChanged: (_) async => await EzConfig.rebuildUI(onComplete),
+                text: EzConfig.l10n.lsShowBack,
+              ),
+              EzConfig.rowSpacer,
+
+              // Show scroll
+              EzSwitchPair(
+                valueKey:
+                    EzConfig.isDark ? darkShowScrollKey : lightShowScrollKey,
+                afterChanged: (_) async => await EzConfig.rebuildUI(onComplete),
+                text: EzConfig.l10n.lsShowScroll,
+              ),
+            ],
+          ),
           EzConfig.divider,
 
-          // Text
+          // Text link
           EzLink(
             EzConfig.l10n.tsPageTitle,
             onTap: () {
@@ -550,7 +626,128 @@ Future<void> openEzFavorites(
             },
             hint: EzConfig.l10n.gOpenLink,
           ),
-          // TODO: text size
+
+          // Text scroll
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            reverseHands: true,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              // Icon size
+              // TODO: handle the whole ping rebuild thang (or just rebuild on modal close without fab?)
+              const EzIconSizeSetting(updateBoth: false),
+              EzConfig.rowSpacer,
+
+              // Font family
+              Tooltip(
+                message: EzConfig.l10n.tsFontFamily,
+                child: EzDropdownMenu<String>(
+                  widthEntries: <String>[fingerPaint],
+                  textStyle: EzConfig.styles.bodyLarge,
+                  dropdownMenuEntries: googleStyles.entries
+                      .map((MapEntry<String, TextStyle> entry) =>
+                          DropdownMenuEntry<String>(
+                            value: entry.key,
+                            label: ezCamelToTitle(entry.key),
+                            style: TextButton.styleFrom(textStyle: entry.value),
+                          ))
+                      .toList(),
+                  enableSearch: false,
+                  initialSelection: EzConfig.styles.bodyLarge?.fontFamily ??
+                      EzConfig.getDefault(EzConfig.isDark
+                          ? darkBodyFontFamilyKey
+                          : lightBodyFontFamilyKey),
+                  onSelected: (String? fontFamily) async {
+                    if (fontFamily == null) return;
+
+                    if (EzConfig.isDark) {
+                      await EzConfig.setString(
+                          darkDisplayFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          darkHeadlineFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          darkTitleFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          darkBodyFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          darkLabelFontFamilyKey, fontFamily);
+                    } else {
+                      await EzConfig.setString(
+                          lightDisplayFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          lightHeadlineFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          lightTitleFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          lightBodyFontFamilyKey, fontFamily);
+                      await EzConfig.setString(
+                          lightLabelFontFamilyKey, fontFamily);
+                    }
+
+                    await EzConfig.rebuildUI(onComplete);
+                  },
+                ),
+              ),
+              // EzConfig.rowSpacer,
+
+              // TODO: Text size (make a version that works without providers)
+              // EzTextBackground(
+              //   EzFontDoubleBatchSetting(
+              //     updateBoth: false,
+              //     displayProvider: widget.displayProvider,
+              //     headlineProvider: widget.headlineProvider,
+              //     titleProvider: widget.titleProvider,
+              //     bodyProvider: widget.bodyProvider,
+              //     labelProvider: widget.labelProvider,
+              //   ),
+              //   backgroundColor: backgroundColor,
+              //   borderRadius: ezPillEdge,
+              // ),
+            ],
+          ),
+          EzConfig.divider,
+
+          // Global scroll II
+          EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            scrollDirection: Axis.horizontal,
+            reverseHands: true,
+            mainAxisAlignment: MainAxisAlignment.center,
+            showScrollHint: true,
+            children: <Widget>[
+              // Reset config
+              EzResetButton(
+                onComplete,
+                appName: appName,
+                resetBoth: false,
+                saveSkip: saveSkip,
+                resetSkip: resetSkip,
+              ),
+              EzConfig.rowSpacer,
+
+              // Load config
+              EzElevatedIconButton(
+                onPressed: () => ezConfigLoader(context),
+                icon: const Icon(Icons.upload),
+                label: EzConfig.l10n.ssSaveConfig,
+              ),
+              EzConfig.rowSpacer,
+
+              // Save config
+              EzElevatedIconButton(
+                onPressed: () => EzConfig.saveConfig(
+                  context,
+                  appName: appName,
+                  androidPackage: androidPackage,
+                  skip: saveSkip,
+                ),
+                icon: const Icon(Icons.save),
+                label: EzConfig.l10n.ssSaveConfig,
+              ),
+            ],
+          ),
           EzSpacer(space: EzConfig.spargin),
         ],
       ),
