14d8a4b3b3f1170f368b899a51bda324635bff6b
empathictech <105748722+empathictech@users.noreply.github.com>
2026-03-26 16:17:42 -0400

    commented in progress presets

--- lib/src/widgets/user_customization/presets/quick_config.dart
+++ lib/src/widgets/user_customization/presets/quick_config.dart
@@ -61,10 +61,10 @@ class EzQuickConfig extends StatelessWidget {
               // Fun
               if (!simple) ...<Widget>[
                 wrapIt(EzNebulaConfig(cleanRebuild)),
-                wrapIt(EzFruityConfig(cleanRebuild)),
-                wrapIt(EzWallHolesConfig(cleanRebuild)),
-                wrapIt(EzEdoConfig(cleanRebuild)),
-                wrapIt(EzWhackyConfig(cleanRebuild)),
+                // wrapIt(EzFruityConfig(cleanRebuild)),
+                // wrapIt(EzWallHolesConfig(cleanRebuild)),
+                // wrapIt(EzEdoConfig(cleanRebuild)),
+                // wrapIt(EzWhackyConfig(cleanRebuild)),
               ],
             ],
           ),
