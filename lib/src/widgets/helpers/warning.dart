007cd657a58cda2f6b47a284fce0cccd56a218ce
empathictech <105748722+empathictech@users.noreply.github.com>
2026-04-29 10:23:48 -0400

    less is more

--- lib/src/widgets/helpers/warning.dart
+++ lib/src/widgets/helpers/warning.dart
@@ -11,91 +11,55 @@ class EzWarning extends StatelessWidget {
   /// What does the user need to know?
   final String body;
 
-  /// Defaults to [TextTheme.bodyLarge]
-  final TextStyle? bodyStyle;
-
   /// Warning [String] to grab the user's attention
   /// Defaults to [EFUILang.gAttention]
   final String? title;
 
-  /// Defaults to [TextTheme.titleLarge]
-  final TextStyle? titleStyle;
-
-  /// Defaults to [ColorScheme.secondary]
-  final Color? iconColor;
-
   /// [Card] wrapper designed to grab attention for warnings...
-  ///  /!\ [title] /!\
-  ///      [body]
-  const EzWarning(
-    this.body, {
-    this.bodyStyle,
-    this.title,
-    this.titleStyle,
-    this.iconColor,
-    super.key,
-  });
+  ///  /!\  [title]  /!\
+  ///       [body]
+  const EzWarning(this.body, {this.title, super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Gather the contextual theme data //
+  Widget build(BuildContext context) => Semantics(
+        label: '${title ?? EzConfig.l10n.gAttention}: $body',
+        readOnly: true,
+        child: ExcludeSemantics(
+          child: Card(
+            child: Container(
+              padding: EdgeInsets.all(EzConfig.marginVal),
+              child: EzCol(children: <Widget>[
+                // Title
+                EzScrollView(
+                  scrollDirection: Axis.horizontal,
+                  startCentered: true,
+                  children: <Widget>[
+                    // Thing1
+                    EzIcon(Icons.warning, color: EzConfig.colors.secondary),
+                    EzConfig.rowMargin,
 
-    final Color alertColor = iconColor ?? EzConfig.colors.secondary;
+                    Text(
+                      title ?? EzConfig.l10n.gAttention,
+                      style: EzConfig.styles.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                    EzConfig.rowMargin,
 
-    final TextStyle? tStyle = titleStyle ?? EzConfig.styles.titleLarge;
+                    // Thing 2
+                    EzIcon(Icons.warning, color: EzConfig.colors.secondary),
+                  ],
+                ),
+                EzConfig.spacer,
 
-    // Return the build //
-
-    final String warning = title ?? EzConfig.l10n.gAttention;
-
-    return Semantics(
-      label: '$warning: $body',
-      readOnly: true,
-      child: ExcludeSemantics(
-        child: Card(
-          child: Container(
-            padding: EdgeInsets.all(EzConfig.marginVal),
-            child: EzCol(children: <Widget>[
-              // Title
-              EzScrollView(
-                scrollDirection: Axis.horizontal,
-                startCentered: true,
-                children: <Widget>[
-                  // Thing1
-                  Icon(
-                    Icons.warning,
-                    color: alertColor,
-                    size: tStyle?.fontSize,
-                  ),
-                  EzConfig.rowMargin,
-
-                  Text(
-                    warning,
-                    style: tStyle,
-                    textAlign: TextAlign.center,
-                  ),
-                  EzConfig.rowMargin,
-
-                  // Thing 2
-                  Icon(
-                    Icons.warning,
-                    color: alertColor,
-                    size: tStyle?.fontSize,
-                  ),
-                ],
-              ),
-              EzConfig.spacer,
-
-              // Body
-              Text(
-                body,
-                style: bodyStyle ?? EzConfig.styles.bodyLarge,
-                textAlign: TextAlign.center,
-              ),
-            ]),
+                // Body
+                Text(
+                  body,
+                  style: EzConfig.styles.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ]),
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
